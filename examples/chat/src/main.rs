d942a69357bb51955bab1f2fda65f44291d3bf76
Patrick O'Grady <me@patrickogrady.xyz>
2024-08-15 06:02:41 -0700

    [examples/chat] run as `release` (#25)
    * run chat with release
    
    * change job name to CI

--- examples/chat/src/main.rs
+++ examples/chat/src/main.rs
@@ -5,31 +5,31 @@
 //! ## Friend 1 (Bootstrapper)
 //!
 //! ```sh
-//! cargo run -- --me=1@3001 --friends=1,2,3,4
+//! cargo run --release -- --me=1@3001 --friends=1,2,3,4
 //! ```
 //!
 //! ## Friend 2
 //!
 //! ```sh
-//! cargo run -- --me=2@3002 --friends=1,2,3,4 --bootstrappers=1@127.0.0.1:3001
+//! cargo run --release -- --me=2@3002 --friends=1,2,3,4 --bootstrappers=1@127.0.0.1:3001
 //! ```
 //!
 //! ### Friend 3
 //!
 //! ```sh
-//! cargo run -- --me=3@3003 --friends=1,2,3,4 --bootstrappers=1@127.0.0.1:3001
+//! cargo run --release -- --me=3@3003 --friends=1,2,3,4 --bootstrappers=1@127.0.0.1:3001
 //! ```
 //!
 //! ### Friend 4 (Different Friend as Bootstrapper)
 //!
 //! ```sh
-//! cargo run -- --me=4@3004 --friends=1,2,3,4 --bootstrappers=3@127.0.0.1:3003
+//! cargo run --release -- --me=4@3004 --friends=1,2,3,4 --bootstrappers=3@127.0.0.1:3003
 //! ```
 //!
 //! ### Not Friend (Blocked)
 //!
 //! ```sh
-//! cargo run -- --me=5@3005 --friends=1,2,3,4,5 --bootstrappers=1@127.0.0.1:3001
+//! cargo run --release -- --me=5@3005 --friends=1,2,3,4,5 --bootstrappers=1@127.0.0.1:3001
 //! ```
 
 #[doc(hidden)]
