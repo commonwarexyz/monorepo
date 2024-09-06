e5c012bdba61005fede995ce929d7793319b9be1
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-06 09:41:59 -0700

    add some todos to mock

---
+++ p2p/src/mock/mod.rs
@@ -0,0 +1,4 @@
+//! Send messages between arbitrary peers with configurable performance (drops, latency, corruption, etc.)
+//!
+//! TODO: move to a separate crate because it requires registering instances to handle messages (far outside of
+//! the scope of p2p...this is probably ok if it is just a trait).
