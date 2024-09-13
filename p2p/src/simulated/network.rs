5f0eba48d1d1f2a54581a7bbb6af608851376198
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-13 08:16:36 -0700

    implement link/register

--- p2p/src/simulated/network.rs
+++ p2p/src/simulated/network.rs
@@ -1,8 +1,19 @@
 use super::Error;
+use crate::Recipients;
+use bytes::Bytes;
 use commonware_cryptography::PublicKey;
+use std::collections::HashMap;
 use std::time::Duration;
+use tokio::sync::mpsc;
 
-pub struct Network {}
+type Message = (Recipients, Bytes);
+
+pub struct Network {
+    sender: mpsc::Sender<Message>,
+    receiver: mpsc::Receiver<Message>,
+    links: HashMap<PublicKey, HashMap<PublicKey, Link>>,
+    agents: HashMap<PublicKey, mpsc::Sender<Message>>,
+}
 
 pub struct Link {
     pub latency: Duration,
@@ -10,16 +21,42 @@ pub struct Link {
 
 impl Network {
     pub fn new() -> Self {
-        Self {}
+        let (sender, receiver) = mpsc::channel(1024);
+        Self {
+            sender,
+            receiver,
+            links: HashMap::new(),
+            agents: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, sender: PublicKey, receiver: PublicKey, config: Link) {}
+    /// Link can be called multiple times for the same sender/receiver. The latest
+    /// setting will be used.
+    pub fn link(&mut self, sender: PublicKey, receiver: PublicKey, config: Link) {
+        self.links
+            .entry(sender)
+            .or_insert_with(HashMap::new)
+            .insert(receiver, config);
+    }
 
-    pub fn register(&mut self, public_key: PublicKey) -> (Sender, Receiver) {}
+    pub fn register(&mut self, public_key: PublicKey) -> (Sender, Receiver) {
+        let (sender, receiver) = mpsc::channel(1024);
+        self.agents.insert(public_key, sender);
+        (
+            Sender {
+                sender: self.sender.clone(),
+            },
+            Receiver { receiver },
+        )
+    }
+
+    pub fn run(self) {}
 }
 
 #[derive(Clone)]
-pub struct Sender {}
+pub struct Sender {
+    sender: mpsc::Sender<Message>,
+}
 
 impl crate::Sender for Sender {
     type Error = Error;
@@ -27,13 +64,15 @@ impl crate::Sender for Sender {
     async fn send(
         &self,
         recipients: crate::Recipients,
-        message: bytes::Bytes,
+        message: Bytes,
         priority: bool,
     ) -> Result<Vec<PublicKey>, Error> {
     }
 }
 
-pub struct Receiver {}
+pub struct Receiver {
+    receiver: mpsc::Receiver<Message>,
+}
 
 impl crate::Receiver for Receiver {
     type Error = Error;
