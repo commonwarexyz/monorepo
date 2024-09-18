200131da8473bdcfd17267af502e6f74c315275f
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-18 07:57:09 -0700

    update simulated tests

--- p2p/src/simulated/mod.rs
+++ p2p/src/simulated/mod.rs
@@ -25,103 +25,103 @@ mod tests {
     use crate::{Receiver, Recipients, Sender};
     use bytes::Bytes;
     use commonware_cryptography::{ed25519::insecure_signer, utils::hex, Scheme};
-    use commonware_executor::{deterministic::Deterministic, Executor};
+    use commonware_executor::{deterministic::Executor, Runner, Spawner};
     use rand::Rng;
     use std::collections::HashMap;
     use tokio::sync::mpsc;
 
     fn simulate_messages(seed: u64, size: usize) -> Vec<usize> {
         // Create simulated network
-        let executor = Deterministic::new(seed);
-        executor.run({
-            let executor = executor.clone();
-            async move {
-                let mut network = network::Network::new(
-                    executor.clone(),
-                    network::Config {
-                        max_message_size: 1024 * 1024,
-                    },
-                );
+        let (runner, context) = Executor::init(seed);
+        runner.start(async move {
+            let mut network = network::Network::new(
+                context.clone(),
+                network::Config {
+                    max_message_size: 1024 * 1024,
+                },
+            );
 
-                // Register agents
-                let mut agents = HashMap::new();
-                let (seen_sender, mut seen_receiver) = mpsc::channel(1024);
-                for i in 0..size {
-                    let pk = insecure_signer(i as u64).me();
-                    let (sender, mut receiver) = network.register(pk.clone());
-                    agents.insert(pk, sender);
-                    let agent_sender = seen_sender.clone();
-                    executor.spawn(async move {
-                        for _ in 0..size {
-                            receiver.recv().await.unwrap();
-                        }
-                        agent_sender.send(i).await.unwrap();
-
-                        // Exiting early here tests the case where the recipient end of an agent is dropped
-                    });
-                }
-
-                // Randomly link agents
-                let only_inbound = insecure_signer(0).me();
-                for agent in agents.keys() {
-                    if agent == &only_inbound {
-                        // Test that we can gracefully handle missing links
-                        continue;
+            // Register agents
+            let mut agents = HashMap::new();
+            let (seen_sender, mut seen_receiver) = mpsc::channel(1024);
+            for i in 0..size {
+                let pk = insecure_signer(i as u64).me();
+                let (sender, mut receiver) = network.register(pk.clone());
+                agents.insert(pk, sender);
+                let agent_sender = seen_sender.clone();
+                context.spawn(async move {
+                    for _ in 0..size {
+                        receiver.recv().await.unwrap();
                     }
-                    for other in agents.keys() {
-                        let result = network.link(
-                            agent.clone(),
-                            other.clone(),
-                            network::Link {
-                                latency_mean: 5.0,
-                                latency_stddev: 2.5,
-                                success_rate: 0.75,
-                                capacity: 1,
-                            },
-                        );
-                        if agent == other {
-                            assert!(matches!(result, Err(Error::LinkingSelf)));
-                        } else {
-                            assert!(result.is_ok());
-                        }
-                    }
-                }
+                    agent_sender.send(i).await.unwrap();
 
-                // Send messages
-                executor.spawn({
-                    let mut executor = executor.clone();
-                    async move {
-                        let mut keys = agents.keys().collect::<Vec<_>>();
-                        keys.sort();
-                        loop {
-                            let index = executor.gen_range(0..keys.len());
-                            let sender = keys[index];
-                            let msg = format!("hello from {}", hex(sender));
-                            let msg = Bytes::from(msg);
-                            let message_sender = agents.get(sender).unwrap().clone();
-                            let sent = message_sender
-                                .send(Recipients::All, msg.clone(), false)
-                                .await
-                                .unwrap();
-                            if sender == &only_inbound {
-                                assert_eq!(sent.len(), 0);
-                            } else {
-                                assert_eq!(sent.len(), keys.len() - 1);
-                            }
-                        }
-                    }
+                    // Exiting early here tests the case where the recipient end of an agent is dropped
                 });
-
-                // Start network
-                executor.spawn(network.run());
-
-                // Wait for all recipients
-                let mut results = Vec::new();
-                for _ in 0..size {
-                    results.push(seen_receiver.recv().await.unwrap());
-                }
-                results
             }
+
+            // Randomly link agents
+            let only_inbound = insecure_signer(0).me();
+            for agent in agents.keys() {
+                if agent == &only_inbound {
+                    // Test that we can gracefully handle missing links
+                    continue;
+                }
+                for other in agents.keys() {
+                    let result = network.link(
+                        agent.clone(),
+                        other.clone(),
+                        network::Link {
+                            latency_mean: 5.0,
+                            latency_stddev: 2.5,
+                            success_rate: 0.75,
+                            capacity: 1,
+                        },
+                    );
+                    if agent == other {
+                        assert!(matches!(result, Err(Error::LinkingSelf)));
+                    } else {
+                        assert!(result.is_ok());
+                    }
+                }
+            }
+
+            // Send messages
+            context.spawn({
+                let mut context = context.clone();
+                async move {
+                    // Sort agents for deterministic output
+                    let mut keys = agents.keys().collect::<Vec<_>>();
+                    keys.sort();
+
+                    // Send messages
+                    loop {
+                        let index = context.gen_range(0..keys.len());
+                        let sender = keys[index];
+                        let msg = format!("hello from {}", hex(sender));
+                        let msg = Bytes::from(msg);
+                        let message_sender = agents.get(sender).unwrap().clone();
+                        let sent = message_sender
+                            .send(Recipients::All, msg.clone(), false)
+                            .await
+                            .unwrap();
+                        if sender == &only_inbound {
+                            assert_eq!(sent.len(), 0);
+                        } else {
+                            assert_eq!(sent.len(), keys.len() - 1);
+                        }
+                    }
+                }
+            });
+
+            // Start network
+            context.spawn(network.run());
+
+            // Wait for all recipients
+            let mut results = Vec::new();
+            for _ in 0..size {
+                results.push(seen_receiver.recv().await.unwrap());
+            }
+            results
         })
     }
 
@@ -156,44 +156,41 @@ mod tests {
 
     #[test]
     fn test_invalid_message() {
-        let executor = Deterministic::new(0);
-        executor.run({
-            let mut executor = executor.clone();
-            async move {
-                // Create simulated network
-                let mut network = network::Network::new(
-                    executor.clone(),
-                    network::Config {
-                        max_message_size: 1024 * 1024,
-                    },
-                );
+        let (runner, mut context) = Executor::init(0);
+        runner.start(async move {
+            // Create simulated network
+            let mut network = network::Network::new(
+                context.clone(),
+                network::Config {
+                    max_message_size: 1024 * 1024,
+                },
+            );
 
-                // Register agents
-                let mut agents = HashMap::new();
-                for i in 0..10 {
-                    let pk = insecure_signer(i as u64).me();
-                    let (sender, _) = network.register(pk.clone());
-                    agents.insert(pk, sender);
-                }
-
-                // Start network
-                executor.spawn(network.run());
-
-                // Send invalid message
-                let keys = agents.keys().collect::<Vec<_>>();
-                let index = executor.gen_range(0..keys.len());
-                let sender = keys[index];
-                let message_sender = agents.get(sender).unwrap().clone();
-                let mut msg = vec![0u8; 1024 * 1024 + 1];
-                executor.fill(&mut msg[..]);
-                let result = message_sender
-                    .send(Recipients::All, msg.into(), false)
-                    .await
-                    .unwrap_err();
-
-                // Confirm error is correct
-                assert!(matches!(result, Error::MessageTooLarge(_)));
+            // Register agents
+            let mut agents = HashMap::new();
+            for i in 0..10 {
+                let pk = insecure_signer(i as u64).me();
+                let (sender, _) = network.register(pk.clone());
+                agents.insert(pk, sender);
             }
+
+            // Start network
+            context.spawn(network.run());
+
+            // Send invalid message
+            let keys = agents.keys().collect::<Vec<_>>();
+            let index = context.gen_range(0..keys.len());
+            let sender = keys[index];
+            let message_sender = agents.get(sender).unwrap().clone();
+            let mut msg = vec![0u8; 1024 * 1024 + 1];
+            context.fill(&mut msg[..]);
+            let result = message_sender
+                .send(Recipients::All, msg.into(), false)
+                .await
+                .unwrap_err();
+
+            // Confirm error is correct
+            assert!(matches!(result, Error::MessageTooLarge(_)));
         });
     }
 }
