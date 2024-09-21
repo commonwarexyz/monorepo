ea71eb1b5386923d275af091893e887a7030c3d0
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-21 12:46:12 -0700

    add select loop tests

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -142,6 +142,8 @@ macro_rules! select {
 mod tests {
     use super::*;
     use crate::tokio::Config;
+    use core::panic;
+    use futures::{channel::mpsc, SinkExt, StreamExt};
     use std::panic::{catch_unwind, AssertUnwindSafe};
     use std::sync::Mutex;
     use utils::reschedule;
@@ -238,6 +240,50 @@ mod tests {
         });
     }
 
+    /// Ensure future fusing works as expected.
+    fn test_select_loop(runner: impl Runner, context: impl Clock) {
+        runner.start(async move {
+            // Should hit timeout
+            let (mut sender, mut receiver) = mpsc::unbounded();
+            for _ in 0..2 {
+                select! {
+                    v = receiver.next() => {
+                        panic!("unexpected value: {:?}", v);
+                    },
+                    _timeout = context.sleep(Duration::from_millis(100)) => {
+                        continue;
+                    },
+                };
+            }
+
+            // Populate channel
+            sender.send(0).await.unwrap();
+            sender.send(1).await.unwrap();
+
+            // Prefer not reading channel without losing messages
+            select! {
+                _timeout = async {} => {
+                    // Skip reading from channel eventhough populated
+                },
+                v = receiver.next() => {
+                    panic!("unexpected value: {:?}", v);
+                },
+            };
+
+            // Process messages
+            for i in 0..2 {
+                select! {
+                    _timeout = context.sleep(Duration::from_millis(100)) => {
+                        panic!("timeout");
+                    },
+                    v = receiver.next() => {
+                        assert_eq!(v.unwrap(), i);
+                    },
+                };
+            }
+        });
+    }
+
     #[test]
     fn test_deterministic_future() {
         let (runner, _, _) = deterministic::Executor::init(1, Duration::from_millis(1));
@@ -286,6 +332,12 @@ mod tests {
     fn test_deterministic_select() {
         let (runner, context, _) = deterministic::Executor::init(1, Duration::from_millis(1));
         test_select(runner, context);
+    }
+
+    #[test]
+    fn test_deterministic_select_loop() {
+        let (runner, context, _) = deterministic::Executor::init(1, Duration::from_millis(1));
+        test_select_loop(runner, context);
     }
 
     #[test]
@@ -342,5 +394,12 @@ mod tests {
         let cfg = Config::default();
         let (runner, context) = tokio::Executor::init(cfg);
         test_select(runner, context);
+    }
+
+    #[test]
+    fn test_tokio_select_loop() {
+        let cfg = Config::default();
+        let (runner, context) = tokio::Executor::init(cfg);
+        test_select_loop(runner, context);
     }
 }
