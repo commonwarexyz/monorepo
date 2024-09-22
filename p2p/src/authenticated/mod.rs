d90c99630a1c1f9645cbeb196cf5406b686568ab
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-22 16:12:31 -0700

    tests passing

--- p2p/src/authenticated/mod.rs
+++ p2p/src/authenticated/mod.rs
@@ -336,7 +336,7 @@ mod tests {
                 // Register basic application
                 let (mut sender, mut receiver) = network.register(
                     0,
-                    Quota::per_second(NonZeroU32::new(2).unwrap()),
+                    Quota::per_second(NonZeroU32::new(1).unwrap()), // Ensure we hit the rate limit
                     1_024,
                     128,
                     None,
