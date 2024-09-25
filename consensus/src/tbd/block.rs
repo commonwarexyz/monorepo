914d09b3798e2f2535ed9f6cc4918d3bbe5979ec
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-25 12:35:35 -0700

    cleaning up block structure

--- consensus/src/tbd/block.rs
+++ consensus/src/tbd/block.rs
@@ -1,16 +1,27 @@
-pub struct Block {
-    Timestamp: u64,
+use bytes::Bytes;
 
-    Height: u64,
-    Parent: BlockHash,
-    Epoch: u64,
-    View: u64,
-    // TODO: add partial signatures
-    Payload: Bytes, // TODO: use function that returns hash for constructing block header
+type Hash = [u8; 32];
+
+pub struct Signer {
+    pub public_key: Bytes,
+    pub signature: Bytes,
+}
+
+pub struct Block {
+    pub timestamp: u64, // in ms
+
+    pub height: u64,
+    pub parent: Hash,
+
+    pub epoch: u64,
+    pub view: u64,
+    pub partials: Vec<Signer>,
+
+    pub payload: Bytes, // TODO: use function that returns hash for constructing block header
 }
 
 impl Block {
-    pub fn hash(&self) -> BlockHash {
+    pub fn hash(&self) -> Hash {
         // H(timestamp, H(parent, epoch, view, height), payload_hash)
     }
 }
