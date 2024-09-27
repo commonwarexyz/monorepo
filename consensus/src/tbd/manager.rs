e11c6603e6999a6aaa2ac794e7e2e93d806fcbfe
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-27 08:46:13 -0700

    handle vote implementation

--- consensus/src/tbd/manager.rs
+++ consensus/src/tbd/manager.rs
@@ -1,4 +1,4 @@
-use super::config::Config;
+use super::{config::Config, wire};
 use bytes::Bytes;
 use commonware_cryptography::{PublicKey, Signature};
 use commonware_runtime::Clock;
@@ -10,7 +10,7 @@ struct Info {
     leader_deadline: SystemTime,
     advance_deadline: SystemTime,
 
-    proposal: Option<Bytes>,
+    proposal: Option<(Bytes, wire::Proposal)>,
     // other_proposals: HashMap<Bytes, wire::Proposal>, // Faults (TODO: this is an OOM if we aren't careful -> should just store info needed to post a complaint)
     proposal_votes: HashMap<PublicKey, Signature>,
     // other_votes: HashMap<PublicKey, Signature>, // Faults
@@ -24,6 +24,28 @@ struct Info {
 
     finalizes: HashMap<PublicKey, Signature>,
     finalization: Option<Bytes>,
+}
+
+impl Info {
+    fn new(leader_deadline: SystemTime, advance_deadline: SystemTime) -> Self {
+        Self {
+            leader_deadline,
+            advance_deadline,
+
+            proposal: None,
+            proposal_votes: HashMap::new(),
+            proposal_notarization: None,
+
+            null_votes: HashMap::new(),
+            null_notarization: None,
+
+            seeds: HashMap::new(),
+            beacon: None,
+
+            finalizes: HashMap::new(),
+            finalization: None,
+        }
+    }
 }
 
 pub struct Manager<E: Clock> {
@@ -46,26 +68,52 @@ impl<E: Clock> Manager<E> {
         let leader_deadline = now + self.cfg.leader_timeout;
         let advance_deadline = now + self.cfg.advance_timeout;
         let view = (epoch, view);
-        self.rounds.insert(
-            view,
-            Info {
-                leader_deadline,
-                advance_deadline,
-
-                proposal: None,
-                proposal_votes: HashMap::new(),
-                proposal_notarization: None,
-
-                null_votes: HashMap::new(),
-                null_notarization: None,
-
-                seeds: HashMap::new(),
-                beacon: None,
-
-                finalizes: HashMap::new(),
-                finalization: None,
-            },
-        );
+        self.rounds
+            .insert(view, Info::new(leader_deadline, advance_deadline));
     }
-    pub fn vote(&mut self, epoch: u64, view: u64, public_key: PublicKey, signature: Signature) {}
+
+    pub fn vote(
+        &mut self,
+        epoch: u64,
+        view: u64,
+        block: Bytes, // hash
+        public_key: PublicKey,
+        signature: Signature,
+    ) {
+        // Get view info
+        let view = (epoch, view);
+        let info = match self.rounds.get_mut(&view) {
+            Some(info) => info,
+            None => {
+                // If we have yet to start a given view, ignore the vote.
+                //
+                // If we tried to track arbitrary future votes, it would
+                // be trivial for a peer to OOM us.
+                //
+                // TODO: consider being a bit more flexible here and allow
+                // vote collection for next view.
+                return;
+            }
+        };
+
+        // Get proposal info
+        let (hash, proposal) = match &info.proposal {
+            Some(proposal) => proposal,
+            None => {
+                // If we have yet to receive a proposal, ignore the vote.
+                return;
+            }
+        };
+
+        // If vote is for the proposal, store it.
+        if hash == &block {
+            info.proposal_votes.insert(public_key, signature);
+        } else if block.len() == 0 {
+            info.null_votes.insert(public_key, signature);
+        } else {
+            // This vote is for a different proposal than what we have,
+            // this means either the proposer sent conflicting blocks
+            // or the voter is malicious.
+        }
+    }
 }
