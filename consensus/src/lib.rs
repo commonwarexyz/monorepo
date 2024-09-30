c773834d5d465af5498a1c88a9d7ab4203cb9085
Patrick O'Grady <me@patrickogrady.xyz>
2024-09-30 09:51:36 -0700

    add some test ideas

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -3,3 +3,10 @@
 pub mod tbd;
 
 // TODO: add simulated dialect for applications to test their execution environments under
+
+// TODO: tests
+// - sync from scratch
+// - halt (50% shutdown) and recover
+// - 33% shutdown and recover
+// - full shutdown and recover (no safety failure from voting incorrectly)
+// - 33% double-voting
