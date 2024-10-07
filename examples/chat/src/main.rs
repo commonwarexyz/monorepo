8c30d7afb33f48e614d2a8815ae7862a1307e07c
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-06 20:33:00 -0400

    [Docs] Add synchronization assumption (#106)

--- examples/chat/src/main.rs
+++ examples/chat/src/main.rs
@@ -1,12 +1,20 @@
-//! Send encrypted messages to a group of friends using [commonware-cryptography](https://crates.io/crates/commonware-cryptography)
-//! and [commonware-p2p](https://crates.io/crates/commonware-p2p).
+//! Send encrypted messages to a group of friends using [commonware-cryptography::ed25519](https://docs.rs/commonware-cryptography/latest/commonware_cryptography/ed25519/index.html)
+//! and [commonware-p2p::authenticated](https://docs.rs/commonware-p2p/latest/commonware_p2p/authenticated/index.html).
 //!
 //! # Offline Friends
 //!
 //! `commonware-chat` only sends messages to connected friends. If a friend is offline at the time a message is sent,
-//! `commonware-p2p` will drop the message. You can confirm you are connected to all your friends by checking the value
-//! of `p2p_connections` in the "Metrics Panel" in the right corner of the window. This metric should be equal to
-//! `count(friends)- 1` (you don't connect to yourself).
+//! `commonware-p2p::authenticated` will drop the message. You can confirm you are connected to all your friends by
+//! checking the value of `p2p_connections` in the "Metrics Panel" in the right corner of the window. This metric should
+//! be equal to `count(friends)- 1` (you don't connect to yourself).
+//!
+//! # Synchonized Friends
+//!
+//! `commonware-p2p::authenticated` requires all friends to have the same set of friends for friend discovery to work
+//! correctly. If you do not synchronize friends, you may be able to form connections between specific friends but may
+//! not be able to form connections with all friends. You can learn more about why
+//! this is [here](https://docs.rs/commonware-p2p/latest/commonware_p2p/authenticated/index.html#discovery). Other
+//! dialects of `commonware-p2p` may not have this requirement.
 //!
 //! # Usage (4 Friends)
 //!
