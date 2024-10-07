723218d4553f6ffe12578134127f08e4f6b01392
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-06 19:05:57 -0700

    remove tokio tracing

--- p2p/src/authenticated/mod.rs
+++ p2p/src/authenticated/mod.rs
@@ -506,7 +506,6 @@ mod tests {
 
     #[test]
     fn test_tokio_connectivity() {
-        tracing_subscriber::fmt().with_test_writer().init();
         let cfg = tokio::Config::default();
         let (executor, runtime) = tokio::Executor::init(cfg.clone());
         executor.start(async move {
