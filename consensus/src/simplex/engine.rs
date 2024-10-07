8c5db2662b3404bc7521da26e366daf423c4adee
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-07 13:15:31 -0700

    cleanup engine handling with voter

--- consensus/src/simplex/engine.rs
+++ consensus/src/simplex/engine.rs
@@ -49,72 +49,27 @@ impl<E: Clock + Rng, C: Scheme, A: Application, S: Sender, R: Receiver> Engine<E
         }
     }
 
-    // TODO: add shared logic for attempting all view messages after each is received
-    async fn handle_proposal(&mut self, proposal: wire::Proposal) {
-        // Store the proposal
-        let proposal_view = proposal.view;
-        self.voter.proposal(proposal);
-
+    async fn send_view_messages(&mut self, view: u64) {
         // Attempt to vote
-        let vote = match self.voter.construct_vote(proposal_view) {
-            Some(vote) => vote,
-            None => {
-                return;
-            }
+        if let Some(vote) = self.voter.construct_vote(view) {
+            // Broadcast the vote
+            let msg = wire::Message {
+                payload: Some(wire::message::Payload::Vote(vote.clone())),
+            };
+            let msg = msg.encode_to_vec();
+            self.voter_network
+                .0
+                .send(Recipients::All, msg.into(), true)
+                .await
+                .unwrap();
+
+            // Handle the vote
+            self.voter.vote(vote);
         };
-
-        // Broadcast the vote
-        let msg = wire::Message {
-            payload: Some(wire::message::Payload::Vote(vote.clone())),
-        };
-        let msg = msg.encode_to_vec();
-        self.voter_network
-            .0
-            .send(Recipients::All, msg.into(), true)
-            .await
-            .unwrap();
-
-        // Handle the vote
-        self.handle_vote(vote).await;
-
-        // TODO: attempt to build notarization, finalize, and finalization (may be blocked on this)
-    }
-
-    async fn handle_vote(&mut self, vote: wire::Vote) {
-        // Store the vote
-        let vote_view = vote.view;
-        self.voter.vote(vote);
 
         // Attempt to notarize
-        let notarization = match self.voter.construct_notarization(vote_view) {
-            Some(notarization) => notarization,
-            None => {
-                return;
-            }
-        };
-
-        // Broadcast the notarization
-        let msg = wire::Message {
-            payload: Some(wire::message::Payload::Notarization(notarization.clone())),
-        };
-        let msg = msg.encode_to_vec();
-        self.voter_network
-            .0
-            .send(Recipients::All, msg.into(), true)
-            .await
-            .unwrap();
-
-        // Handle the notarization
-        self.handle_notarization(notarization).await;
-    }
-
-    async fn handle_notarization(&mut self, notarization: wire::Notarization) {
-        // Store the notarization
-        let notarization_view = notarization.view;
-        self.voter.notarization(notarization);
-
-        // Attempt to notarize
-        if let Some(notarization) = self.voter.construct_notarization(notarization_view) {
+        if let Some(notarization) = self.voter.construct_notarization(view) {
+            // Broadcast the notarization
             let msg = wire::Message {
                 payload: Some(wire::message::Payload::Notarization(notarization.clone())),
             };
@@ -124,112 +79,97 @@ impl<E: Clock + Rng, C: Scheme, A: Application, S: Sender, R: Receiver> Engine<E
                 .send(Recipients::All, msg.into(), true)
                 .await
                 .unwrap();
+
+            // Handle the notarization
+            self.voter.notarization(notarization);
         };
 
         // Attempt to finalize
-        let finalize = match self.voter.construct_finalize(notarization_view) {
-            Some(finalize) => finalize,
-            None => {
-                return;
-            }
+        if let Some(finalize) = self.voter.construct_finalize(view) {
+            // Broadcast the finalize
+            let msg = wire::Message {
+                payload: Some(wire::message::Payload::Finalize(finalize.clone())),
+            };
+            let msg = msg.encode_to_vec();
+            self.voter_network
+                .0
+                .send(Recipients::All, msg.into(), true)
+                .await
+                .unwrap();
+
+            // Handle the finalize
+            self.voter.finalize(finalize);
         };
-        let msg = wire::Message {
-            payload: Some(wire::message::Payload::Finalize(finalize.clone())),
+
+        // Attempt to finalization
+        if let Some(finalization) = self.voter.construct_finalization(view) {
+            // Broadcast the finalization
+            let msg = wire::Message {
+                payload: Some(wire::message::Payload::Finalization(finalization.clone())),
+            };
+            let msg = msg.encode_to_vec();
+            self.voter_network
+                .0
+                .send(Recipients::All, msg.into(), true)
+                .await
+                .unwrap();
+
+            // Handle the finalization
+            self.voter.finalization(finalization);
         };
-        let msg = msg.encode_to_vec();
-        self.voter_network
-            .0
-            .send(Recipients::All, msg.into(), true)
-            .await
-            .unwrap();
-
-        // Handle the finalize
-        self.handle_finalize(finalize).await;
-    }
-
-    async fn handle_finalize(&mut self, finalize: wire::Finalize) {
-        // Store the finalize
-        let finalize_view = finalize.view;
-        self.voter.finalize(finalize);
-
-        // Broadcast the finalization (ours may be better than what we received)
-        let finalization = match self.voter.construct_finalization(finalize_view) {
-            Some(finalization) => finalization,
-            None => {
-                return;
-            }
-        };
-        let msg = wire::Message {
-            payload: Some(wire::message::Payload::Finalization(finalization.clone())),
-        };
-        let msg = msg.encode_to_vec();
-        self.voter_network
-            .0
-            .send(Recipients::All, msg.into(), true)
-            .await
-            .unwrap();
-
-        // Handle the finalization
-        self.handle_finalization(finalization).await;
-    }
-
-    async fn handle_finalization(&mut self, finalization: wire::Finalization) {
-        // Store the finalization
-        self.voter.finalization(finalization);
-    }
-
-    async fn handle_timeout(&mut self) {
-        // Trigger the timeout
-        let vote = self.voter.timeout();
-
-        // Broadcast the vote
-        let msg = wire::Message {
-            payload: Some(wire::message::Payload::Vote(vote.clone())),
-        };
-        let msg = msg.encode_to_vec();
-        self.voter_network
-            .0
-            .send(Recipients::All, msg.into(), true)
-            .await
-            .unwrap();
-
-        // Handle the vote
-        self.handle_vote(vote).await;
     }
 
     pub async fn run(mut self) -> Result<(), Error> {
         // Process messages
         loop {
             // Attempt to propose a block
-            if let Some(proposal) = self.store.propose() {
+            if let Some(proposal) = self.voter.propose() {
                 // Broadcast the proposal
                 let msg = wire::Message {
                     payload: Some(wire::message::Payload::Proposal(proposal.clone())),
                 };
                 let msg = msg.encode_to_vec();
-                self.sender
+                self.voter_network
+                    .0
                     .send(Recipients::All, msg.into(), true)
                     .await
                     .unwrap();
 
                 // Handle the proposal
-                self.handle_proposal(proposal).await;
+                let proposal_view = proposal.view;
+                self.voter.proposal(proposal);
+                self.send_view_messages(proposal_view).await;
             }
 
             // Wait for a timeout to fire or for a message to arrive
-            //
-            // TODO: clean this up (done this way to prevent multiple mutable borrows)
-            let null_timeout = self.store.timeout_deadline();
+            let null_timeout = self.voter.timeout_deadline();
             let result = select! {
                 _timeout = self.runtime.sleep_until(null_timeout) => {
                     None
                 },
-                result = self.receiver.recv() => {
+                result = self.voter_network.1.recv() => {
                     Some(result.map_err(|_| Error::NetworkClosed)?)
                 },
             };
             if result.is_none() {
-                self.handle_timeout().await;
+                // Trigger the timeout
+                let vote = self.voter.timeout();
+
+                // Broadcast the vote
+                let msg = wire::Message {
+                    payload: Some(wire::message::Payload::Vote(vote.clone())),
+                };
+                let msg = msg.encode_to_vec();
+                self.voter_network
+                    .0
+                    .send(Recipients::All, msg.into(), true)
+                    .await
+                    .unwrap();
+
+                // Handle the vote
+                let vote_view = vote.view;
+                self.voter.vote(vote);
+                self.send_view_messages(vote_view).await;
                 continue;
             }
             let (sender, msg) = result.unwrap();
@@ -254,28 +194,36 @@ impl<E: Clock + Rng, C: Scheme, A: Application, S: Sender, R: Receiver> Engine<E
             //
             // While syncing any missing blocks, continue to listen to messages at
             // tip (immediately vote dummy when entering round).
+            let view;
             match payload {
-                // TODO: verify signature is from sender
                 wire::message::Payload::Proposal(proposal) => {
-                    self.handle_proposal(proposal).await;
+                    view = proposal.view;
+                    self.voter.proposal(proposal);
                 }
                 wire::message::Payload::Vote(vote) => {
-                    self.handle_vote(vote).await;
+                    view = vote.view;
+                    self.voter.vote(vote);
                 }
                 wire::message::Payload::Notarization(notarization) => {
-                    self.handle_notarization(notarization).await;
+                    view = notarization.view;
+                    self.voter.notarization(notarization);
                 }
                 wire::message::Payload::Finalize(finalize) => {
-                    self.handle_finalize(finalize).await;
+                    view = finalize.view;
+                    self.voter.finalize(finalize);
                 }
                 wire::message::Payload::Finalization(finalization) => {
-                    self.handle_finalization(finalization).await;
+                    view = finalization.view;
+                    self.voter.finalization(finalization);
                 }
                 _ => {
                     debug!(sender = hex(&sender), "unexpected message");
                     continue;
                 }
             };
+
+            // Attempt to send any new view messages
+            self.send_view_messages(view).await;
         }
     }
 }
