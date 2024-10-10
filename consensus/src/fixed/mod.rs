44e1142d2f36e1ff3992505e1509e5889fc1e573
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-10 09:01:59 -0700

    test compiles

--- consensus/src/fixed/mod.rs
+++ consensus/src/fixed/mod.rs
@@ -341,116 +341,168 @@ mod tests {
         });
     }
 
-    // #[test]
-    // fn test_catchup() {
-    //     // Configure logging
-    //     tracing_subscriber::fmt()
-    //         .with_max_level(Level::DEBUG)
-    //         .with_line_number(true)
-    //         .init();
+    #[test]
+    fn test_catchup() {
+        // Configure logging
+        tracing_subscriber::fmt()
+            .with_max_level(Level::DEBUG)
+            .with_line_number(true)
+            .init();
 
-    //     // Create runtime
-    //     let n = 5;
-    //     let (executor, runtime, _) = Executor::default();
-    //     executor.start(async move {
-    //         // Create simulated network
-    //         let (network, mut oracle) = Network::new(
-    //             runtime.clone(),
-    //             Config {
-    //                 registry: Arc::new(Mutex::new(Registry::default())),
-    //             },
-    //         );
+        // Create runtime
+        let n = 5;
+        let (executor, runtime, _) = Executor::default();
+        executor.start(async move {
+            // Create simulated network
+            let (network, mut oracle) = Network::new(
+                runtime.clone(),
+                Config {
+                    registry: Arc::new(Mutex::new(Registry::default())),
+                },
+            );
 
-    //         // Start network
-    //         runtime.spawn("network", network.run());
+            // Start network
+            runtime.spawn("network", network.run());
 
-    //         // Register participants
-    //         let mut schemes = Vec::new();
-    //         let mut validators = Vec::new();
-    //         for i in 0..n {
-    //             let scheme = Ed25519::from_seed(i as u64);
-    //             let pk = scheme.public_key();
-    //             schemes.push(scheme);
-    //             validators.push(pk);
-    //         }
-    //         validators.sort();
-    //         let view_validators = BTreeMap::from_iter(vec![(0, validators.clone())]);
+            // Register participants
+            let mut schemes = Vec::new();
+            let mut validators = Vec::new();
+            for i in 0..n {
+                let scheme = Ed25519::from_seed(i as u64);
+                let pk = scheme.public_key();
+                schemes.push(scheme);
+                validators.push(pk);
+            }
+            validators.sort();
+            let view_validators = BTreeMap::from_iter(vec![(0, validators.clone())]);
 
-    //         // Create engines
-    //         let (start_delayed, started_delayed) = oneshot::channel();
-    //         let (done_sender, mut done_receiver) = mpsc::channel(schemes.len());
-    //         for (idx, scheme) in schemes.into_iter().enumerate() {
-    //             // Register on network
-    //             let validator = scheme.public_key();
-    //             let (block_sender, block_receiver) = oracle
-    //                 .register(validator.clone(), 0, 1024 * 1024)
-    //                 .await
-    //                 .unwrap();
-    //             let (vote_sender, vote_receiver) = oracle
-    //                 .register(validator.clone(), 1, 1024 * 1024)
-    //                 .await
-    //                 .unwrap();
+            // Create engines
+            let (done_sender, mut done_receiver) = mpsc::channel(schemes.len());
+            for (idx, scheme) in schemes.into_iter().enumerate() {
+                // Skip first peer
+                if idx == 0 {
+                    continue;
+                }
 
-    //             // Link to all other validators
-    //             for other in validators.iter() {
-    //                 if other == &validator {
-    //                     continue;
-    //                 }
-    //                 oracle
-    //                     .add_link(
-    //                         validator.clone(),
-    //                         other.clone(),
-    //                         Link {
-    //                             latency_mean: 10.0,
-    //                             latency_stddev: 1.0,
-    //                             success_rate: 1.0,
-    //                         },
-    //                     )
-    //                     .await
-    //                     .unwrap();
-    //             }
+                // Register on network
+                let validator = scheme.public_key();
+                let (block_sender, block_receiver) = oracle
+                    .register(validator.clone(), 0, 1024 * 1024)
+                    .await
+                    .unwrap();
+                let (vote_sender, vote_receiver) = oracle
+                    .register(validator.clone(), 1, 1024 * 1024)
+                    .await
+                    .unwrap();
 
-    //             // Start engine
-    //             let cfg = config::Config {
-    //                 crypto: scheme,
-    //                 application: MockApplication {
-    //                     participant: validator,
-    //                     verified: HashMap::new(),
-    //                     finalized: HashMap::new(),
-    //                     done_height: 100,
-    //                     done: done_sender.clone(),
-    //                 },
-    //                 registry: Arc::new(Mutex::new(Registry::default())),
-    //                 namespace: Bytes::from("consensus"),
-    //                 leader_timeout: Duration::from_secs(1),
-    //                 notarization_timeout: Duration::from_secs(1),
-    //                 null_vote_retry: Duration::from_secs(1),
-    //                 fetch_timeout: Duration::from_secs(1),
-    //                 validators: view_validators.clone(),
-    //             };
-    //             let engine = Engine::new(runtime.clone(), cfg);
-    //             runtime.spawn("engine", async move {
-    //                 if idx == 0 {
-    //                     started_delayed.await.unwrap();
-    //                 }
-    //                 // TODO: enable messages to account
-    //                 engine
-    //                     .run((block_sender, block_receiver), (vote_sender, vote_receiver))
-    //                     .await;
-    //             });
-    //         }
+                // Link to all other validators
+                for other in validators.iter() {
+                    if other == &validator {
+                        continue;
+                    }
+                    oracle
+                        .add_link(
+                            validator.clone(),
+                            other.clone(),
+                            Link {
+                                latency_mean: 10.0,
+                                latency_stddev: 1.0,
+                                success_rate: 1.0,
+                            },
+                        )
+                        .await
+                        .unwrap();
+                }
 
-    //         // Run network
-    //         runtime.spawn("network", network.run());
+                // Start engine
+                let cfg = config::Config {
+                    crypto: scheme,
+                    application: MockApplication {
+                        participant: validator,
+                        verified: HashMap::new(),
+                        finalized: HashMap::new(),
+                        done_height: 100,
+                        done: done_sender.clone(),
+                    },
+                    registry: Arc::new(Mutex::new(Registry::default())),
+                    namespace: Bytes::from("consensus"),
+                    leader_timeout: Duration::from_secs(1),
+                    notarization_timeout: Duration::from_secs(1),
+                    null_vote_retry: Duration::from_secs(1),
+                    fetch_timeout: Duration::from_secs(1),
+                    validators: view_validators.clone(),
+                };
+                let engine = Engine::new(runtime.clone(), cfg);
+                runtime.spawn("engine", async move {
+                    engine
+                        .run((block_sender, block_receiver), (vote_sender, vote_receiver))
+                        .await;
+                });
+            }
 
-    //         // Wait for all engines to finish
-    //         for _ in 0..(n - 1) {
-    //             done_receiver.next().await.unwrap();
-    //         }
+            // Wait for all engines to finish
+            for _ in 0..(n - 1) {
+                done_receiver.next().await.unwrap();
+            }
 
-    //         // Start delayed engine
-    //         start_delayed.send(()).unwrap();
-    //         done_receiver.next().await.unwrap();
-    //     });
-    // }
+            // Start engine for first peer
+            let scheme = Ed25519::from_seed(0);
+            let validator = scheme.public_key();
+            let (block_sender, block_receiver) = oracle
+                .register(validator.clone(), 0, 1024 * 1024)
+                .await
+                .unwrap();
+            let (vote_sender, vote_receiver) = oracle
+                .register(validator.clone(), 1, 1024 * 1024)
+                .await
+                .unwrap();
+
+            // Link to all other validators
+            for other in validators.iter() {
+                if other == &validator {
+                    continue;
+                }
+                oracle
+                    .add_link(
+                        validator.clone(),
+                        other.clone(),
+                        Link {
+                            latency_mean: 10.0,
+                            latency_stddev: 1.0,
+                            success_rate: 1.0,
+                        },
+                    )
+                    .await
+                    .unwrap();
+            }
+
+            // Start engine
+            let cfg = config::Config {
+                crypto: scheme,
+                application: MockApplication {
+                    participant: validator,
+                    verified: HashMap::new(),
+                    finalized: HashMap::new(),
+                    done_height: 100,
+                    done: done_sender.clone(),
+                },
+                registry: Arc::new(Mutex::new(Registry::default())),
+                namespace: Bytes::from("consensus"),
+                leader_timeout: Duration::from_secs(1),
+                notarization_timeout: Duration::from_secs(1),
+                null_vote_retry: Duration::from_secs(1),
+                fetch_timeout: Duration::from_secs(1),
+                validators: view_validators.clone(),
+            };
+            let engine = Engine::new(runtime.clone(), cfg);
+            runtime.spawn("engine", async move {
+                engine
+                    .run((block_sender, block_receiver), (vote_sender, vote_receiver))
+                    .await;
+            });
+
+            // Wait for new engine to finish
+            done_receiver.next().await.unwrap();
+        });
+    }
 }
