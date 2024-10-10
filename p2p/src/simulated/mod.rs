bff7b8181ed5d2dcfbb300b6d299f89b3ac2c374
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-10 07:54:07 -0700

    improve mod.rs

--- p2p/src/simulated/mod.rs
+++ p2p/src/simulated/mod.rs
@@ -1,9 +1,13 @@
-//! Simulate messaging between arbitrary peers with configurable performance (i.e. drops, latency, corruption, etc.).
+//! Simulate networking between peers with configurable link behavior (i.e. drops, latency, corruption, etc.).
 //!
-//! To support advanced testing, dynamic addition/removal of both peers and the links between them is supported. This can
-//! be used to simulate network partitions.
+//! Both peer and link modification can be performed dynamically over the lifetime of the simulated network. This
+//! can be used to mimic transient network partitions, offline nodes (that later connect), and/or degrading link
+//! quality.
 //!
-//! To make the simulation deterministic, employ `commonware-runtime`'s `deterministic::Executor` (with a given seed).
+//! # Determinism
+//!
+//! `commonware-p2p::simulated` can be run deterministically when paired with `commonware-runtime::deterministic`.
+//! This makes it possible to reproduce an arbitrary order of delivered/dropped messages with a given seed.
 //!
 //! # Example
 //!
