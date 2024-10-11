814bd1f44c606f92286f7c23e16d89a50dbfe284
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-11 12:04:54 -0700

    spike on log macro

---
+++ macros/README.md
@@ -0,0 +1,9 @@
+# commonware-macros
+
+[![Crates.io](https://img.shields.io/crates/v/commonware-macros.svg)](https://crates.io/crates/commonware-macros)
+
+TBD
+
+## Status 
+
+`commonware-macros` is **ALPHA** software and is not yet recommended for production use. Developers should expect breaking changes and occasional instability.
---
+++ macros/src/lib.rs
@@ -0,0 +1,62 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn, LitStr};
+
+#[proc_macro_attribute]
+pub fn traced_test(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(item as ItemFn);
+
+    // Parse the attribute argument for log level
+    let log_level = if attr.is_empty() {
+        // Default log level is DEBUG
+        quote! { tracing::Level::DEBUG }
+    } else {
+        // Parse the attribute as a string literal
+        let level_str = parse_macro_input!(attr as LitStr);
+        let level_ident = level_str.value().to_uppercase();
+        match level_ident.as_str() {
+            "TRACE" => quote! { tracing::Level::TRACE },
+            "DEBUG" => quote! { tracing::Level::DEBUG },
+            "INFO" => quote! { tracing::Level::INFO },
+            "WARN" => quote! { tracing::Level::WARN },
+            "ERROR" => quote! { tracing::Level::ERROR },
+            _ => {
+                // Return a compile error for invalid log levels
+                return syn::Error::new_spanned(
+                    level_str,
+                    "Invalid log level. Expected one of: TRACE, DEBUG, INFO, WARN, ERROR.",
+                )
+                .to_compile_error()
+                .into();
+            }
+        }
+    };
+
+    // Extract function components
+    let attrs = input.attrs;
+    let vis = input.vis;
+    let sig = input.sig;
+    let block = input.block;
+
+    // Generate output tokens
+    let expanded = quote! {
+        #[test]
+        #(#attrs)*
+        #vis #sig {
+            // Create a subscriber and dispatcher with the specified log level
+            let subscriber = tracing_subscriber::fmt()
+                .with_test_writer()
+                .with_max_level(#log_level)
+                .with_line_number(true)
+                .finish();
+            let dispatcher = tracing::Dispatch::new(subscriber);
+
+            // Set the subcriber for the scope of the test
+            tracing::dispatcher::with_default(&dispatcher, || {
+                #block
+            });
+        }
+    };
+    TokenStream::from(expanded)
+}
