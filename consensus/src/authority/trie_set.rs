69e90aba4dd9abe76feae90070bde339e0ad4351
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-16 14:17:25 -0700

    cleanup code

--- consensus/src/authority/trie_set.rs
+++ consensus/src/authority/trie_set.rs
@@ -14,47 +14,27 @@ impl<K: PartialEq + PartialOrd + Eq + StdHash + Clone> TrieSet<K> {
     /// Prune clears all entries less than a given index.
     pub fn prune(&mut self, min: u64) {
         // Clean container index
-        let keys_to_remove: Vec<u64> = self
-            .index
-            .iter()
-            .filter(|(key, _)| **key < min)
-            .map(|(key, _)| *key)
-            .collect();
-        for key in keys_to_remove {
-            let hash = self.index.remove(&key).unwrap();
-            self.stored.remove(&hash);
-        }
+        self.index.retain(|&key, hash| {
+            if key >= min {
+                return true;
+            }
+            self.stored.remove(hash);
+            false
+        });
 
         // Clean pending index
-        let keys_to_remove: Vec<u64> = self
-            .pending
-            .keys()
-            .cloned()
-            .filter(|key| *key < min)
-            .collect();
-        for key in keys_to_remove {
-            self.pending.remove(&key);
-        }
+        self.pending.retain(|key, _| *key >= min);
     }
 
     pub fn pending(&self, parent: &Hash) -> Vec<K> {
         let mut possible = self.pending.values().flatten().collect::<HashSet<_>>();
         let mut next = parent;
-        loop {
-            if let Some((hash, set)) = self.stored.get(next) {
-                let mut to_remove = Vec::new();
-                for key in &possible {
-                    if set.contains(key) {
-                        to_remove.push(key.clone());
-                    }
-                }
-                for key in to_remove {
-                    possible.remove(&key);
-                }
-                next = hash;
-            } else {
-                break;
-            }
+
+        // Iterate through all ancestors we know about and remove from possible
+        // if already included.
+        while let Some((hash, set)) = self.stored.get(next) {
+            possible.retain(|key| !set.contains(key));
+            next = hash;
         }
 
         // Collect all remaining possible
@@ -63,34 +43,25 @@ impl<K: PartialEq + PartialOrd + Eq + StdHash + Clone> TrieSet<K> {
 
     pub fn discover(&mut self, keys: Vec<(u64, K)>) {
         for (index, key) in keys {
-            let entry = self.pending.entry(index).or_default();
-            entry.insert(key);
+            self.pending.entry(index).or_default().insert(key);
         }
     }
 
     pub fn track(&mut self, item: Hash, index: u64, parent: Hash, keys: Vec<K>) {
-        let mut set = HashSet::new();
-        for key in keys.into_iter() {
-            set.insert(key);
-        }
-        self.stored.insert(item.clone(), (parent, set));
+        let keys = keys.into_iter().collect();
+        self.stored.insert(item.clone(), (parent, keys));
         self.index.insert(index, item);
     }
 
     /// Check all ancestors for an overlapping key in a particular ancestry.
     pub fn check(&self, parent: &Hash, keys: Vec<&K>) -> bool {
         let mut next = parent;
-        loop {
-            if let Some((hash, set)) = self.stored.get(next) {
-                for key in keys.iter() {
-                    if set.contains(*key) {
-                        return true;
-                    }
-                }
-                next = hash;
-            } else {
-                return false;
+        while let Some((hash, set)) = self.stored.get(next) {
+            if keys.iter().any(|key| set.contains(*key)) {
+                return true;
             }
+            next = hash;
         }
+        false
     }
 }
