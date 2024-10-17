bf57dddc1dac1327feefd562e5fbb86346879a50
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-17 13:10:23 -0700

    update hasher

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -28,14 +28,26 @@ type Hash = Bytes; // use fixed size bytes
 
 /// Hasher is provided by the application for hashing.
 ///
+/// In practice, the hasher is not bundled with the application so that
+/// it can be cheaply copied for concurrent hashing.
+///
 /// This is configurable because some hash functions are better suited for
 /// SNARK/STARK proofs than others.
 pub trait Hasher: Clone + Send + 'static {
+    /// Append the digest to the recorded data.
+    fn update(&mut self, digest: &[u8]);
+
+    /// Hash all recorded data and reset
+    /// the hasher to the initial state.
+    fn finalize(&mut self) -> Hash;
+
+    /// Reset the hasher without generating a hash.
+    ///
+    /// This is not required to call before calling `record`.
+    fn reset(&mut self);
+
     /// Validate the hash.
     fn validate(hash: &Hash) -> bool;
-
-    /// Hash the given digest.
-    fn hash(&mut self, digest: &[u8]) -> Hash;
 
     /// Size of the hash in bytes.
     fn size() -> usize;
--- consensus/src/sha256.rs
+++ consensus/src/sha256.rs
@@ -4,25 +4,35 @@ use sha2::{Digest, Sha256 as ISha256};
 const HASH_LENGTH: usize = 32;
 
 #[derive(Clone, Default)]
-pub struct Sha256 {}
+pub struct Sha256 {
+    hasher: Option<ISha256>,
+}
 
 impl Sha256 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            hasher: Some(ISha256::new()),
+        }
     }
 }
 
 impl Hasher for Sha256 {
-    fn validate(hash: &Hash) -> bool {
-        hash.len() == HASH_LENGTH
+    fn update(&mut self, digest: &[u8]) {
+        self.hasher.as_mut().unwrap().update(digest);
     }
 
-    fn hash(&mut self, data: &[u8]) -> Hash {
-        // We do not use self in this implementation but we could use it in the future
-        // to avoid allocating memory for a new hasher every time.
-        let mut hasher = ISha256::new();
-        hasher.update(data);
-        hasher.finalize().to_vec().into()
+    fn finalize(&mut self) -> Hash {
+        let hash = self.hasher.take().unwrap().finalize().to_vec().into();
+        self.reset();
+        hash
+    }
+
+    fn reset(&mut self) {
+        self.hasher = Some(ISha256::new());
+    }
+
+    fn validate(hash: &Hash) -> bool {
+        hash.len() == HASH_LENGTH
     }
 
     fn size() -> usize {
@@ -38,12 +48,29 @@ mod tests {
     #[test]
     fn test_sha256() {
         let digest = b"hello world";
+
+        // Generate initial hash
         let mut hasher = Sha256::new();
-        let hash = hasher.hash(digest);
+        hasher.update(digest);
+        let hash = hasher.finalize();
         assert!(Sha256::validate(&hash));
         assert_eq!(
             hex(&hash),
             "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
         );
+
+        // Reuse hasher
+        hasher.update(digest);
+        let hash = hasher.finalize();
+        assert!(Sha256::validate(&hash));
+        assert_eq!(
+            hex(&hash),
+            "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+        );
+    }
+
+    #[test]
+    fn test_sha256_size() {
+        assert_eq!(Sha256::size(), HASH_LENGTH);
     }
 }
