39b12555874c36798fd6d0151be86534987d536b
Patrick O'Grady <me@patrickogrady.xyz>
2024-10-30 08:09:02 -0700

    spike on read_next

--- storage/src/journal/disk.rs
+++ storage/src/journal/disk.rs
@@ -1,4 +1,5 @@
 use super::{Config, Error};
+use bytes::Bytes;
 use commonware_runtime::{Blob, Storage};
 use std::{collections::BTreeMap, marker::PhantomData};
 use tracing::debug;
@@ -40,5 +41,56 @@ impl<B: Blob, E: Storage<B>> Journal<B, E> {
 
             _phantom_b: PhantomData,
         })
+    }
+
+    async fn read_next(&self, blob: &mut B, offset: usize) -> Result<(usize, Bytes), Error> {
+        // Read item size
+        let mut size = [0u8; 4];
+        let bytes_read = blob
+            .read_at(&mut size, offset)
+            .await
+            .map_err(Error::Runtime)?;
+        if bytes_read != 4 {
+            unimplemented!();
+        }
+        let size = u32::from_be_bytes(size) as usize;
+        let offset = offset + 4;
+
+        // Read item
+        let mut item = vec![0u8; size];
+        let bytes_read = blob
+            .read_at(&mut item, offset + 4)
+            .await
+            .map_err(Error::Runtime)?;
+        if bytes_read != size {
+            unimplemented!();
+        }
+        let offset = offset + size;
+        let checksum = crc32fast::hash(&item);
+
+        // Read checksum
+        let mut stored_checksum = [0u8; 4];
+        let bytes_read = blob
+            .read_at(&mut stored_checksum, offset)
+            .await
+            .map_err(Error::Runtime)?;
+        if bytes_read != 4 {
+            unimplemented!();
+        }
+        let offset = offset + 4;
+        let stored_checksum = u32::from_be_bytes(stored_checksum);
+        if checksum != stored_checksum {
+            unimplemented!();
+        }
+
+        // Return item
+        Ok((offset, Bytes::from(item)))
+    }
+
+    pub async fn replay(&mut self, f: impl Fn(u64, Bytes) -> bool) -> Result<(), Error> {
+        for (index, blob) in &self.blobs {
+            let cursor = 0;
+        }
+        Ok(())
     }
 }
