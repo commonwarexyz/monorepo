bfb08732f35c712880e8d494f18c57dfb3f441ee
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-02 14:40:57 -0700

    add cap tests

--- storage/src/archive/translator.rs
+++ storage/src/archive/translator.rs
@@ -3,7 +3,7 @@ use super::Translator;
 fn cap<const N: usize>(key: &[u8]) -> [u8; N] {
     let mut capped = [0; N];
     let len = key.len().min(N);
-    capped.copy_from_slice(&key[..len]);
+    capped[..len].copy_from_slice(&key[..len]);
     capped
 }
 
@@ -37,5 +37,43 @@ impl Translator for EightCap {
 
     fn transform(&self, key: &[u8]) -> Self::Key {
         cap(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_cap() {
+        let translator = TwoCap;
+        assert_eq!(translator.transform(b""), [0, 0]);
+        assert_eq!(translator.transform(b"a"), [b'a', 0]);
+        assert_eq!(translator.transform(b"ab"), [b'a', b'b']);
+        assert_eq!(translator.transform(b"abc"), [b'a', b'b']);
+    }
+
+    #[test]
+    fn test_four_cap() {
+        let translator = FourCap;
+        assert_eq!(translator.transform(b""), [0, 0, 0, 0]);
+        assert_eq!(translator.transform(b"a"), [b'a', 0, 0, 0]);
+        assert_eq!(translator.transform(b"abcd"), [b'a', b'b', b'c', b'd']);
+        assert_eq!(translator.transform(b"abcdef"), [b'a', b'b', b'c', b'd']);
+    }
+
+    #[test]
+    fn test_eight_cap() {
+        let translator = EightCap;
+        assert_eq!(translator.transform(b""), [0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(translator.transform(b"a"), [b'a', 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(
+            translator.transform(b"abcdefgh"),
+            [b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h']
+        );
+        assert_eq!(
+            translator.transform(b"abcdefghijk"),
+            [b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h']
+        );
     }
 }
