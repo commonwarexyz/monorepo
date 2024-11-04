112c595d47ca4b61371fbf71efc90ecf3d0b7462
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-04 09:49:05 -0800

    use translator macro

--- storage/src/archive/translator.rs
+++ storage/src/archive/translator.rs
@@ -7,42 +7,25 @@ fn cap<const N: usize>(key: &[u8]) -> [u8; N] {
     capped[..len].copy_from_slice(&key[..len]);
     capped
 }
+macro_rules! define_cap_translator {
+    ($name:ident, $size:expr) => {
+        #[doc = concat!("A translator that caps the key to ", stringify!($size), " bytes.")]
+        #[derive(Clone)]
+        pub struct $name;
 
-/// A translator that caps the key to two bytes.
-#[derive(Clone)]
-pub struct TwoCap;
+        impl Translator for $name {
+            type Key = [u8; $size];
 
-impl Translator for TwoCap {
-    type Key = [u8; 2];
-
-    fn transform(&self, key: &[u8]) -> Self::Key {
-        cap(key)
-    }
+            fn transform(&self, key: &[u8]) -> Self::Key {
+                cap(key)
+            }
+        }
+    };
 }
 
-/// A translator that caps the key to four bytes.
-#[derive(Clone)]
-pub struct FourCap;
-
-impl Translator for FourCap {
-    type Key = [u8; 4];
-
-    fn transform(&self, key: &[u8]) -> Self::Key {
-        cap(key)
-    }
-}
-
-/// A translator that caps the key to eight bytes.
-#[derive(Clone)]
-pub struct EightCap;
-
-impl Translator for EightCap {
-    type Key = [u8; 8];
-
-    fn transform(&self, key: &[u8]) -> Self::Key {
-        cap(key)
-    }
-}
+define_cap_translator!(TwoCap, 2);
+define_cap_translator!(FourCap, 4);
+define_cap_translator!(EightCap, 8);
 
 #[cfg(test)]
 mod tests {
