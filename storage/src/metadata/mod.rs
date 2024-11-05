3f64b90f5f5ef249c6b36d213be346ace00db6ee
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-05 12:30:32 -0800

    add failing test

--- storage/src/metadata/mod.rs
+++ storage/src/metadata/mod.rs
@@ -104,4 +104,69 @@ mod tests {
             assert_eq!(value, &hello);
         });
     }
+
+    #[test_traced]
+    fn test_multi_sync() {
+        // Initialize the deterministic runtime
+        let (executor, context, _) = Executor::default();
+        executor.start(async move {
+            // Create a metadata store
+            let registry = Arc::new(Mutex::new(Registry::default()));
+            let cfg = Config {
+                registry: registry.clone(),
+                partition: "test".to_string(),
+            };
+            let mut metadata = Metadata::init(context.clone(), cfg).await.unwrap();
+
+            // Put a key
+            let key = 42;
+            let hello = Bytes::from("hello");
+            metadata.put(key, hello.clone());
+
+            // Sync the metadata store
+            metadata.sync().await.unwrap();
+
+            // Check metrics
+            let mut buffer = String::new();
+            encode(&mut buffer, &registry.lock().unwrap()).unwrap();
+            assert!(buffer.contains("syncs_total 1"));
+            assert!(buffer.contains("keys 1"));
+
+            // Put an overlapping key and a new key
+            let world = Bytes::from("world");
+            metadata.put(key, world.clone());
+            let key2 = 43;
+            let foo = Bytes::from("foo");
+            metadata.put(key2, foo.clone());
+
+            // Close the metadata store
+            metadata.close().await.unwrap();
+
+            // Check metrics
+            let mut buffer = String::new();
+            encode(&mut buffer, &registry.lock().unwrap()).unwrap();
+            assert!(buffer.contains("syncs_total 2"));
+            assert!(buffer.contains("keys 2"));
+
+            // Reopen the metadata store
+            let registry = Arc::new(Mutex::new(Registry::default()));
+            let cfg = Config {
+                registry: registry.clone(),
+                partition: "test".to_string(),
+            };
+            let metadata = Metadata::init(context.clone(), cfg).await.unwrap();
+
+            // Check metrics
+            let mut buffer = String::new();
+            encode(&mut buffer, &registry.lock().unwrap()).unwrap();
+            assert!(buffer.contains("syncs_total 0"));
+            assert!(buffer.contains("keys 2"));
+
+            // Get the key
+            let value = metadata.get(key).unwrap();
+            assert_eq!(value, &world);
+            let value = metadata.get(key2).unwrap();
+            assert_eq!(value, &foo);
+        });
+    }
 }
