510e1cb3dfc1d2fccbb3878cbd40b0c4a03a5249
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-12 10:11:41 -0800

    add backfiller config

--- consensus/src/authority/actors/backfiller/mod.rs
+++ consensus/src/authority/actors/backfiller/mod.rs
@@ -1,3 +1,21 @@
 mod actor;
 mod ingress;
 mod priority_queue;
+
+use crate::Automaton;
+use bytes::Bytes;
+use commonware_cryptography::{Hasher, Scheme};
+use governor::Quota;
+use std::time::Duration;
+
+pub struct Config<C: Scheme, H: Hasher, A: Automaton> {
+    pub crypto: C,
+    pub hasher: H,
+    pub application: A,
+
+    pub namespace: Bytes,
+    pub fetch_timeout: Duration,
+    pub max_fetch_count: u32,
+    pub max_fetch_size: usize,
+    pub fetch_rate_per_peer: Quota,
+}
