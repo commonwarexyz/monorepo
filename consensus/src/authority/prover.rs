8a42aabe10a93f4ccc6176b3b1a7862c8908f457
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-19 16:35:37 -0800

    prover compiles

--- consensus/src/authority/prover.rs
+++ consensus/src/authority/prover.rs
@@ -168,11 +168,11 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
     pub(crate) fn serialize_conflicting_proposal(
         view: View,
         height_1: Height,
-        parent_1: Digest,
+        parent_1: View,
         payload_1: Digest,
         signature_1: wire::Signature,
         height_2: Height,
-        parent_2: Digest,
+        parent_2: View,
         payload_2: Digest,
         signature_2: wire::Signature,
     ) -> Proof {
@@ -182,11 +182,11 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         let len = 8
             + public_key_len
             + 8
-            + digest_len
+            + 8
             + digest_len
             + signature_len
             + 8
-            + digest_len
+            + 8
             + digest_len
             + signature_len;
 
@@ -201,20 +201,20 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         proof.put_u64(view);
         proof.put(public_key);
         proof.put_u64(height_1);
-        proof.put(parent_1);
+        proof.put_u64(parent_1);
         proof.put(payload_1);
         proof.put(signature_1.signature);
         proof.put_u64(height_2);
-        proof.put(parent_2);
+        proof.put_u64(parent_2);
         proof.put(payload_2);
         proof.put(signature_2.signature);
         proof.into()
     }
 
     pub fn deserialize_conflicting_proposal(
-        &self,
         mut proof: Proof,
         check_sig: bool,
+        namespace: &[u8],
     ) -> Option<(PublicKey, View)> {
         // Ensure proof is big enough
         let digest_len = H::len();
@@ -222,11 +222,11 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         let len = 8
             + public_key_len
             + 8
-            + digest_len
+            + 8
             + digest_len
             + signature_len
             + 8
-            + digest_len
+            + 8
             + digest_len
             + signature_len;
         if proof.len() != len {
@@ -237,11 +237,11 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         let view = proof.get_u64();
         let public_key = proof.copy_to_bytes(public_key_len);
         let height_1 = proof.get_u64();
-        let parent_1 = proof.copy_to_bytes(digest_len);
+        let parent_1 = proof.get_u64();
         let payload_1 = proof.copy_to_bytes(digest_len);
         let signature_1 = proof.copy_to_bytes(signature_len);
         let height_2 = proof.get_u64();
-        let parent_2 = proof.copy_to_bytes(digest_len);
+        let parent_2 = proof.get_u64();
         let payload_2 = proof.copy_to_bytes(digest_len);
         let signature_2 = proof.copy_to_bytes(signature_len);
 
@@ -250,19 +250,11 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
             if !C::validate(&public_key) {
                 return None;
             }
-            let proposal_message_1 = proposal_message(view, height_1, &parent_1, &payload_1);
-            let proposal_message_2 = proposal_message(view, height_2, &parent_2, &payload_2);
-            if !C::verify(
-                &self.proposal_namespace,
-                &proposal_message_1,
-                &public_key,
-                &signature_1,
-            ) || !C::verify(
-                &self.proposal_namespace,
-                &proposal_message_2,
-                &public_key,
-                &signature_2,
-            ) {
+            let proposal_message_1 = proposal_message(view, height_1, parent_1, &payload_1);
+            let proposal_message_2 = proposal_message(view, height_2, parent_2, &payload_2);
+            if !C::verify(namespace, &proposal_message_1, &public_key, &signature_1)
+                || !C::verify(namespace, &proposal_message_2, &public_key, &signature_2)
+            {
                 return None;
             }
         }
@@ -272,176 +264,78 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
     pub(crate) fn serialize_conflicting_vote(
         view: View,
         height_1: Height,
-        hash_1: Digest,
+        parent_1: View,
+        payload_1: Digest,
         signature_1: wire::Signature,
         height_2: Height,
-        hash_2: Digest,
+        parent_2: View,
+        payload_2: Digest,
         signature_2: wire::Signature,
     ) -> Proof {
-        // Setup proof
-        let digest_len = H::len();
-        let (public_key_len, signature_len) = C::len();
-        let len =
-            8 + public_key_len + 8 + digest_len + signature_len + 8 + digest_len + signature_len;
-
-        // Ensure proof can be generated correctly
-        if signature_1.public_key != signature_2.public_key {
-            panic!("public keys do not match");
-        }
-        let public_key = signature_1.public_key;
-
-        // Encode proof
-        let mut proof = Vec::with_capacity(len);
-        proof.put_u64(view);
-        proof.put(public_key);
-        proof.put_u64(height_1);
-        proof.put(hash_1);
-        proof.put(signature_1.signature);
-        proof.put_u64(height_2);
-        proof.put(hash_2);
-        proof.put(signature_2.signature);
-        proof.into()
+        Self::serialize_conflicting_proposal(
+            view,
+            height_1,
+            parent_1,
+            payload_1,
+            signature_1,
+            height_2,
+            parent_2,
+            payload_2,
+            signature_2,
+        )
     }
 
     pub fn deserialize_conflicting_vote(
         &self,
-        mut proof: Proof,
+        proof: Proof,
         check_sig: bool,
     ) -> Option<(PublicKey, View)> {
-        // Ensure proof is big enough
-        let digest_len = H::len();
-        let (public_key_len, signature_len) = C::len();
-        let len =
-            8 + public_key_len + 8 + digest_len + signature_len + 8 + digest_len + signature_len;
-        if proof.len() != len {
-            return None;
-        }
-
-        // Decode proof
-        let view = proof.get_u64();
-        let public_key = proof.copy_to_bytes(public_key_len);
-        let height_1 = proof.get_u64();
-        let hash_1 = proof.copy_to_bytes(digest_len);
-        let signature_1 = proof.copy_to_bytes(signature_len);
-        let height_2 = proof.get_u64();
-        let hash_2 = proof.copy_to_bytes(digest_len);
-        let signature_2 = proof.copy_to_bytes(signature_len);
-
-        // Verify signatures
-        if check_sig {
-            if !C::validate(&public_key) {
-                return None;
-            }
-            let vote_message_1 = vote_message(view, Some(height_1), Some(&hash_1));
-            let vote_message_2 = vote_message(view, Some(height_2), Some(&hash_2));
-            if !C::verify(
-                &self.vote_namespace,
-                &vote_message_1,
-                &public_key,
-                &signature_1,
-            ) || !C::verify(
-                &self.vote_namespace,
-                &vote_message_2,
-                &public_key,
-                &signature_2,
-            ) {
-                return None;
-            }
-        }
-        Some((public_key, view))
+        Self::deserialize_conflicting_proposal(proof, check_sig, &self.vote_namespace)
     }
 
     pub(crate) fn serialize_conflicting_finalize(
         view: View,
         height_1: Height,
-        hash_1: Digest,
+        parent_1: View,
+        payload_1: Digest,
         signature_1: wire::Signature,
         height_2: Height,
-        hash_2: Digest,
+        parent_2: View,
+        payload_2: Digest,
         signature_2: wire::Signature,
     ) -> Proof {
-        // Setup proof
-        let digest_len = H::len();
-        let (public_key_len, signature_len) = C::len();
-        let len =
-            8 + public_key_len + 8 + digest_len + signature_len + 8 + digest_len + signature_len;
-
-        // Ensure proof can be generated correctly
-        if signature_1.public_key != signature_2.public_key {
-            panic!("public keys do not match");
-        }
-        let public_key = signature_1.public_key;
-
-        // Encode proof
-        let mut proof = Vec::with_capacity(len);
-        proof.put_u64(view);
-        proof.put(public_key);
-        proof.put_u64(height_1);
-        proof.put(hash_1);
-        proof.put(signature_1.signature);
-        proof.put_u64(height_2);
-        proof.put(hash_2);
-        proof.put(signature_2.signature);
-        proof.into()
+        Self::serialize_conflicting_proposal(
+            view,
+            height_1,
+            parent_1,
+            payload_1,
+            signature_1,
+            height_2,
+            parent_2,
+            payload_2,
+            signature_2,
+        )
     }
 
     pub fn deserialize_conflicting_finalize(
         &self,
-        mut proof: Proof,
+        proof: Proof,
         check_sig: bool,
     ) -> Option<(PublicKey, View)> {
-        let digest_len = H::len();
-        let (public_key_len, signature_len) = C::len();
-        let len =
-            8 + public_key_len + 8 + digest_len + signature_len + 8 + digest_len + signature_len;
-        if proof.len() != len {
-            return None;
-        }
-
-        // Decode proof
-        let view = proof.get_u64();
-        let public_key = proof.copy_to_bytes(public_key_len);
-        let height_1 = proof.get_u64();
-        let hash_1 = proof.copy_to_bytes(digest_len);
-        let signature_1 = proof.copy_to_bytes(signature_len);
-        let height_2 = proof.get_u64();
-        let hash_2 = proof.copy_to_bytes(digest_len);
-        let signature_2 = proof.copy_to_bytes(signature_len);
-
-        // Verify signatures
-        if check_sig {
-            if !C::validate(&public_key) {
-                return None;
-            }
-            let finalize_message_1 = finalize_message(view, height_1, &hash_1);
-            let finalize_message_2 = finalize_message(view, height_2, &hash_2);
-            if !C::verify(
-                &self.finalize_namespace,
-                &finalize_message_1,
-                &public_key,
-                &signature_1,
-            ) || !C::verify(
-                &self.finalize_namespace,
-                &finalize_message_2,
-                &public_key,
-                &signature_2,
-            ) {
-                return None;
-            }
-        }
-        Some((public_key, view))
+        Self::deserialize_conflicting_proposal(proof, check_sig, &self.finalize_namespace)
     }
 
     pub(crate) fn serialize_null_finalize(
         view: View,
         height: Height,
-        digest: Digest,
+        parent: View,
+        payload: Digest,
         signature_finalize: wire::Signature,
         signature_null: wire::Signature,
     ) -> Proof {
         // Setup proof
         let (public_key_len, signature_len) = C::len();
-        let len = 8 + public_key_len + 8 + H::len() + signature_len + signature_len;
+        let len = 8 + public_key_len + 8 + 8 + H::len() + signature_len + signature_len;
 
         // Ensure proof can be generated correctly
         if signature_finalize.public_key != signature_null.public_key {
@@ -454,7 +348,8 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         proof.put_u64(view);
         proof.put(public_key);
         proof.put_u64(height);
-        proof.put(digest);
+        proof.put_u64(parent);
+        proof.put(payload);
         proof.put(signature_finalize.signature);
         proof.put(signature_null.signature);
         proof.into()
@@ -467,7 +362,7 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
     ) -> Option<(PublicKey, View)> {
         // Ensure proof is big enough
         let (public_key_len, signature_len) = C::len();
-        let len = 8 + public_key_len + 8 + H::len() + signature_len + signature_len;
+        let len = 8 + public_key_len + 8 + 8 + H::len() + signature_len + signature_len;
         if proof.len() != len {
             return None;
         }
@@ -476,7 +371,8 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
         let view = proof.get_u64();
         let public_key = proof.copy_to_bytes(public_key_len);
         let height = proof.get_u64();
-        let digest = proof.copy_to_bytes(H::len());
+        let parent = proof.get_u64();
+        let payload = proof.copy_to_bytes(H::len());
         let signature_finalize = proof.copy_to_bytes(signature_len);
         let signature_null = proof.copy_to_bytes(signature_len);
 
@@ -485,8 +381,8 @@ impl<C: Scheme, H: Hasher> Prover<C, H> {
             if !C::validate(&public_key) {
                 return None;
             }
-            let finalize_message = finalize_message(view, height, &digest);
-            let null_message = vote_message(view, None, None);
+            let finalize_message = proposal_message(view, height, parent, &payload);
+            let null_message = null_message(view);
             if !C::verify(
                 &self.finalize_namespace,
                 &finalize_message,
