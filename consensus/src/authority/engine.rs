55fbda358a3510567fdabd2132f92030bef3b698
Patrick O'Grady <me@patrickogrady.xyz>
2024-11-23 18:03:39 -0800

    engine compiles

--- consensus/src/authority/engine.rs
+++ consensus/src/authority/engine.rs
@@ -1,5 +1,5 @@
 use super::{actors::voter, config::Config, Context, View};
-use crate::{Automaton, Supervisor};
+use crate::{Automaton, Finalizer, Relay, Supervisor};
 use commonware_cryptography::{Hasher, Scheme};
 use commonware_macros::select;
 use commonware_p2p::{Receiver, Sender};
@@ -12,7 +12,7 @@ pub struct Engine<
     E: Clock + GClock + Rng + CryptoRng + Spawner,
     C: Scheme,
     H: Hasher,
-    A: Automaton<Context = Context>,
+    A: Automaton<Context = Context> + Relay + Finalizer,
     S: Supervisor<Seed = (), Index = View>,
 > {
     runtime: E,
@@ -27,7 +27,7 @@ impl<
         E: Clock + GClock + Rng + CryptoRng + Spawner,
         C: Scheme,
         H: Hasher,
-        A: Automaton<Context = Context>,
+        A: Automaton<Context = Context> + Relay + Finalizer,
         S: Supervisor<Seed = (), Index = View>,
     > Engine<E, C, H, A, S>
 {
