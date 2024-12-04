619f78fa2b4b38a8585f07da0ea82c78c9bb54cd
Patrick O'Grady <me@patrickogrady.xyz>
2024-12-04 14:28:18 -0800

    add comments for config values

--- p2p/src/utils/requester.rs
+++ p2p/src/utils/requester.rs
@@ -21,9 +21,16 @@ pub type ID = u64;
 
 /// Configuration for `Requester`.
 pub struct Config<C: Scheme> {
+    /// Cryptographic primitives.
     pub crypto: C,
+
+    /// Rate limit for requests per participant.
     pub rate_limit: Quota,
+
+    /// Initial expected performance for new participants.
     pub initial: Duration,
+
+    /// Timeout for requests.
     pub timeout: Duration,
 }
 
