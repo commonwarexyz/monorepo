e8ad854dad0e0b54a7dd61ddf2b393fe5e797bf0
Patrick O'Grady <me@patrickogrady.xyz>
2024-12-09 13:13:14 -0800

    populate config

--- examples/log/src/application/mod.rs
+++ examples/log/src/application/mod.rs
@@ -1,4 +1,6 @@
-//! Log is a simple application that
+//! This crate contains all logic typically implemented by an application developer.
+//! This includes things like how to produce/verify blocks and how to identify which
+//! participants are active at a given view.
 
 use commonware_consensus::simplex::Prover;
 use commonware_cryptography::{Hasher, PublicKey, Scheme};
@@ -8,9 +10,18 @@ pub use actor::Application;
 mod ingress;
 mod supervisor;
 
+/// Configuration for the application.
 pub struct Config<C: Scheme, H: Hasher> {
+    /// Hashing scheme to use.
     pub hasher: H,
+
+    /// Prover used to decode opaque proofs from consensus.
     pub prover: Prover<C, H>,
+
+    /// Participants active in consensus.
     pub participants: Vec<PublicKey>,
+
+    /// Number of messages from consensus to hold in our backlog
+    /// before blocking.
     pub mailbox_size: usize,
 }
