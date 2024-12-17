a00b19f21c1a88bc64932264476b2ba09c749c6b
Brendan Chou <brendan@commonware.xyz>
2024-12-17 14:50:56 -0800

    fix bench

--- stream/src/public_key/benches/connection.rs
+++ stream/src/public_key/benches/connection.rs
@@ -1,66 +1,71 @@
 use chacha20poly1305::{ChaCha20Poly1305, KeyInit};
 use commonware_runtime::mocks;
 use commonware_stream::{public_key::Connection, Receiver, Sender};
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use futures::executor::block_on;
 
 fn benchmark_sender(c: &mut Criterion) {
-    let msg = b"hello";
-    let max_message_size = 1024 * 1024; // 1MB
-
-    c.bench_function(
-        &format!(
-            "sender: max_message_size_len={} msg_len={}",
-            max_message_size,
-            msg.len()
-        ),
-        |b| {
+    let kbs = [1, 4, 16, 256, 1024, 4_096, 16_384, 65_536];
+    let mut group = c.benchmark_group("sender");
+    for &kb in &kbs {
+        let message_size = kb * 1024;
+        let msg = vec![0u8; message_size];
+        group.bench_with_input(BenchmarkId::from_parameter(message_size), &msg, |b, msg| {
             b.iter_batched(
                 || {
                     let cipher = ChaCha20Poly1305::new(&[0u8; 32].into());
                     let (sink, stream) = mocks::Channel::init();
-                    let connection = Connection::new(true, sink, stream, cipher, max_message_size);
+                    let connection = Connection::new(true, sink, stream, cipher, message_size);
                     let (sender, _receiver) = connection.split();
-                    sender
+                    let msg = msg.clone();
+                    (sender, msg)
                 },
-                |mut sender| async move {
-                    sender.send(msg).await.unwrap();
+                |(mut sender, msg)| {
+                    block_on(async move {
+                        sender.send(&msg).await.unwrap();
+                    });
                 },
                 BatchSize::SmallInput,
             );
-        },
-    );
+        });
+    }
 }
 
 fn benchmark_receiver(c: &mut Criterion) {
-    let msg = b"hello";
-    let max_message_size = 1024 * 1024; // 1MB
-
-    c.bench_function(
-        &format!(
-            "receiver: max_message_size_len={} msg_len={}",
-            max_message_size,
-            msg.len()
-        ),
-        |b| {
+    let kbs = [1, 4, 16, 256, 1024, 4_096, 16_384, 65_536];
+    let mut group = c.benchmark_group("receiver");
+    for &kb in &kbs {
+        let message_size = kb * 1024;
+        let msg = vec![0u8; message_size];
+        group.bench_with_input(BenchmarkId::from_parameter(message_size), &msg, |b, msg| {
             b.iter_batched(
                 || {
+                    // Set up a connection between two parties.
+                    // We only send messages in one direction from A to B.
                     let cipher = ChaCha20Poly1305::new(&[0u8; 32].into());
                     let (sink, stream) = mocks::Channel::init();
-                    let connection = Connection::new(true, sink, stream, cipher, max_message_size);
-                    let (mut sender, receiver) = connection.split();
+                    let (sink_dummy, stream_dummy) = mocks::Channel::init();
+                    let conn_a =
+                        Connection::new(false, sink, stream_dummy, cipher.clone(), message_size);
+                    let conn_b = Connection::new(true, sink_dummy, stream, cipher, message_size);
+
+                    let (mut sender, _) = conn_a.split();
+                    let (_, receiver) = conn_b.split();
+
                     block_on(async {
                         sender.send(msg).await.unwrap();
                     });
                     receiver
                 },
-                |mut receiver| async move {
-                    receiver.receive().await.unwrap();
+                |mut receiver| {
+                    block_on(async move {
+                        receiver.receive().await.unwrap();
+                    });
                 },
                 BatchSize::SmallInput,
             );
-        },
-    );
+        });
+    }
 }
 
 criterion_group!(benches, benchmark_sender, benchmark_receiver);
