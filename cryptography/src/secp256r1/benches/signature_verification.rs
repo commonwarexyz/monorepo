6823e84b49f7b570ac00075ce89c2e5cef4ff089
nathan haim <haim.nathan@icloud.com>
2025-01-09 19:43:14 +0100

    [cryptography/secp256r1] create signature verification benchmark (#333)

---
+++ cryptography/src/secp256r1/benches/signature_verification.rs
@@ -0,0 +1,42 @@
+use commonware_cryptography::{Scheme, Secp256r1};
+use criterion::{criterion_group, BatchSize, Criterion};
+use rand::{thread_rng, Rng};
+use std::hint::black_box;
+
+fn benchmark_signature_verify(c: &mut Criterion) {
+    let namespace = b"namespace";
+    let mut msg = [0u8; 32];
+    thread_rng().fill(&mut msg);
+    c.bench_function(
+        &format!(
+            "{}/ns_len={} msg_len={}",
+            module_path!(),
+            namespace.len(),
+            msg.len()
+        ),
+        |b| {
+            b.iter_batched(
+                || {
+                    let mut signer = Secp256r1::new(&mut thread_rng());
+                    let signature = signer.sign(Some(namespace), &msg);
+                    (signer, signature)
+                },
+                |(signer, signature)| {
+                    black_box(Secp256r1::verify(
+                        Some(namespace),
+                        &msg,
+                        &signer.public_key(),
+                        &signature,
+                    ));
+                },
+                BatchSize::SmallInput,
+            );
+        },
+    );
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark_signature_verify
+}
