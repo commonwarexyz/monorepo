5ca4feb1000bc5e4217f11f2498ce547d0d8d070
Roberto Bayardo <roberto@commonware.xyz>
2025-01-11 17:37:43 -0800

    make the MMR compute the hash values

--- storage/src/mmr/mem.rs
+++ storage/src/mmr/mem.rs
@@ -1,9 +1,10 @@
 //! A bare-bones MMR structure without pruning and where all elements are hashes & maintained in memory.
 
-use crate::mmr::Hash;
+use crate::mmr::{Hash, Hasher};
 
 /// Implementation of `InMemoryMMR`.
-pub struct InMemoryMMR<const N: usize> {
+pub struct InMemoryMMR<const N: usize, H: Hasher<N>> {
+    hasher: H,
     elements: Vec<Hash<N>>,
 }
 
@@ -34,18 +35,11 @@ impl Iterator for PeakIterator {
     }
 }
 
-impl<const N: usize> Default for InMemoryMMR<N> {
-    fn default() -> Self {
-        InMemoryMMR::new()
-    }
-}
-
-impl<const N: usize> InMemoryMMR<N> {
+impl<const N: usize, H: Hasher<N>> InMemoryMMR<N, H> {
     /// Return a new (empty) `InMemoryMMR`.
-    pub fn new() -> Self {
+    pub fn new(hasher: H) -> Self {
         Self {
-            //cfg,
-            //hasher: Sha256::new(),
+            hasher,
             elements: Vec::new(),
         }
     }
@@ -56,18 +50,19 @@ impl<const N: usize> InMemoryMMR<N> {
         if sz == 0 {
             return PeakIterator::default();
         }
-        // Compute the starting peak. This starting peak will not be in the MMR unless it happens to be a full binary
-        // tree, but that's OK as we'll just descend until we find one.
+        // Compute the starting peak. This starting peak will not be in the MMR unless it happens to
+        // be a full binary tree, but that's OK as we will descend leftward until we find one.
         let peak = u64::MAX >> sz.leading_zeros();
         let two_h = 1 << peak.trailing_ones();
         PeakIterator { sz, peak, two_h }
     }
 
-    // If this MMR has a peak that is a leaf, this function returns it along with all other peaks of consecutively
-    // increasing height. This is precisely the set of peaks we need from which to compute new parent nodes after
-    // adding a new element to the MMR. Peaks are returned from highest to lowest (the leaf peak is last).
-    fn peaks_from_leaf(&mut self, peaks: &mut Vec<u64>) {
-        peaks.clear();
+    // Returns the set of peaks that will require a new parent after adding the next element to the
+    // MMR. This set is non-empty only if there is a height-0 (leaf) peak in the MMR. The result
+    // will contain this leaf peak plus all other MMR peaks of consecutively increasing height, in
+    // reverse order (that is, the leaf peak comes last).
+    fn nodes_needing_parents(&mut self) -> Vec<u64> {
+        let mut peaks = Vec::new();
         let it = self.peak_iterator();
         let mut last_height = u32::MAX;
         for (height, peak) in it {
@@ -83,44 +78,118 @@ impl<const N: usize> InMemoryMMR<N> {
             // there is no peak that is a leaf
             peaks.clear();
         }
+        peaks
     }
 
-    /// Add a hash to the MMR and return its index.
-    pub fn add_leaf_hash(&mut self, element: &Hash<N>) -> u64 {
-        let mut v = Vec::new();
-        self.peaks_from_leaf(&mut v);
-        let index = self.elements.len() as u64;
-        self.elements.push(*element);
-        // simulate adding new parents.. ultimately this will involve hashing pairs of elements of course
-        for _ in 0..v.len() {
-            self.elements.push(*element);
+    /// Add an element to the MMR and return its position in the MMR.
+    pub fn add(&mut self, element: &Hash<N>) -> u64 {
+        let peaks = self.nodes_needing_parents();
+
+        // insert the new leaf hash into the MMR
+        let leaf_pos = self.elements.len() as u64;
+        let mut current_hash = self.hasher.hash_leaf(leaf_pos, element);
+        self.elements.push(current_hash);
+
+        // Compute the new parent nodes, if any, and insert them into the MMR.
+        for sibling_pos in peaks.iter().rev() {
+            let parent_pos = self.elements.len() as u64;
+            current_hash = self.hasher.hash_node(
+                parent_pos,
+                &self.elements[*sibling_pos as usize],
+                &current_hash,
+            );
+            self.elements.push(current_hash);
         }
-        index
+        leaf_pos
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::mmr::{Hash, Hasher, InMemoryMMR, Sha256Hasher};
 
     #[test]
-    /// Test the `add_leaf_hash` function by adding 11 elements to the MMR. In the end the MMR should have 19 elements
-    /// total with 3 peaks (14, 17, 18) as pictured in the MMR example here:
+    /// Test MMR building by consecutively adding 11 equal values to a new MMR. In the end the MMR
+    /// should have 19 nodes total with 3 peaks, exactly as pictured in the MMR example here:
     /// https://docs.grin.mw/wiki/chain-state/merkle-mountain-range/
-    fn test_add_elements() {
-        let mut mmr: InMemoryMMR<32> = InMemoryMMR::new();
+    ///
+    /// Pasted here for convenience:
+    ///
+    ///    Height
+    ///      3              14
+    ///                   /    \
+    ///                  /      \
+    ///                 /        \
+    ///                /          \
+    ///      2        6            13
+    ///             /   \        /    \
+    ///      1     2     5      9     12     17
+    ///           / \   / \    / \   /  \   /  \
+    ///      0   0   1 3   4  7   8 10  11 15  16 18
+    fn test_add_eleven_values() {
+        let mut mmr: InMemoryMMR<32, Sha256Hasher> = InMemoryMMR::new(Sha256Hasher::new());
         // the empty iterator should have no peaks
         assert_eq!(mmr.peak_iterator().next(), None);
 
         let element: Hash<32> = Hash(*b"01234567012345670123456701234567");
+        let mut leaves: Vec<u64> = Vec::new();
         for _ in 0..11 {
-            mmr.add_leaf_hash(&element);
+            leaves.push(mmr.add(&element));
             let peaks: Vec<(u32, u64)> = mmr.peak_iterator().collect();
             assert_ne!(peaks.len(), 0);
             assert!(peaks.len() <= mmr.elements.len());
         }
-        assert_eq!(mmr.elements.len(), 19);
+        assert_eq!(mmr.elements.len(), 19, "mmr not of expected size");
+        assert_eq!(
+            leaves,
+            vec![0, 1, 3, 4, 7, 8, 10, 11, 15, 16, 18],
+            "mmr leaf positions not as expected"
+        );
         let peaks: Vec<(u32, u64)> = mmr.peak_iterator().collect();
-        assert_eq!(peaks, vec![(3, 14), (1, 17), (0, 18)]);
+        assert_eq!(
+            peaks,
+            vec![(3, 14), (1, 17), (0, 18)],
+            "mmr peaks not as expected"
+        );
+
+        // Test nodes_needing_parents on the final MMR. Since there's a height gap between the
+        // heighest peak (14) and the next, only the lower two peaks (17, 18) should be returned.
+        let peaks_needing_parents = mmr.nodes_needing_parents();
+        assert_eq!(
+            peaks_needing_parents,
+            vec![17, 18],
+            "mmr nodes needing parents not as expected"
+        );
+
+        // verify leaf hashes
+        let mut hasher = Sha256Hasher::new();
+        for leaf in leaves {
+            let hash = hasher.hash_leaf(leaf, &element);
+            assert_eq!(mmr.elements[leaf as usize], hash);
+        }
+
+        // verify height=1 hashes
+        let hash2 = hasher.hash_node(2, &mmr.elements[0], &mmr.elements[1]);
+        assert_eq!(mmr.elements[2], hash2);
+        let hash5 = hasher.hash_node(5, &mmr.elements[3], &mmr.elements[4]);
+        assert_eq!(mmr.elements[5], hash5);
+        let hash9 = hasher.hash_node(9, &mmr.elements[7], &mmr.elements[8]);
+        assert_eq!(mmr.elements[9], hash9);
+        let hash12 = hasher.hash_node(12, &mmr.elements[10], &mmr.elements[11]);
+        assert_eq!(mmr.elements[12], hash12);
+        let hash17 = hasher.hash_node(17, &mmr.elements[15], &mmr.elements[16]);
+        assert_eq!(mmr.elements[17], hash17);
+
+        // verify height=2 hashes
+        let hash6 = hasher.hash_node(6, &mmr.elements[2], &mmr.elements[5]);
+        assert_eq!(mmr.elements[6], hash6);
+        let hash13 = hasher.hash_node(13, &mmr.elements[9], &mmr.elements[12]);
+        assert_eq!(mmr.elements[13], hash13);
+        let hash17 = hasher.hash_node(17, &mmr.elements[15], &mmr.elements[16]);
+        assert_eq!(mmr.elements[17], hash17);
+
+        // verify topmost hash
+        let hash14 = hasher.hash_node(14, &mmr.elements[6], &mmr.elements[13]);
+        assert_eq!(mmr.elements[14], hash14);
     }
 }
