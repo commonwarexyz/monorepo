8d518e59ffc6703110200fbbe038fc4f1b70cc14
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-15 05:52:41 -0800

    [consenus/threshold-simplex] Cleanup mod.rs (#345)

--- consensus/src/simplex/mod.rs
+++ consensus/src/simplex/mod.rs
@@ -1,8 +1,8 @@
 //! Simple and fast BFT agreement inspired by Simplex Consensus.
 //!
 //! Inspired by [Simplex Consensus](https://eprint.iacr.org/2023/463), `simplex` provides
-//! simple and fast BFT agreement that minimizes both view latency (i.e. block time)
-//! and finalization latency in a partially synchronous setting.
+//! simple and fast BFT agreement that minimizes both view (i.e. block time) and finalization
+//! latency in a partially synchronous setting.
 //!
 //! _If your application would benefit from succinct consensus certificates or a bias-resistant
 //! VRF, see [Threshold Simplex](crate::threshold_simplex)._
