5493c096053da9761068f6a2761ed3c61fc4022d
Marson Kotovi4 <137284002+MarsonKotovi4@users.noreply.github.com>
2025-01-17 21:29:56 +0200

    Update main.rs

--- examples/vrf/src/main.rs
+++ examples/vrf/src/main.rs
@@ -185,7 +185,7 @@ fn main() {
         .get_many::<u64>("participants")
         .expect("Please provide allowed keys")
         .copied();
-    if participants.len() == 0 {
+    if participants.is_empty() {
         panic!("Please provide at least one participant");
     }
     for peer in participants {
@@ -237,7 +237,7 @@ fn main() {
             .get_many::<u64>("contributors")
             .expect("Please provide contributors")
             .copied();
-        if participants.len() == 0 {
+        if participants.is_empty() {
             panic!("Please provide at least one contributor");
         }
         for peer in participants {
