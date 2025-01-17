bc8888a9eff6fed85dc330d28f32b9bae2059933
Marson Kotovi4 <137284002+MarsonKotovi4@users.noreply.github.com>
2025-01-17 22:43:10 +0200

    Update dealer.rs

--- examples/bridge/src/bin/dealer.rs
+++ examples/bridge/src/bin/dealer.rs
@@ -36,7 +36,7 @@ fn main() {
         .get_many::<u64>("participants")
         .expect("Please provide allowed keys")
         .copied();
-    if participants.is_empty() {
+    if participants.clone().next().is_none() {
         panic!("Please provide at least one participant");
     }
     for peer in participants {
