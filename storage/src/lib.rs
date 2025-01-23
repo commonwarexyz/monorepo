6b478dc42b5912499e821b40a8d5f4707d2caa1e
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-23 09:19:10 -0800

    add storage wasm

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -5,7 +5,10 @@
 //! `commonware-storage` is **ALPHA** software and is not yet recommended for production use. Developers should
 //! expect breaking changes and occasional instability.
 
+#[cfg(not(target_arch = "wasm32"))]
 pub mod archive;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod journal;
+#[cfg(not(target_arch = "wasm32"))]
 pub mod metadata;
 pub mod mmr;
