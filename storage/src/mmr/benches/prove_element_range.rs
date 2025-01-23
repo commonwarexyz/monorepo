59194c2212ef49b5dd893ff9b3289dcfddbaeb6d
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-23 06:00:09 -0800

    update element range

--- storage/src/mmr/benches/prove_element_range.rs
+++ storage/src/mmr/benches/prove_element_range.rs
@@ -1,51 +1,55 @@
 use commonware_cryptography::{Digest, Hasher, Sha256};
 use commonware_storage::mmr::mem::Mmr;
 use criterion::{criterion_group, Criterion};
+use rand::{rngs::StdRng, Rng, SeedableRng};
 
 fn bench_prove_element_range(c: &mut Criterion) {
-    let mut mmr = Mmr::<Sha256>::new();
-    let mut leaf_sample = Vec::new();
-    let element = Digest::from_static(&[100u8; 32]);
-    const NUM_ELEMENTS: usize = 5_000_000;
     const SAMPLE_SIZE: usize = 100;
-    let mut elements = Vec::with_capacity(NUM_ELEMENTS);
-    for i in 0..NUM_ELEMENTS {
-        let pos = mmr.add(&element);
-        elements.push(element.clone());
-        if i % SAMPLE_SIZE == 0 {
-            leaf_sample.push(pos);
-        }
+    for n in [10_000, 100_000, 1_000_000, 5_000_000, 10_000_000] {
+        c.bench_function(
+            &format!("{}/n={} samples={}", module_path!(), n, SAMPLE_SIZE),
+            |b| {
+                b.iter_batched(
+                    || {
+                        let mut mmr = Mmr::<Sha256>::new();
+                        let mut leaf_sample = Vec::new();
+                        let mut sampler = StdRng::seed_from_u64(0);
+                        let mut elements = Vec::with_capacity(n);
+                        for i in 0..n {
+                            let digest: [u8; 32] = sampler.gen();
+                            let element = Digest::from(digest.to_vec());
+                            let pos = mmr.add(&element);
+                            elements.push(element.clone());
+                            if i % SAMPLE_SIZE == 0 {
+                                leaf_sample.push(pos);
+                            }
+                        }
+                        let root_hash = mmr.root_hash();
+                        (mmr, root_hash, elements, leaf_sample)
+                    },
+                    |(mmr, mmr_root, elements, leaf_sample)| {
+                        let mut hasher = Sha256::new();
+                        let mut iter = leaf_sample.iter();
+                        let mut pos1 = iter.next().unwrap();
+                        let mut count: usize = 0;
+                        for pos2 in iter {
+                            let proof = mmr.range_proof(*pos1, *pos2);
+                            assert!(proof.verify_range_inclusion(
+                                &elements[count..count + SAMPLE_SIZE + 1],
+                                *pos1,
+                                *pos2,
+                                &mmr_root,
+                                &mut hasher
+                            ));
+                            pos1 = pos2;
+                            count += SAMPLE_SIZE;
+                        }
+                    },
+                    criterion::BatchSize::SmallInput,
+                )
+            },
+        );
     }
-    let root_hash = mmr.root_hash();
-
-    c.bench_function(
-        &format!(
-            "{}/n={} samples={}",
-            module_path!(),
-            NUM_ELEMENTS,
-            SAMPLE_SIZE
-        ),
-        |b| {
-            b.iter(|| {
-                let mut hasher = Sha256::new();
-                let mut iter = leaf_sample.iter();
-                let mut pos1 = iter.next().unwrap();
-                let mut count: usize = 0;
-                for pos2 in iter {
-                    let proof = mmr.range_proof(*pos1, *pos2);
-                    assert!(proof.verify_range_inclusion(
-                        &elements[count..count + SAMPLE_SIZE + 1],
-                        *pos1,
-                        *pos2,
-                        &root_hash,
-                        &mut hasher
-                    ));
-                    pos1 = pos2;
-                    count += SAMPLE_SIZE;
-                }
-            })
-        },
-    );
 }
 
 criterion_group! {
