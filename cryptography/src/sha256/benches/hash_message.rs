cb0aa57c02e5e7ec02f4969b185164a50926caa6
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-23 17:44:18 -0800

    fix hash benchmark

--- cryptography/src/sha256/benches/hash_message.rs
+++ cryptography/src/sha256/benches/hash_message.rs
@@ -1,7 +1,6 @@
 use commonware_cryptography::{Hasher, Sha256};
-use criterion::{criterion_group, BatchSize, Criterion};
+use criterion::{criterion_group, Criterion};
 use rand::{thread_rng, Rng};
-use std::hint::black_box;
 
 fn benchmark_hash_message(c: &mut Criterion) {
     for message_length in [100, 1000, 10000].into_iter() {
@@ -9,23 +8,11 @@ fn benchmark_hash_message(c: &mut Criterion) {
         thread_rng().fill(msg.as_mut_slice());
         let msg = msg.as_slice();
         c.bench_function(&format!("{}/msg_len={}", module_path!(), msg.len()), |b| {
-            b.iter_batched(
-                || {
-                    let mut hasher = Sha256::new();
-                    hasher.update(msg);
-                    hasher
-                },
-                |mut hasher| {
-                    black_box((|| {
-                        for _ in 0..message_length {
-                            hasher.reset();
-                            hasher.update(msg);
-                            hasher.finalize();
-                        }
-                    })());
-                },
-                BatchSize::SmallInput,
-            );
+            b.iter(|| {
+                let mut hasher = Sha256::new();
+                hasher.update(msg);
+                hasher.finalize();
+            });
         });
     }
 }
