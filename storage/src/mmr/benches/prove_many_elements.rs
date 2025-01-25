1c8774546119e4f6cec368b52a5f687a2766ef93
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-25 14:37:26 -0800

    fix benches

--- storage/src/mmr/benches/prove_many_elements.rs
+++ storage/src/mmr/benches/prove_many_elements.rs
@@ -53,11 +53,11 @@ fn bench_prove_many_elements(c: &mut Criterion) {
                             for ((start_index, end_index), (start_pos, end_pos)) in samples {
                                 let proof = mmr.range_proof(start_pos, end_pos).unwrap();
                                 assert!(proof.verify_range_inclusion(
+                                    &mut hasher,
                                     &elements[start_index..=end_index],
                                     start_pos,
                                     end_pos,
                                     &root_hash,
-                                    &mut hasher
                                 ));
                             }
                         },
