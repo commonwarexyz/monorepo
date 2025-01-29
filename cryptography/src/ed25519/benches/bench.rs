4d9dd03826a4e1962db6bd7d9719289903ed158d
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-29 10:31:04 -0800

    [cryptography/{ed25519,secp256r1] Add signature generation benchmarks (#420)

--- cryptography/src/ed25519/benches/bench.rs
+++ cryptography/src/ed25519/benches/bench.rs
@@ -2,9 +2,11 @@ use criterion::criterion_main;
 
 mod batch_verify_multiple_messages;
 mod batch_verify_multiple_public_keys;
+mod signature_generation;
 mod signature_verification;
 
 criterion_main!(
+    signature_generation::benches,
     signature_verification::benches,
     batch_verify_multiple_public_keys::benches,
     batch_verify_multiple_messages::benches,
