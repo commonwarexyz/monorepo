3157854470188841b55619565310acb349dda24c
Patrick O'Grady <me@patrickogrady.xyz>
2025-01-29 10:05:09 -0800

    use len 32 msg

--- cryptography/src/bls12381/benches/signature_generation.rs
+++ cryptography/src/bls12381/benches/signature_generation.rs
@@ -1,11 +1,12 @@
 use commonware_cryptography::{Bls12381, Scheme};
 use criterion::{criterion_group, BatchSize, Criterion};
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 use std::hint::black_box;
 
 fn benchmark_signature_generation(c: &mut Criterion) {
     let namespace = b"namespace";
-    let msg = b"hello";
+    let mut msg = [0u8; 32];
+    thread_rng().fill(&mut msg);
     c.bench_function(
         &format!(
             "{}/ns_len={} msg_len={}",
@@ -17,7 +18,7 @@ fn benchmark_signature_generation(c: &mut Criterion) {
             b.iter_batched(
                 || Bls12381::new(&mut thread_rng()),
                 |mut signer| {
-                    black_box(signer.sign(Some(namespace), msg));
+                    black_box(signer.sign(Some(namespace), &msg));
                 },
                 BatchSize::SmallInput,
             );
