b5444964ca9c70e212819150190f5b3fa334fe01
Patrick O'Grady <me@patrickogrady.xyz>
2025-02-27 07:34:22 -0700

    cleanup cli structure

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -1,72 +1,79 @@
-mod aws;
-mod services;
-mod setup;
-mod teardown;
-mod utils;
-
 use clap::{App, Arg, SubCommand};
+use commonware_utils::crate_version;
+use serde_yaml::with;
 use std::error::Error;
+
+mod ec2;
 
 /// Main entry point for the Commonware Deployer CLI
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Define CLI application structure
     let matches = App::new("deployer")
-        .version("1.0")
-        .about("Deploys a binary and config to EC2 instances across AWS regions with monitoring and logging")
+        .version(crate_version())
+        .about("TBD")
         .subcommand(
-            SubCommand::with_name("setup")
-                .about("Sets up EC2 instances and deploys files with monitoring and logging")
-                .arg(
-                    Arg::with_name("config")
-                        .long("config")
-                        .takes_value(true)
-                        .required(true)
-                        .help("Path to YAML config file"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("teardown")
-                .about("Deletes all deployed resources")
-                .arg(
-                    Arg::with_name("tag")
-                        .long("tag")
-                        .takes_value(true)
-                        .required(true)
-                        .help("Deployment tag"),
+            SubCommand::with_name("ec2")
+                .about("TBD")
+                .subcommand(
+                    SubCommand::with_name("setup")
+                        .about(
+                            "Sets up EC2 instances and deploys files with monitoring and logging",
+                        )
+                        .arg(
+                            Arg::with_name("config")
+                                .long("config")
+                                .takes_value(true)
+                                .required(true)
+                                .help("Path to YAML config file"),
+                        ),
                 )
-                .arg(
-                    Arg::with_name("config")
-                        .long("config")
-                        .takes_value(true)
-                        .required(true)
-                        .help("Path to YAML config file"),
+                .subcommand(
+                    SubCommand::with_name("teardown")
+                        .about("Deletes all deployed resources")
+                        .arg(
+                            Arg::with_name("tag")
+                                .long("tag")
+                                .takes_value(true)
+                                .required(true)
+                                .help("Deployment tag"),
+                        )
+                        .arg(
+                            Arg::with_name("config")
+                                .long("config")
+                                .takes_value(true)
+                                .required(true)
+                                .help("Path to YAML config file"),
+                        ),
                 ),
         )
         .get_matches();
 
-    // Fetch the deployer's public IP
-    let deployer_ip = reqwest::get("https://ipv4.icanhazip.com")
-        .await?
-        .text()
-        .await?
-        .trim()
-        .to_string();
-    println!("Deployer IP: {}", deployer_ip);
-
-    // Handle subcommands
-    match matches.subcommand() {
-        ("setup", Some(sub_m)) => {
-            let config_path = sub_m.value_of("config").unwrap();
-            let tag = setup::setup(config_path, &deployer_ip).await?;
-            println!("Deployment tag: {}", tag);
+    // Handle ec2 subcommands
+    if let Some(ec2_matches) = matches.subcommand_matches("ec2") {
+        // Fetch the deployer's public IP
+        let deployer_ip = reqwest::get("https://ipv4.icanhazip.com")
+            .await?
+            .text()
+            .await?
+            .trim()
+            .to_string();
+        println!("Deployer IP: {}", deployer_ip);
+        match ec2_matches.subcommand() {
+            ("setup", Some(sub_m)) => {
+                let config_path = sub_m.value_of("config").unwrap();
+                let tag = setup::setup(config_path, &deployer_ip).await?;
+                println!("Deployment tag: {}", tag);
+            }
+            ("teardown", Some(sub_m)) => {
+                let tag = sub_m.value_of("tag").unwrap();
+                let config_path = sub_m.value_of("config").unwrap();
+                teardown::teardown(tag, config_path).await?;
+            }
+            _ => println!("Invalid command. Use 'setup' or 'teardown'."),
         }
-        ("teardown", Some(sub_m)) => {
-            let tag = sub_m.value_of("tag").unwrap();
-            let config_path = sub_m.value_of("config").unwrap();
-            teardown::teardown(tag, config_path).await?;
-        }
-        _ => println!("Invalid command. Use 'setup' or 'teardown'."),
+    } else {
+        println!("Invalid command. Use 'ec2'.");
     }
 
     Ok(())
