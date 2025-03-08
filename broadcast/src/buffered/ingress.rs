8b10586182e0df942eb34d19ed3854818a4a1103
Brendan Chou <brendan@commonware.xyz>
2025-03-08 14:28:27 -0500

    add retrieval

--- broadcast/src/buffered/ingress.rs
+++ broadcast/src/buffered/ingress.rs
@@ -1,33 +1,52 @@
 use super::{Broadcaster, Digestible};
 use commonware_utils::Array;
-use futures::{channel::mpsc, SinkExt};
-use std::marker::PhantomData;
+use futures::{
+    channel::{mpsc, oneshot},
+    SinkExt,
+};
 
 /// Message types that can be sent to the `Mailbox`
-pub enum Message<B> {
-    Broadcast { blob: B },
+pub enum Message<D, B> {
+    Broadcast {
+        blob: B,
+    },
+    Retrieve {
+        digest: D,
+        responder: oneshot::Sender<B>,
+    },
 }
 
 /// Ingress mailbox for [`Engine`](super::Engine).
 #[derive(Clone)]
 pub struct Mailbox<D: Array, B: Digestible<D>> {
-    sender: mpsc::Sender<Message<B>>,
-    _digest: PhantomData<D>,
+    sender: mpsc::Sender<Message<D, B>>,
 }
 
 impl<D: Array, B: Digestible<D>> Mailbox<D, B> {
-    pub(super) fn new(sender: mpsc::Sender<Message<B>>) -> Self {
-        Self {
-            sender,
-            _digest: PhantomData,
-        }
+    pub(super) fn new(sender: mpsc::Sender<Message<D, B>>) -> Self {
+        Self { sender }
+    }
+}
+
+impl<D: Array, B: Digestible<D>> Mailbox<D, B> {
+    /// Retrieve a blob by digest.
+    pub async fn retrieve(&mut self, digest: D) -> oneshot::Receiver<B> {
+        let (sender, receiver) = oneshot::channel();
+        self.sender
+            .send(Message::Retrieve {
+                digest,
+                responder: sender,
+            })
+            .await
+            .expect("mailbox closed");
+        receiver
     }
 }
 
 impl<D: Array, B: Digestible<D>> Broadcaster for Mailbox<D, B> {
     type Blob = B;
 
-    async fn boradcast(&mut self, blob: Self::Blob) {
+    async fn broadcast(&mut self, blob: Self::Blob) {
         self.sender
             .send(Message::Broadcast { blob })
             .await
