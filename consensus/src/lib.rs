386fc79a670fa648500218e2ebd33971b95427c4
Patrick O'Grady <me@patrickogrady.xyz>
2025-04-02 17:48:44 -0700

    disable other structs for better compilation

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -5,8 +5,8 @@
 //! `commonware-consensus` is **ALPHA** software and is not yet recommended for production use. Developers should
 //! expect breaking changes and occasional instability.
 
-pub mod ordered_broadcast;
-pub mod simplex;
+// pub mod ordered_broadcast;
+// pub mod simplex;
 pub mod threshold_simplex;
 
 cfg_if::cfg_if! {
