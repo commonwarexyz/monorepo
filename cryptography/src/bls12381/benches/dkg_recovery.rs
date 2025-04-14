cc6a319b1d17716a536dd4c562c1d1a45d8856ae
Patrick O'Grady <me@patrickogrady.xyz>
2025-04-13 19:26:59 -0700

    update dkg

--- cryptography/src/bls12381/benches/dkg_recovery.rs
+++ cryptography/src/bls12381/benches/dkg_recovery.rs
@@ -4,6 +4,7 @@ use commonware_utils::quorum;
 use criterion::{criterion_group, BatchSize, Criterion};
 use rand::rngs::StdRng;
 use rand::SeedableRng;
+use rayon::ThreadPoolBuilder;
 use std::collections::HashMap;
 use std::hint::black_box;
 
@@ -11,38 +12,54 @@ fn benchmark_dkg_recovery(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(0);
     for &n in &[5, 10, 20, 50, 100, 250, 500, 1000, 1500, 2000] {
         let t = quorum(n).unwrap();
-        c.bench_function(&format!("{}/n={} t={}", module_path!(), n, t), |b| {
-            b.iter_batched(
-                || {
-                    // Create contributors
-                    let mut contributors = (0..n)
-                        .map(|i| Ed25519::from_seed(i as u64).public_key())
-                        .collect::<Vec<_>>();
-                    contributors.sort();
+        for concurrency in [1, 2, 4, 8] {
+            c.bench_function(
+                &format!("{}/conc={} n={} t={}", module_path!(), concurrency, n, t),
+                |b| {
+                    b.iter_batched(
+                        || {
+                            // Create contributors
+                            let mut contributors = (0..n)
+                                .map(|i| Ed25519::from_seed(i as u64).public_key())
+                                .collect::<Vec<_>>();
+                            contributors.sort();
 
-                    // Create player
-                    let me = contributors[0].clone();
-                    let mut player =
-                        Player::new(me, None, contributors.clone(), contributors.clone(), None);
+                            // Create thread pool
+                            let pool = ThreadPoolBuilder::new()
+                                .num_threads(concurrency)
+                                .build()
+                                .unwrap();
 
-                    // Create commitments and send shares to player
-                    let mut commitments = HashMap::new();
-                    for (idx, dealer) in contributors.iter().take(t as usize).enumerate() {
-                        let (_, commitment, shares) =
-                            Dealer::new(&mut rng, None, contributors.clone());
-                        player
-                            .share(dealer.clone(), commitment.clone(), shares[0])
-                            .unwrap();
-                        commitments.insert(idx as u32, commitment);
-                    }
-                    (player, commitments)
+                            // Create player
+                            let me = contributors[0].clone();
+                            let mut player = Player::new(
+                                me,
+                                None,
+                                contributors.clone(),
+                                contributors.clone(),
+                                Some(pool),
+                            );
+
+                            // Create commitments and send shares to player
+                            let mut commitments = HashMap::new();
+                            for (idx, dealer) in contributors.iter().take(t as usize).enumerate() {
+                                let (_, commitment, shares) =
+                                    Dealer::new(&mut rng, None, contributors.clone());
+                                player
+                                    .share(dealer.clone(), commitment.clone(), shares[0])
+                                    .unwrap();
+                                commitments.insert(idx as u32, commitment);
+                            }
+                            (player, commitments)
+                        },
+                        |(player, commitments)| {
+                            black_box(player.finalize(commitments, HashMap::new()).unwrap());
+                        },
+                        BatchSize::SmallInput,
+                    );
                 },
-                |(player, commitments)| {
-                    black_box(player.finalize(commitments, HashMap::new()).unwrap());
-                },
-                BatchSize::SmallInput,
             );
-        });
+        }
     }
 }
 
