6e3de41426bea9d1bc4c07d38f288b19a8975e27
Patrick O'Grady <me@patrickogrady.xyz>
2025-04-14 13:16:25 -0700

    fix docs

--- cryptography/src/bls12381/primitives/mod.rs
+++ cryptography/src/bls12381/primitives/mod.rs
@@ -35,7 +35,7 @@
 //! }
 //!
 //! // Aggregate partial signatures
-//! let threshold_sig = threshold_signature_recover(t, partials).unwrap();
+//! let threshold_sig = threshold_signature_recover(t, &partials).unwrap();
 //!
 //! // Verify threshold signature
 //! let threshold_pub = public(&commitment);
