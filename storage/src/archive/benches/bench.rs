74735ef0061da47133dad329b43f50541b67ccad
Patrick O'Grady <me@patrickogrady.xyz>
2025-04-26 13:55:37 -0700

    remove hashmap

--- storage/src/archive/benches/bench.rs
+++ storage/src/archive/benches/bench.rs
@@ -3,17 +3,11 @@ use criterion::criterion_main;
 mod get_random_index;
 mod get_random_key;
 mod get_sequential_index;
-mod hashmap_insert;
-mod hashmap_insert_fixed;
-mod hashmap_iteration;
 mod put;
 mod replay;
 mod utils;
 
 criterion_main!(
-    hashmap_iteration::benches,
-    hashmap_insert_fixed::benches,
-    hashmap_insert::benches,
     put::benches,
     get_sequential_index::benches,
     get_random_index::benches,
