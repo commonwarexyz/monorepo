2af26fde06b07c7e1f2df67545cc61603a78499f
Patrick O'Grady <me@patrickogrady.xyz>
2025-04-28 16:26:06 -0700

    [runtime] Remove Hand-Rolled Metrics Server + [deployer] Use `24.04` (#855)

--- runtime/src/tokio/telemetry.rs
+++ runtime/src/tokio/telemetry.rs
@@ -4,8 +4,15 @@ use super::{
     tracing::{export, Config},
     Context,
 };
-use crate::{telemetry::metrics, Metrics, Spawner};
+use crate::{Metrics, Spawner};
+use axum::{
+    body::Body,
+    http::{header, Response, StatusCode},
+    routing::get,
+    serve, Extension, Router,
+};
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 use tracing::Level;
 use tracing_subscriber::{layer::SubscriberExt, Registry};
 
@@ -26,7 +33,38 @@ pub fn init(context: Context, level: Level, metrics: Option<SocketAddr>, traces:
     if let Some(cfg) = metrics {
         context
             .with_label("metrics")
-            .spawn(move |context| async move { metrics::server::serve(context, cfg).await });
+            .spawn(move |context| async move {
+                // Create a tokio listener for the metrics server.
+                //
+                // We explicitly avoid using a runtime `Listener` because
+                // it will track bandwidth used for metrics and apply a policy
+                // for read/write timeouts fit for a p2p network.
+                let listener = TcpListener::bind(cfg)
+                    .await
+                    .expect("Failed to bind metrics server");
+
+                // Create a router for the metrics server
+                let app = Router::new()
+                    .route(
+                        "/metrics",
+                        get(|Extension(ctx): Extension<Context>| async move {
+                            Response::builder()
+                                .status(StatusCode::OK)
+                                .header(header::CONTENT_TYPE, "text/plain; version=0.0.4")
+                                .body(Body::from(ctx.encode()))
+                                .expect("Failed to create response")
+                        }),
+                    )
+                    .layer(Extension(context));
+
+                // Serve the metrics over HTTP.
+                //
+                // `serve` will spawn its own tasks using `tokio::spawn` (and there is no way to specify
+                // it to do otherwise). These tasks will not be tracked like metrics spawned using `Spawner`.
+                serve(listener, app.into_make_service())
+                    .await
+                    .expect("Could not serve metrics");
+            });
     }
 
     // Combine layers into a single subscriber
