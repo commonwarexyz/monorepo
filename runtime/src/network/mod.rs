dfdd09b03c456ce4a053ac1cc9c97e740f64edd1
Dan Laine <dan@commonware.xyz>
2025-05-01 17:14:39 -0400

    parameterize test on Network, not Context

--- runtime/src/network/mod.rs
+++ runtime/src/network/mod.rs
@@ -5,126 +5,122 @@ pub(crate) mod tokio;
 // Add this to the tests module
 #[cfg(test)]
 mod network_tests {
-    use crate::{deterministic, tokio::Runner as TokioRunner, Network as _, Runner, Spawner};
+    use crate::{tokio::Runner as TokioRunner, Network as _, Runner, Spawner};
     use crate::{Listener, Metrics, Sink, Stream};
     use futures::join;
     use std::net::SocketAddr;
+    use std::time::Duration;
 
     // TODO danlaine: remove
     const PORT_NUMBER: u16 = 5000;
 
     // Basic network connectivity test
-    fn test_network_bind_and_dial<R: Runner>(runner: R)
-    where
-        R::Context: crate::Network + Spawner,
-    {
-        runner.start(|context| async move {
-            let listener_addr = SocketAddr::from(([127, 0, 0, 1], PORT_NUMBER));
+    async fn test_network_bind_and_dial<N: crate::Network>(network: N) {
+        let listener_addr = SocketAddr::from(([127, 0, 0, 1], PORT_NUMBER));
 
-            // Start a server
-            let mut listener = context.bind(listener_addr).await.expect("Failed to bind");
+        // Start a server
+        let mut listener = network.bind(listener_addr).await.expect("Failed to bind");
 
-            // Server task
-            let server = context.clone().spawn(move |_| async move {
-                let (client_addr, mut sink, mut stream) =
-                    listener.accept().await.expect("Failed to accept");
+        let runtime = tokio::runtime::Handle::current();
 
-                // Echo server
-                let mut buf = [0u8; 10];
+        // Server task
+        let server = runtime.spawn(async move {
+            let (client_addr, mut sink, mut stream) =
+                listener.accept().await.expect("Failed to accept");
+
+            // Echo server
+            let mut buf = [0u8; 10];
+            stream.recv(&mut buf).await.expect("Failed to receive");
+            sink.send(&buf).await.expect("Failed to send");
+
+            client_addr
+        });
+
+        // Client task
+        let client = runtime.spawn(async move {
+            // Connect to the server
+            let (mut sink, mut stream) = network
+                .dial(listener_addr)
+                .await
+                .expect("Failed to dial server");
+
+            // Send and receive data
+            let data = b"Hello Test";
+            sink.send(data).await.expect("Failed to send data");
+
+            let mut buf = [0u8; 10];
+            stream.recv(&mut buf).await.expect("Failed to receive data");
+
+            assert_eq!(&buf, data);
+        });
+
+        // Wait for both tasks to complete
+        let (server_result, client_result) = join!(server, client);
+        assert!(server_result.is_ok());
+        assert!(client_result.is_ok());
+    }
+
+    // Test handling multiple clients
+    async fn test_network_multiple_clients<N: crate::Network>(network: N) {
+        const CLIENT_SEND_DATA: &'static str = "client_send_data";
+        const SERVER_SEND_DATA: &'static str = "server_send_data";
+
+        let listener_addr = SocketAddr::from(([127, 0, 0, 1], PORT_NUMBER));
+
+        let runtime = tokio::runtime::Handle::current();
+
+        // Start a server
+        let mut listener = network.bind(listener_addr).await.expect("Failed to bind");
+
+        // Server task
+        let server = runtime.spawn(async move {
+            // Handle multiple clients
+            for _ in 0..3 {
+                let (_, mut sink, mut stream) = listener.accept().await.expect("Failed to accept");
+
+                // runtime.spawn(async move {
+                let mut buf = [0u8; CLIENT_SEND_DATA.len()];
                 stream.recv(&mut buf).await.expect("Failed to receive");
-                sink.send(&buf).await.expect("Failed to send");
+                assert_eq!(&buf, CLIENT_SEND_DATA.as_bytes());
 
-                client_addr
-            });
+                sink.send(SERVER_SEND_DATA.as_bytes())
+                    .await
+                    .expect("Failed to send");
+                // });
+            }
+        });
 
-            // Client task
-            let client = context.spawn(move |context| async move {
+        // Create multiple clients
+        // let mut client_handles = Vec::new();
+
+        let client = runtime.spawn(async move {
+            for _ in 0..3 {
                 // Connect to the server
-                let (mut sink, mut stream) = context
+                let (mut sink, mut stream) = network
                     .dial(listener_addr)
                     .await
                     .expect("Failed to dial server");
 
-                // Send and receive data
-                let data = b"Hello Test";
-                sink.send(data).await.expect("Failed to send data");
+                // Send a message to the server
+                sink.send(CLIENT_SEND_DATA.as_bytes())
+                    .await
+                    .expect("Failed to send data");
 
-                let mut buf = [0u8; 10];
+                // Receive a message from the server
+                let mut buf = [0u8; SERVER_SEND_DATA.len()];
                 stream.recv(&mut buf).await.expect("Failed to receive data");
-
-                assert_eq!(&buf, data);
-            });
-
-            // Wait for both tasks to complete
-            let (server_result, client_result) = join!(server, client);
-            assert!(server_result.is_ok());
-            assert!(client_result.is_ok());
-        });
-    }
-
-    // Test handling multiple clients
-    fn test_network_multiple_clients<R: Runner>(runner: R)
-    where
-        R::Context: crate::Network + Spawner + Metrics,
-    {
-        const CLIENT_SEND_DATA: &'static str = "client_send_data";
-        const SERVER_SEND_DATA: &'static str = "server_send_data";
-
-        runner.start(|context| async move {
-            let listener_addr = SocketAddr::from(([127, 0, 0, 1], PORT_NUMBER));
-
-            // Start a server
-            let mut listener = context.bind(listener_addr).await.expect("Failed to bind");
-
-            // Server task
-            let server = context.clone().spawn(|context| async move {
-                // Handle multiple clients
-                for _ in 0..3 {
-                    let (_, mut sink, mut stream) =
-                        listener.accept().await.expect("Failed to accept");
-
-                    context.clone().spawn(|_| async move {
-                        let mut buf = [0u8; CLIENT_SEND_DATA.len()];
-                        stream.recv(&mut buf).await.expect("Failed to receive");
-                        assert_eq!(&buf, CLIENT_SEND_DATA.as_bytes());
-
-                        sink.send(SERVER_SEND_DATA.as_bytes())
-                            .await
-                            .expect("Failed to send");
-                    });
-                }
-            });
-
-            // Create multiple clients
-            let mut client_handles = Vec::new();
-            for _ in 0..3 {
-                let client = context.clone().spawn(move |context| async move {
-                    // Connect to the server
-                    let (mut sink, mut stream) = context
-                        .dial(listener_addr)
-                        .await
-                        .expect("Failed to dial server");
-
-                    // Send a message to the server
-                    sink.send(CLIENT_SEND_DATA.as_bytes())
-                        .await
-                        .expect("Failed to send data");
-
-                    // Receive a message from the server
-                    let mut buf = [0u8; SERVER_SEND_DATA.len()];
-                    stream.recv(&mut buf).await.expect("Failed to receive data");
-                    // Verify the received data
-                    assert_eq!(&buf, SERVER_SEND_DATA.as_bytes());
-                });
-                client_handles.push(client);
-            }
-
-            // Wait for server and all clients
-            server.await.expect("Server task failed");
-            for (i, handle) in client_handles.into_iter().enumerate() {
-                handle.await.expect(&format!("Client {} failed", i));
+                // Verify the received data
+                assert_eq!(&buf, SERVER_SEND_DATA.as_bytes());
             }
         });
+        // client_handles.push(client);
+
+        // Wait for server and all clients
+        server.await.expect("Server task failed");
+        // for (i, handle) in client_handles.into_iter().enumerate() {
+        //     handle.await.expect(&format!("Client {} failed", i));
+        // }
+        client.await.expect("Client task failed");
     }
 
     // // Test large data transfer
@@ -271,17 +267,19 @@ mod network_tests {
 
     // // Run the tests for each runtime implementation
 
-    #[test]
-    fn test_deterministic_network_bind_and_dial() {
-        let executor = deterministic::Runner::default();
-        test_network_bind_and_dial(executor);
-    }
+    // #[test]
+    // fn test_deterministic_network_bind_and_dial() {
+    //     let executor = deterministic::Runner::default();
+    //     test_network_bind_and_dial(executor);
+    // }
 
-    #[test]
-    fn test_deterministic_network_multiple_clients() {
-        let executor = deterministic::Runner::default();
-        test_network_multiple_clients(executor);
-    }
+    // #[test]
+    // fn test_deterministic_network_multiple_clients() {
+    //     let network = tokio::Config::default()
+    //         .with_read_timeout(Duration::from_secs(15))
+    //         .with_write_timeout(Duration::from_secs(15));
+    //     test_network_multiple_clients(network).await;
+    // }
 
     // #[test]
     // fn test_deterministic_network_large_data() {
@@ -301,16 +299,22 @@ mod network_tests {
     //     test_network_timeouts(executor);
     // }
 
-    #[test]
-    fn test_tokio_network_bind_and_dial() {
-        let executor = TokioRunner::default();
-        test_network_bind_and_dial(executor);
+    #[tokio::test]
+    async fn test_tokio_network_bind_and_dial() {
+        let network: super::tokio::Network = super::tokio::Config::default()
+            .with_read_timeout(Duration::from_secs(15))
+            .with_write_timeout(Duration::from_secs(15))
+            .into();
+        test_network_bind_and_dial(network).await;
     }
 
-    #[test]
-    fn test_tokio_network_multiple_clients() {
-        let executor = TokioRunner::default();
-        test_network_multiple_clients(executor);
+    #[tokio::test]
+    async fn test_tokio_network_multiple_clients() {
+        let network: super::tokio::Network = super::tokio::Config::default()
+            .with_read_timeout(Duration::from_secs(15))
+            .with_write_timeout(Duration::from_secs(15))
+            .into();
+        test_network_multiple_clients(network).await;
     }
 
     // #[test]
