34e20722da987b735e6350311bb5a3021a0b00d9
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-02 11:24:53 -0700

    revert

--- storage/src/index/storage.rs
+++ storage/src/index/storage.rs
@@ -154,7 +154,7 @@ impl<'a, T: Translator, V: PartialEq + Eq> Cursor<'a, T, V> {
                 });
 
                 // Set the phase to the new record.
-                self.phase = Phase::Next(new);
+                self.phase = Phase::Stale(Some(new));
                 self.collisions.inc();
             }
             Phase::Next(mut current) => {
