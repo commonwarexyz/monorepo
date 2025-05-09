92e1a2b5ac83d08df52523206f3618cb6afe0eb4
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-08 17:39:47 -0700

    add batch verify

---
+++ cryptography/src/bls12381/benches/batch_verify_multiple_public_keys.rs
@@ -0,0 +1,35 @@
+use commonware_cryptography::{bls12381::Bls12381Batch, BatchScheme, Bls12381, Signer};
+use criterion::{criterion_group, BatchSize, Criterion};
+use rand::{thread_rng, Rng};
+use std::hint::black_box;
+
+fn benchmark_batch_verify_multiple_public_keys(c: &mut Criterion) {
+    let namespace = b"namespace";
+    let mut msg = [0u8; 32];
+    thread_rng().fill(&mut msg);
+    for n_signers in [1, 10, 100, 1000, 10000].into_iter() {
+        c.bench_function(&format!("{}/pks={}", module_path!(), n_signers), |b| {
+            b.iter_batched(
+                || {
+                    let mut batch = Bls12381Batch::new();
+                    for _ in 0..n_signers {
+                        let mut signer = Bls12381::new(&mut thread_rng());
+                        let sig = signer.sign(Some(namespace), &msg);
+                        assert!(batch.add(Some(namespace), &msg, &signer.public_key(), &sig));
+                    }
+                    batch
+                },
+                |batch| {
+                    black_box(batch.verify(&mut thread_rng()));
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark_batch_verify_multiple_public_keys
+);
--- cryptography/src/bls12381/benches/bench.rs
+++ cryptography/src/bls12381/benches/bench.rs
@@ -5,6 +5,7 @@ mod aggregate_signatures;
 mod aggregate_verify_multiple_messages;
 mod aggregate_verify_multiple_public_keys;
 mod batch_verify_multiple_messages;
+mod batch_verify_multiple_public_keys;
 mod dkg_recovery;
 mod dkg_reshare_recovery;
 mod signature_generation;
@@ -22,4 +23,5 @@ criterion_main!(
     aggregate_verify_multiple_messages::benches,
     aggregate_verify_multiple_public_keys::benches,
     batch_verify_multiple_messages::benches,
+    batch_verify_multiple_public_keys::benches,
 );
