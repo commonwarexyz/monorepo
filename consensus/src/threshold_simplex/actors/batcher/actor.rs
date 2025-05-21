63e12255495a3e79b8bb9efbe0d255523232f3b2
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-21 10:27:43 -0400

    remove message histogram

--- consensus/src/threshold_simplex/actors/batcher/actor.rs
+++ consensus/src/threshold_simplex/actors/batcher/actor.rs
@@ -326,7 +326,6 @@ pub struct Actor<
     verified: Counter,
     batch_size: Histogram,
     verify_latency: histogram::Timed<E>,
-    notify_latency: histogram::Timed<E>,
 
     _phantom: PhantomData<C>,
 }
@@ -368,12 +367,6 @@ impl<
             "latency of partial signature verification",
             verify_latency.clone(),
         );
-        let notify_latency = Histogram::new(Buckets::CRYPTOGRAPHY.into_iter());
-        context.register(
-            "notify_latency",
-            "latency of notifying the consensus engine",
-            notify_latency.clone(),
-        );
         let (sender, receiver) = mpsc::channel(cfg.mailbox_size);
         let arc_context = Arc::new(context.clone());
         (
@@ -393,7 +386,6 @@ impl<
                 verified,
                 batch_size,
                 verify_latency: histogram::Timed::new(verify_latency, arc_context.clone()),
-                notify_latency: histogram::Timed::new(notify_latency, arc_context.clone()),
                 _phantom: PhantomData,
             },
             Mailbox::new(sender),
@@ -558,9 +550,7 @@ impl<
             trace!(view, batch, "batch verified messages");
             self.verified.inc_by(batch as u64);
             self.batch_size.observe(batch as f64);
-            let mut timer = self.notify_latency.timer();
             consensus.verified(voters).await;
-            timer.observe();
 
             // Block invalid signers
             if !failed.is_empty() {
