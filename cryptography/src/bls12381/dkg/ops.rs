c3a517f93efc6ae170f441ec9b6ce0c5e02eb0fb
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-20 22:38:51 -0400

    add tests for precomputed

--- cryptography/src/bls12381/dkg/ops.rs
+++ cryptography/src/bls12381/dkg/ops.rs
@@ -42,10 +42,10 @@ pub fn generate_shares<R: RngCore, V: Variant>(
     (commitment, shares)
 }
 
-/// Evaluates the polynomial at all indices.
+/// Evaluates the polynomial at all indices (0..=degree + 1).
 pub fn evaluate_all<V: Variant>(polynomial: &poly::Public<V>) -> Vec<V::Public> {
-    let mut evals = Vec::new();
-    for index in 0..=polynomial.degree() {
+    let mut evals = Vec::with_capacity(polynomial.degree() as usize);
+    for index in 0..=polynomial.degree() + 1 {
         evals.push(polynomial.evaluate(index).value);
     }
     evals
