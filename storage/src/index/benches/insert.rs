c5f0669d5c4cd9ffa4c2286c32f3c138061a42c8
Patrick O'Grady <me@patrickogrady.xyz>
2025-05-23 15:42:00 -0700

    add benchmark for index

--- storage/src/index/benches/insert.rs
+++ storage/src/index/benches/insert.rs
@@ -1,30 +1,52 @@
-use commonware_storage::rmap::RMap;
+use commonware_cryptography::hash;
+use commonware_runtime::Metrics;
+use commonware_storage::index::{translator::TwoCap, Index};
 use criterion::{criterion_group, Criterion};
+use prometheus_client::registry::Metric;
 use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 use std::time::{Duration, Instant};
 
-fn bench_put(c: &mut Criterion) {
+#[derive(Clone)]
+struct DummyMetrics;
+
+impl Metrics for DummyMetrics {
+    fn label(&self) -> String {
+        "".to_string()
+    }
+
+    fn with_label(&self, _: &str) -> Self {
+        Self
+    }
+
+    fn encode(&self) -> String {
+        "".to_string()
+    }
+
+    fn register<N: Into<String>, H: Into<String>>(&self, _: N, _: H, _: impl Metric) {}
+}
+
+fn bench_insert(c: &mut Criterion) {
     for items in [10_000, 50_000, 100_000, 500_000, 1_000_000] {
         let label = format!("{}/items={}", module_path!(), items,);
         c.bench_function(&label, |b| {
             b.iter_custom(move |iters| {
                 // Setup items
                 let mut rng = StdRng::seed_from_u64(0);
-                let mut indices = Vec::with_capacity(items);
+                let mut kvs = Vec::with_capacity(items);
                 for i in 0..items {
-                    indices.push(i as u64);
+                    kvs.push((hash(&i.to_be_bytes()), i as u64));
                 }
 
                 let mut total = Duration::ZERO;
                 for _ in 0..iters {
                     // Shuffle items and setup RMap
-                    indices.shuffle(&mut rng);
-                    let mut rmap = RMap::new();
+                    kvs.shuffle(&mut rng);
+                    let mut index = Index::init(DummyMetrics, TwoCap);
 
                     // Run benchmark
                     let start = Instant::now();
-                    for i in &indices {
-                        rmap.insert(*i);
+                    for (k, v) in &kvs {
+                        index.insert(k, *v);
                     }
                     total += start.elapsed();
                 }
@@ -37,5 +59,5 @@ fn bench_put(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = bench_put
+    targets = bench_insert
 }
