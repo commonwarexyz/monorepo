870e3acc147859061428038e533e8cb5b03bffa3
Dan Laine <dan@commonware.xyz>
2025-06-16 17:44:48 -0400

    add doc text for authenticated/mod.rs

--- p2p/src/authenticated/mod.rs
+++ p2p/src/authenticated/mod.rs
@@ -1,2 +1,11 @@
+//! Communicate with a fixed set of authenticated peers over encrypted connections.
+//!
+//! [discovery] operates under the assumption that peer addresses aren't known in
+//! advance, and that they need to be discovered. Bootstrappers are used to
+//! connect to the network and discover peers.
+//!
+//! [lookup] operates under the assumption that peer addresses are known in advance,
+//! and that they can be looked up by their identifiers.
+
 pub mod discovery;
 pub mod lookup;
