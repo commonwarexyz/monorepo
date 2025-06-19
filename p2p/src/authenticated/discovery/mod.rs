183e4a0ad3283773fea7966619e07e42a84ba5ec
Patrick O'Grady <me@patrickogrady.xyz>
2025-06-19 10:44:18 -0700

    assert no message rate limiting

--- p2p/src/authenticated/discovery/mod.rs
+++ p2p/src/authenticated/discovery/mod.rs
@@ -406,6 +406,21 @@ mod tests {
         for waiter in waiters {
             waiter.await.unwrap();
         }
+
+        // Ensure no message rate limiting occurred
+        //
+        // If a message is rate limited, it would be formatted as:
+        //
+        // ```
+        // peer-9_network_spawner_messages_rate_limited_total{peer="e2e8aa145e1ec5cb01ebfaa40e10e12f0230c832fd8135470c001cb86d77de00",message="data_0"} 1
+        // peer-9_network_spawner_messages_rate_limited_total{peer="e2e8aa145e1ec5cb01ebfaa40e10e12f0230c832fd8135470c001cb86d77de00",message="ping"} 1
+        // ```
+        let metrics = context.encode();
+        assert!(
+            !metrics.contains("messages_rate_limited_total{"),
+            "ping messages should not be rate limited: {}",
+            metrics
+        );
     }
 
     fn run_deterministic_test(seed: u64, mode: Mode) {
