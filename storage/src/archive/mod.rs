9b8c774ff93097e4311ddeb274f1df0612805dd3
Patrick O'Grady <me@patrickogrady.xyz>
2025-06-22 12:32:22 -0700

    add trait

--- storage/src/archive/mod.rs
+++ storage/src/archive/mod.rs
@@ -4,5 +4,59 @@
 //! uniquely associated with both an `index` and a `key`. This is useful for storing ordered data either
 //! [for a limited time](crate::archive::prunable) or [indefinitely](crate::archive::immutable).
 
+use commonware_codec::Codec;
+use commonware_utils::Array;
+
+use crate::identifier::Identifier;
+
 pub mod immutable;
 pub mod prunable;
+
+/// A write-once key-value store where each key is associated with a unique index.
+pub trait Archive {
+    /// The type of the index.
+    type Index;
+
+    /// The type of the key.
+    type Key: Array;
+
+    /// The type of the value.
+    type Value: Codec;
+
+    /// The type of the error.
+    type Error;
+
+    /// Store an item in [Archive]. Both indices and keys are assumed to both be globally unique.
+    ///
+    /// If the index already exists, put does nothing and returns. If the same key is stored multiple times
+    /// at different indices (not recommended), any value associated with the key may be returned.
+    async fn put(
+        &mut self,
+        index: u64,
+        key: Self::Key,
+        value: Self::Value,
+    ) -> Result<(), Self::Error>;
+
+    /// Retrieve an item from [Archive].
+    async fn get(
+        &self,
+        identifier: Identifier<'_, Self::Index, Self::Key>,
+    ) -> Result<Option<Self::Value>, Self::Error>;
+
+    /// Check if an item exists in [Archive].
+    async fn has(
+        &self,
+        identifier: Identifier<'_, Self::Index, Self::Key>,
+    ) -> Result<bool, Self::Error>;
+
+    /// Sync all pending writes.
+    async fn sync(&mut self) -> Result<(), Self::Error>;
+
+    /// Close [Archive] (and underlying storage).
+    ///
+    /// Any pending writes are synced prior to closing.
+    async fn close(self) -> Result<(), Self::Error>;
+
+    /// Remove all persistent data created by this [Archive].
+    async fn destroy(self) -> Result<(), Self::Error>;
+}
