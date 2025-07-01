28d0c922775f310c48b5b978eb0f0a68e5f76efe
Patrick O'Grady <me@patrickogrady.xyz>
2025-07-01 12:40:57 -0700

    spike on bloom

--- cryptography/src/lib.rs
+++ cryptography/src/lib.rs
@@ -13,7 +13,9 @@ pub mod bls12381;
 pub mod ed25519;
 pub mod sha256;
 pub use sha256::{hash, CoreSha256, Sha256};
+mod bloomfilter;
 pub mod secp256r1;
+pub use bloomfilter::BloomFilter;
 
 /// Produces [Signature]s over messages that can be verified with a corresponding [PublicKey].
 pub trait Signer: Send + Sync + Clone + 'static {
