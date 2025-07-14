504639c0e6801f7921c66084571d295d2a477ac4
Patrick O'Grady <me@patrickogrady.xyz>
2025-07-14 07:21:08 -0700

    cleanup benchmarks

--- coding/src/reed_solomon/benches/decode.rs
+++ coding/src/reed_solomon/benches/decode.rs
@@ -7,35 +7,43 @@ fn benchmark_decode(c: &mut Criterion) {
     let mut sampler = StdRng::seed_from_u64(0);
     let cases = [8, 12, 16, 19, 20, 24].map(|i| 2usize.pow(i));
     for data_length in cases.into_iter() {
-        let total_pieces = 7u32;
-        let min_pieces = 4u32;
-        c.bench_function(
-            &format!("{}/data_len={}", module_path!(), data_length),
-            |b| {
-                b.iter_batched(
-                    || {
-                        let mut data = vec![0u8; data_length];
-                        sampler.fill_bytes(&mut data);
-                        let (root, proofs) =
-                            encode::<Sha256>(total_pieces, min_pieces, data).unwrap();
+        for chunks in [10, 25, 50, 100, 250] {
+            let min = chunks / 3;
+            c.bench_function(
+                &format!(
+                    "{}/msg_len={} chunks={}",
+                    module_path!(),
+                    data_length,
+                    chunks
+                ),
+                |b| {
+                    b.iter_batched(
+                        || {
+                            // Generate random data
+                            let mut data = vec![0u8; data_length];
+                            sampler.fill_bytes(&mut data);
 
-                        // Select min_pieces random proofs
-                        let mut pieces = Vec::with_capacity(min_pieces as usize);
-                        let mut indices: Vec<u32> = (0..total_pieces).collect();
-                        indices.shuffle(&mut sampler);
-                        for &i in indices.iter().take(min_pieces as usize) {
-                            pieces.push(proofs[i as usize].clone());
-                        }
+                            // Encode data
+                            let (root, proofs) = encode::<Sha256>(chunks, min, data).unwrap();
 
-                        (root, pieces)
-                    },
-                    |(root, pieces)| {
-                        decode::<Sha256>(total_pieces, min_pieces, &root, pieces).unwrap();
-                    },
-                    BatchSize::SmallInput,
-                );
-            },
-        );
+                            // Select min random chunks
+                            let mut shuffled = Vec::with_capacity(min as usize);
+                            let mut indices: Vec<u32> = (0..chunks).collect();
+                            indices.shuffle(&mut sampler);
+                            for &i in indices.iter().take(min as usize) {
+                                shuffled.push(proofs[i as usize].clone());
+                            }
+
+                            (root, shuffled)
+                        },
+                        |(root, shuffled)| {
+                            decode::<Sha256>(chunks, min, &root, shuffled).unwrap();
+                        },
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
     }
 }
 
--- coding/src/reed_solomon/benches/encode.rs
+++ coding/src/reed_solomon/benches/encode.rs
@@ -7,24 +7,31 @@ fn benchmark_encode(c: &mut Criterion) {
     let mut sampler = StdRng::seed_from_u64(0);
     let cases = [8, 12, 16, 19, 20, 24].map(|i| 2usize.pow(i));
     for data_length in cases.into_iter() {
-        let total_pieces = 7u32;
-        let min_pieces = 4u32;
-        c.bench_function(
-            &format!("{}/data_len={}", module_path!(), data_length),
-            |b| {
-                b.iter_batched(
-                    || {
-                        let mut data = vec![0u8; data_length];
-                        sampler.fill_bytes(&mut data);
-                        data
-                    },
-                    |data| {
-                        encode::<Sha256>(total_pieces, min_pieces, data).unwrap();
-                    },
-                    BatchSize::SmallInput,
-                );
-            },
-        );
+        for chunks in [10, 25, 50, 100, 250] {
+            let min = chunks / 3;
+            c.bench_function(
+                &format!(
+                    "{}/msg_len={} chunks={}",
+                    module_path!(),
+                    data_length,
+                    chunks
+                ),
+                |b| {
+                    b.iter_batched(
+                        || {
+                            // Generate random data
+                            let mut data = vec![0u8; data_length];
+                            sampler.fill_bytes(&mut data);
+                            data
+                        },
+                        |data| {
+                            encode::<Sha256>(chunks, min, data).unwrap();
+                        },
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
     }
 }
 
