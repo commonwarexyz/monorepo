60c5a80a691316341fdfbbb57e51714c213aed82
Dan Laine <dan@commonware.xyz>
2025-07-15 16:13:31 -0400

    WIP change sync lower bound to be inactivity floor; change bitmap pruning boundary in Current::commit

--- storage/src/adb/current.rs
+++ storage/src/adb/current.rs
@@ -360,7 +360,11 @@ impl<
             .await?;
         self.status.sync(&mut grafter).await?;
 
-        self.status.prune_to_bit(self.any.inactivity_floor_loc);
+        let prune_loc = self
+            .any
+            .inactivity_floor_loc
+            .saturating_sub(self.any.pruning_gap);
+        self.status.prune_to_bit(prune_loc);
         self.status
             .write_pruned(
                 self.context.with_label("bitmap"),
