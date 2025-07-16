c866ab7050492eaca3b4970fcd65d0b5b34572f9
Patrick O'Grady <me@patrickogrady.xyz>
2025-07-15 17:15:23 -0700

    generate peers

--- examples/simulator/src/main.rs
+++ examples/simulator/src/main.rs
@@ -1,4 +1,9 @@
 use clap::{value_parser, Arg, Command};
+use commonware_cryptography::{ed25519, PrivateKeyExt, Signer};
+use commonware_p2p::simulated::{Config, Network};
+use commonware_runtime::{deterministic, Metrics, Runner};
+
+const DEFAULT_CHANNEL: u32 = 0;
 
 /// Returns the version of the crate.
 pub fn crate_version() -> &'static str {
@@ -14,7 +19,7 @@ fn main() {
             Arg::new("peers")
                 .long("peers")
                 .required(true)
-                .value_parser(value_parser!(u64))
+                .value_parser(value_parser!(usize))
                 .help("Number of peers to simulate"),
         )
         .arg(
@@ -33,4 +38,46 @@ fn main() {
                 .help("Message processing time in milliseconds"),
         )
         .get_matches();
+    let peers = *matches.get_one::<usize>("peers").unwrap();
+    let regions = matches
+        .get_one::<String>("regions")
+        .unwrap()
+        .split(',')
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+    assert!(
+        peers >= regions.len(),
+        "must have at least as many peers as regions"
+    );
+    let message_processing_time = *matches.get_one::<u64>("message-processing-time").unwrap();
+
+    // Configure deterministic runtime
+    let runtime_cfg = deterministic::Config::new();
+    let executor = deterministic::Runner::new(runtime_cfg);
+
+    // Start context
+    executor.start(async |context| {
+        // Initialize simulated p2p network
+        let (network, mut oracle) = Network::new(
+            context.with_label("network"),
+            Config {
+                max_size: usize::MAX,
+            },
+        );
+
+        // Start network
+        let network_handler = network.start();
+
+        // Generate peers
+        let mut identities = Vec::with_capacity(peers);
+        for i in 0..peers {
+            let identity = ed25519::PrivateKey::from_seed(i as u64).public_key();
+            let (sender, receiver) = oracle
+                .register(identity.clone(), DEFAULT_CHANNEL)
+                .await
+                .unwrap();
+            let region = regions[i % regions.len()].clone();
+            identities.push((identity, region, sender, receiver));
+        }
+    });
 }
