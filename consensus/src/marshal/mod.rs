7c63db4571e214c077be84498881774da35a3bd4
Brendan Chou <brendan@commonware.xyz>
2025-07-22 17:46:27 -0400

    try fix wasm

--- consensus/src/marshal/mod.rs
+++ consensus/src/marshal/mod.rs
@@ -3,12 +3,18 @@
 //! This module is responsible for taking the output of the consensus protocol,
 //! which may be out of order and have gaps, and ordering it into a canonical,
 //! contiguous sequence of finalized blocks. It handles fetching missing data
-//! from peers and storing the blocks and finalizations in persistent storage.
+//! from peers.
 //!
 //! The main component is the [`actor::Actor`], which drives the process.
 
-pub mod actor;
-pub mod config;
-pub mod handler;
-pub mod ingress;
 pub mod types;
+
+cfg_if::cfg_if! {
+    if #[cfg(not(target_arch = "wasm32"))] {
+        mod actor;
+        pub use actor::Actor;
+        mod config;
+        mod handler;
+        mod ingress;
+    }
+}
