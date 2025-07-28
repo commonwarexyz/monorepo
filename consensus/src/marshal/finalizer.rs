b3fa1916145615c141acab68dca049499b787eaa
Brendan Chou <brendan@commonware.xyz>
2025-07-28 16:00:21 -0400

    finalizer updates

--- consensus/src/marshal/finalizer.rs
+++ consensus/src/marshal/finalizer.rs
@@ -1,9 +1,12 @@
 use crate::{marshal::ingress::orchestrator::Orchestrator, Block, Reporter};
 use commonware_runtime::{Clock, Metrics, Spawner, Storage};
 use commonware_storage::metadata::{self, Metadata};
-use commonware_utils::array::{FixedBytes, U64};
+use commonware_utils::array::FixedBytes;
 use futures::{channel::mpsc, StreamExt};
 use tracing::{debug, error};
+
+// The key used to store the last indexed height in the metadata store.
+const LATEST_KEY: FixedBytes<1> = FixedBytes::new([0u8]);
 
 /// Requests the finalized blocks (in order) from the orchestrator, sends them to the application,
 /// waits for confirmation that the application has processed the block.
@@ -21,7 +24,7 @@ pub struct Finalizer<B: Block, R: Spawner + Clock + Metrics + Storage, Z: Report
     notifier_rx: mpsc::Receiver<()>,
 
     // Metadata store that stores the last indexed height.
-    metadata: Metadata<R, FixedBytes<1>, U64>,
+    metadata: Metadata<R, FixedBytes<1>, u64>,
 }
 
 impl<B: Block, R: Spawner + Clock + Metrics + Storage, Z: Reporter<Activity = B>>
@@ -54,58 +57,48 @@ impl<B: Block, R: Spawner + Clock + Metrics + Storage, Z: Reporter<Activity = B>
         }
     }
 
-    /// Run the finalizer.
+    /// Run the finalizer, which continuously fetches and processes finalized blocks.
     pub async fn run(mut self) {
         // Initialize last indexed from metadata store.
         // If the key does not exist, we assume the genesis block (height 0) has been indexed.
-        let latest_key = FixedBytes::new([0u8]);
-        let mut height = self.metadata.get(&latest_key).map_or(0, |x| x.to_u64());
+        let mut height = *self.metadata.get(&LATEST_KEY).unwrap_or(&0);
 
-        // Index all finalized blocks.
-        //
-        // If using state sync, this is not necessary.
+        // The main loop to process finalized blocks. This loop will hot-spin until a block is
+        // available, at which point it will process it and continue. If a block is not available,
+        // it will request a repair and wait for a notification of an update before retrying.
         loop {
-            // Check if the next block is available
             height += 1;
             if let Some(block) = self.orchestrator.get(height).await {
-                // Sanity-check that the block height matches the expected height.
+                // Sanity-check that the block height is the one we expect.
                 assert!(block.height() == height, "block height mismatch");
 
                 // Send the block to the application.
                 //
-                // Once it responds, we can both update the metadata and notify the orchestrator.
-                //
                 // After an unclean shutdown (where the finalizer metadata is not synced after some
                 // height is processed by the application), it is possible that the application may
                 // be asked to process a block it has already seen (which it can simply ignore).
-
-                // Send the block to the application.
                 let commitment = block.commitment();
                 self.application.report(block).await;
 
-                // Update metadata.
-                if let Err(e) = self
-                    .metadata
-                    .put_sync(latest_key.clone(), height.into())
-                    .await
-                {
+                // Record that we have processed up through this height.
+                if let Err(e) = self.metadata.put_sync(LATEST_KEY.clone(), height).await {
                     error!("Failed to update metadata: {e}");
                     return;
                 }
 
-                // Update last view processed (if we have a finalization for this block)
+                // Notify the orchestrator that the block has been processed.
                 self.orchestrator.processed(height, commitment).await;
 
-                // Loop again without waiting for a notification (there may be more to process)
+                // Loop again without waiting for a notification (there may be more to process).
                 continue;
             }
 
             // We've reached a height at which we have no (finalized) block.
-            // Notify the orchestrator that we're trying to access this block.
             // It may be the case that the block is not finalized yet, or that there is a gap.
+            // Notify the orchestrator that we're trying to access this block.
             self.orchestrator.repair(height).await;
 
-            // Wait for a notification that the orchestrator has updated the finalized blocks.
+            // Wait for a notification from the orchestrator that new blocks are available.
             debug!(height, "waiting to index finalized block");
             let _ = self.notifier_rx.next().await;
         }
