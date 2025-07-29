838777588ca5c44197f5cd1441ebdb70bd2b3c98
Brendan Chou <brendan@commonware.xyz>
2025-07-29 18:38:33 -0400

    wasm fix

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -12,7 +12,6 @@ pub mod aggregation;
 pub mod ordered_broadcast;
 pub mod simplex;
 pub mod threshold_simplex;
-pub mod tuple;
 
 /// Viewable is a trait that provides access to the view (round) number.
 /// Any consensus message or object that is associated with a specific view should implement this.
@@ -42,6 +41,7 @@ cfg_if::cfg_if! {
         use std::future::Future;
 
         pub mod marshal;
+        pub mod tuple;
 
         /// Histogram buckets for measuring consensus latency.
         const LATENCY: [f64; 36] = [
