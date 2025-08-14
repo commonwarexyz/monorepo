0a2fb2ebf34b19b379001e31e399500222abb8a6
Patrick O'Grady <me@patrickogrady.xyz>
2025-08-14 14:07:34 -0700

    [storage/mmr/verification] Add verify proof helper (#1405)

--- storage/src/adb/verify.rs
+++ storage/src/adb/verify.rs
@@ -30,3 +30,22 @@ pub fn extract_pinned_nodes<D: Digest>(
     let end_pos_mmr = leaf_num_to_pos(start_loc + operations_len - 1);
     proof.extract_pinned_nodes(start_pos_mmr, end_pos_mmr)
 }
+
+/// Verify that a proof is valid for a range of operations and extract all digests (and their positions)
+/// in the range of the proof.
+pub fn verify_proof_and_extract_digests<Op, H, D>(
+    hasher: &mut Standard<H>,
+    proof: &Proof<D>,
+    start_loc: u64,
+    operations: &[Op],
+    target_root: &D,
+) -> Result<Vec<(u64, D)>, crate::mmr::Error>
+where
+    Op: Encode,
+    H: Hasher<Digest = D>,
+    D: Digest,
+{
+    let start_pos = leaf_num_to_pos(start_loc);
+    let elements = operations.iter().map(|op| op.encode()).collect::<Vec<_>>();
+    proof.verify_range_inclusion_and_extract_digests(hasher, &elements, start_pos, target_root)
+}
