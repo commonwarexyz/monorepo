e6c087cf25b53b55104af6bd9d4d73b6a72750d6
Roberto Bayardo <roberto@commonware.xyz>
2025-08-16 14:26:30 -0700

    [storage/mmr] Support proving over sub-ranges, fix digest-extraction bug (#1415)

--- storage/src/mmr/verification.rs
+++ storage/src/mmr/verification.rs
@@ -37,6 +37,60 @@ pub(crate) enum ReconstructionError {
     MissingElements,
 }
 
+/// A store derived from a [Proof] that can be used to generate proofs over any sub-range of the
+/// original range.
+pub struct ProofStore<D> {
+    digests: HashMap<u64, D>,
+    size: u64,
+}
+
+impl<D: Digest> ProofStore<D> {
+    /// Create a new [ProofStore] from a valid [Proof] over the given range of elements. The
+    /// resulting store can be used to generate proofs over any sub-range of the original range.
+    /// Returns an error if the proof is invalid or could not be verified against the given root.
+    pub fn new<I, H, E>(
+        hasher: &mut H,
+        proof: &Proof<D>,
+        elements: &[E],
+        start_element_pos: u64,
+        root: &D,
+    ) -> Result<Self, Error>
+    where
+        I: CHasher<Digest = D>,
+        H: Hasher<I>,
+        E: AsRef<[u8]>,
+    {
+        let digests = proof.verify_range_inclusion_and_extract_digests(
+            hasher,
+            elements,
+            start_element_pos,
+            root,
+        )?;
+
+        Ok(ProofStore::new_from_digests(digests, proof.size))
+    }
+
+    /// Create a new [ProofStore] from the result of calling
+    /// [Proof::verify_range_inclusion_and_extract_digests]. The resulting store can be used to
+    /// generate proofs over any sub-range of the original range.
+    pub fn new_from_digests(digests: Vec<(u64, D)>, size: u64) -> Self {
+        Self {
+            digests: digests.into_iter().collect(),
+            size,
+        }
+    }
+}
+
+impl<D: Digest> Storage<D> for ProofStore<D> {
+    async fn get_node(&self, pos: u64) -> Result<Option<D>, Error> {
+        Ok(self.digests.get(&pos).cloned())
+    }
+
+    fn size(&self) -> u64 {
+        self.size
+    }
+}
+
 /// Contains the information necessary for proving the inclusion of an element, or some range of
 /// elements, in the MMR from its root digest.
 ///
@@ -272,12 +326,10 @@ impl<D: Digest> Proof<D> {
         if elements_iter.next().is_some() {
             return Err(ReconstructionError::ExtraDigests);
         }
-        let next_sibling = siblings_iter.next();
-        if (proof_digests_used == 0 && next_sibling.is_some())
-            || (next_sibling.is_some()
-                && *next_sibling.unwrap() != self.digests[proof_digests_used - 1])
-        {
-            return Err(ReconstructionError::ExtraDigests);
+        if let Some(next_sibling) = siblings_iter.next() {
+            if proof_digests_used == 0 || *next_sibling != self.digests[proof_digests_used - 1] {
+                return Err(ReconstructionError::ExtraDigests);
+            }
         }
 
         Ok(peak_digests)
@@ -519,9 +571,6 @@ where
             sibling_digests,
             collected_digests.as_deref_mut(),
         )?;
-        if let Some(ref mut collected_digests) = collected_digests {
-            collected_digests.push((left_pos, digest));
-        }
         left_digest = Some(digest);
     }
     if left_pos < range_info.rightmost_pos {
@@ -538,9 +587,6 @@ where
             sibling_digests,
             collected_digests.as_deref_mut(),
         )?;
-        if let Some(ref mut collected_digests) = collected_digests {
-            collected_digests.push((right_pos, digest));
-        }
         right_digest = Some(digest);
     }
 
@@ -555,6 +601,11 @@ where
             Some(hash) => right_digest = Some(*hash),
             None => return Err(ReconstructionError::MissingDigests),
         }
+    }
+
+    if let Some(ref mut collected_digests) = collected_digests {
+        collected_digests.push((left_pos, left_digest.unwrap()));
+        collected_digests.push((right_pos, right_digest.unwrap()));
     }
 
     Ok(hasher.node_digest(
@@ -1409,6 +1460,67 @@ mod tests {
                 .unwrap();
             let num_elements = mid_end - mid_start + 1;
             assert!(mid_range_digests.len() > num_elements);
+        });
+    }
+
+    #[test_traced]
+    fn test_verification_proof_store() {
+        let executor = deterministic::Runner::default();
+        executor.start(|_| async move {
+            // create a new MMR and add a non-trivial amount (49) of elements
+            let mut mmr = Mmr::default();
+            let mut elements = Vec::new();
+            let mut element_positions = Vec::new();
+            let mut hasher: Standard<Sha256> = Standard::new();
+            for i in 0..49 {
+                elements.push(test_digest(i));
+                element_positions.push(mmr.add(&mut hasher, elements.last().unwrap()));
+            }
+            let root = mmr.root(&mut hasher);
+
+            // Extract a ProofStore from a proof over a variety of ranges, starting with the full
+            // range and shrinking each endpoint with each iteration.
+            let mut range_start = 0;
+            let mut range_end = 48;
+            while range_start <= range_end {
+                let range_proof = mmr
+                    .range_proof(element_positions[range_start], element_positions[range_end])
+                    .await
+                    .unwrap();
+                let proof_store = ProofStore::new(
+                    &mut hasher,
+                    &range_proof,
+                    &elements[range_start..range_end + 1],
+                    element_positions[range_start],
+                    &root,
+                )
+                .unwrap();
+
+                // Verify that the ProofStore can be used to generate proofs over a host of sub-ranges
+                // starting with the full range down to a range containing a single element.
+                let mut subrange_start = range_start;
+                let mut subrange_end = range_end;
+                while subrange_start <= subrange_end {
+                    // Verify a proof over a sub-range of the original range.
+                    let sub_range_proof = Proof::<Digest>::range_proof(
+                        &proof_store,
+                        element_positions[subrange_start],
+                        element_positions[subrange_end],
+                    )
+                    .await
+                    .unwrap();
+                    assert!(sub_range_proof.verify_range_inclusion(
+                        &mut hasher,
+                        &elements[subrange_start..subrange_end + 1],
+                        element_positions[subrange_start],
+                        &root
+                    ));
+                    subrange_start += 1;
+                    subrange_end -= 1;
+                }
+                range_start += 1;
+                range_end -= 1;
+            }
         });
     }
 }
