d155528ea7a140176bf1792763d8002a5b7112a3
Denis Kolegov <denis.kolegov@asymmetric.re>
2025-08-21 11:54:34 +0200

    Improve coverage for fuzz tests

--- utils/fuzz/fuzz_targets/stable_buf.rs
+++ utils/fuzz/fuzz_targets/stable_buf.rs
@@ -7,31 +7,57 @@ use libfuzzer_sys::fuzz_target;
 
 #[derive(Arbitrary, Debug)]
 enum FuzzInput {
+    Default,
     FromVec(Vec<u8>),
     FromBytesMut(Vec<u8>),
     PutSlice(Vec<u8>),
-    Truncate(Vec<u8>, usize),
-    GetMutPtr(Vec<u8>),
-    Len(Vec<u8>),
-    IsEmpty(Vec<u8>),
-    Index(Vec<u8>, usize),
-    ConvertToBytes(Vec<u8>),
-    ConvertToVec(Vec<u8>),
+    Truncate(Vec<u8>, usize, StableBufFuzz),
+    GetMut(Vec<u8>, StableBufFuzz),
+    GetMutPtr(Vec<u8>, StableBufFuzz),
+    Len(Vec<u8>, StableBufFuzz),
+    IsEmpty(Vec<u8>, StableBufFuzz),
+    Index(Vec<u8>, usize, StableBufFuzz),
+    ConvertToBytes(Vec<u8>, StableBufFuzz),
+    ConvertToVec(Vec<u8>, StableBufFuzz),
+}
+
+#[derive(Arbitrary, Debug)]
+enum StableBufFuzz {
+    Vec,
+    BytesMut,
 }
 
 fn fuzz(input: Vec<FuzzInput>) {
     for op in input {
         match op {
+            FuzzInput::Default => {
+                let b = StableBuf::default();
+                assert_eq!(b.len(), 0);
+            }
+
             FuzzInput::FromVec(data) => {
                 let len = data.len();
-                let buf = StableBuf::from(data);
+                let buf = StableBuf::from(data.clone());
                 assert_eq!(buf.len(), len);
+
+                let stable_buf_vec = StableBuf::Vec(data.clone());
+                let stable_buf_bytes_mut = StableBuf::BytesMut(BytesMut::from(&data[..]));
+                assert_eq!(stable_buf_vec.len(), len);
+                assert_eq!(stable_buf_bytes_mut.len(), len);
+
+                let bytes_from_vec: Bytes = stable_buf_vec.into();
+                let bytes_from_bytes_mut: Bytes = stable_buf_bytes_mut.into();
+
+                assert_eq!(bytes_from_vec, bytes_from_bytes_mut);
             }
 
             FuzzInput::FromBytesMut(data) => {
                 let len = data.len();
                 let buf = BytesMut::from(data.as_slice());
                 assert_eq!(buf.len(), len);
+
+                let stable_buf: StableBuf = buf.into();
+                assert_eq!(stable_buf.len(), len);
             }
 
             FuzzInput::PutSlice(data) => {
@@ -54,16 +80,22 @@ fn fuzz(input: Vec<FuzzInput>) {
                 }
             }
 
-            FuzzInput::Truncate(data, new_len) => {
-                let mut buf = StableBuf::from(data);
+            FuzzInput::Truncate(data, new_len, kind) => {
+                let mut buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
                 if new_len <= buf.len() {
                     buf.truncate(new_len);
                     assert_eq!(buf.len(), new_len);
                 }
             }
 
-            FuzzInput::GetMutPtr(data) => {
-                let mut buf = StableBuf::from(data);
+            FuzzInput::GetMutPtr(data, kind) => {
+                let mut buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
 
                 let ptr1 = buf.as_mut_ptr();
                 let ptr2 = buf.as_mut_ptr();
@@ -75,20 +107,42 @@ fn fuzz(input: Vec<FuzzInput>) {
                 }
             }
 
-            FuzzInput::Len(data) => {
+            FuzzInput::GetMut(data, kind) => {
+                let mut buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
+                let b = buf.as_mut();
+                if b.len() > 0 {
+                    b[0] = 0;
+                    assert_eq!(buf[0], 0);
+                }
+            }
+
+            FuzzInput::Len(data, kind) => {
+                let buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
                 let len = data.len();
-                let buf = StableBuf::from(data);
                 assert_eq!(buf.len(), len);
             }
 
-            FuzzInput::IsEmpty(data) => {
+            FuzzInput::IsEmpty(data, kind) => {
                 let is_empty = data.is_empty();
-                let buf = StableBuf::from(data);
+                let buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
                 assert_eq!(buf.is_empty(), is_empty);
             }
 
-            FuzzInput::Index(data, idx) => {
-                let buf = StableBuf::from(data);
+            FuzzInput::Index(data, idx, kind) => {
+                let buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
+
                 if idx < buf.len() {
                     let byte = buf[idx];
 
@@ -97,14 +151,20 @@ fn fuzz(input: Vec<FuzzInput>) {
                 }
             }
 
-            FuzzInput::ConvertToBytes(data) => {
-                let buf = StableBuf::from(data.clone());
+            FuzzInput::ConvertToBytes(data, kind) => {
+                let buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
                 let bytes: Bytes = buf.into();
                 assert_eq!(bytes.to_vec(), data);
             }
 
-            FuzzInput::ConvertToVec(data) => {
-                let buf = StableBuf::from(data.clone());
+            FuzzInput::ConvertToVec(data, kind) => {
+                let buf = match kind {
+                    StableBufFuzz::Vec => StableBuf::Vec(data.clone()),
+                    StableBufFuzz::BytesMut => StableBuf::BytesMut(BytesMut::from(&data[..])),
+                };
                 let vec: Vec<u8> = buf.into();
                 assert_eq!(vec, data);
             }
