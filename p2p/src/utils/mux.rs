b0736d5f1680764910b38e686eb91465d05afdc7
Denis Kolegov <denis.kolegov@asymmetric.re>
2025-08-29 17:43:11 +0200

    ignore blocks

--- p2p/src/utils/mux.rs
+++ p2p/src/utils/mux.rs
@@ -296,6 +296,7 @@ mod tests {
             context.with_label("network"),
             SimConfig {
                 max_size: 1024 * 1024,
+                ignore_blocks: true
             },
         );
         network.start();
