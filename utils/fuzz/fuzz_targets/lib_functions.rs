d2d18feee5528efb2f3229f04d20a90fb80acae1
Denis Kolegov <denis.kolegov@asymmetric.re>
2025-09-03 15:05:11 +0200

    fmt

--- utils/fuzz/fuzz_targets/lib_functions.rs
+++ utils/fuzz/fuzz_targets/lib_functions.rs
@@ -70,11 +70,9 @@ fn fuzz(input: FuzzInput) {
         FuzzInput::FromHex { hex_str } => {
             if !hex_str.is_empty() && !hex_str.chars().any(|c| c.is_ascii_hexdigit()) {
                 assert_eq!(from_hex(&hex_str), None)
-            } else {
-                if let Some(decoded) = from_hex(&hex_str) {
-                    let re_encoded = hex(&decoded);
-                    assert_eq!(from_hex(&re_encoded), Some(decoded));
-                }
+            } else if let Some(decoded) = from_hex(&hex_str) {
+                let re_encoded = hex(&decoded);
+                assert_eq!(from_hex(&re_encoded), Some(decoded));
             }
         }
 
