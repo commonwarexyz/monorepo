02cd0849af1565b3dc02b27387c0e27dc9268b8f
Lucas Meier <lucas@cronokirby.com>
2025-09-10 12:43:32 -0700

    [cryptography] handshake: move cipher to separate module

--- cryptography/src/handshake.rs
+++ cryptography/src/handshake.rs
@@ -1,4 +1,3 @@
-use chacha20poly1305::{aead::AeadMut, ChaCha20Poly1305, KeyInit};
 use commonware_codec::Encode;
 use rand_core::CryptoRngCore;
 
@@ -9,6 +8,9 @@ use crate::{
 
 mod key_exchange;
 use key_exchange::{EphemeralPublicKey, SecretKey};
+
+mod cipher;
+pub use cipher::{RecvCipher, SendCipher};
 
 const NAMESPACE: &'static [u8] = b"commonware/handshake";
 const LABEL_CIPHER_L2D: &'static [u8] = b"cipher_l2d";
@@ -43,69 +45,6 @@ pub struct ListenState {
     confirmation: Summary,
     send: SendCipher,
     recv: RecvCipher,
-}
-
-struct CounterNonce {
-    inner: u128,
-}
-
-impl CounterNonce {
-    pub fn new() -> Self {
-        Self { inner: 0 }
-    }
-
-    pub fn inc(&mut self) -> [u8; 16] {
-        if self.inner >= 1 << 96 {
-            panic!("overflowed nonce");
-        }
-        let out = self.inner.to_le_bytes();
-        self.inner += 1;
-        out
-    }
-}
-
-pub struct SendCipher {
-    nonce: CounterNonce,
-    inner: ChaCha20Poly1305,
-}
-
-impl SendCipher {
-    fn new(mut rng: impl CryptoRngCore) -> Self {
-        let mut key = [0u8; 32];
-        rng.fill_bytes(&mut key[..]);
-        Self {
-            nonce: CounterNonce::new(),
-            inner: ChaCha20Poly1305::new(&key.into()),
-        }
-    }
-
-    pub fn send(&mut self, data: &[u8]) -> Vec<u8> {
-        self.inner
-            .encrypt((&self.nonce.inc()[..12]).into(), data)
-            .unwrap()
-    }
-}
-
-pub struct RecvCipher {
-    nonce: CounterNonce,
-    inner: ChaCha20Poly1305,
-}
-
-impl RecvCipher {
-    fn new(mut rng: impl CryptoRngCore) -> Self {
-        let mut key = [0u8; 32];
-        rng.fill_bytes(&mut key[..]);
-        Self {
-            nonce: CounterNonce::new(),
-            inner: ChaCha20Poly1305::new(&key.into()),
-        }
-    }
-
-    pub fn recv(&mut self, encrypted_data: &[u8]) -> Vec<u8> {
-        self.inner
-            .decrypt((&self.nonce.inc()[..12]).into(), encrypted_data)
-            .unwrap()
-    }
 }
 
 pub struct Context<S, P> {
