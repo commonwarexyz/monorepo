88829ceaa301a7b00eb68e68e60bcccddf25d065
Lucas Meier <lucas@cronokirby.com>
2025-09-16 19:49:08 -0700

    [cryptography] add benchmarks for handshake

---
+++ cryptography/src/handshake/benches/handshake.rs
@@ -0,0 +1,9 @@
+use criterion::{criterion_group, Criterion};
+
+fn bench_connect(c: &mut Criterion) {
+    c.bench_function(&format!("{}", module_path!()), |b| {
+        b.iter(|| super::connect().unwrap())
+    });
+}
+
+criterion_group!(benches, bench_connect);
