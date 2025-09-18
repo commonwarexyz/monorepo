46dad6f722b23d1cec1457360060ab7aa71f751e
Roberto Bayardo <roberto@commonware.xyz>
2025-09-18 06:58:15 -0700

    make insert benchmark use both index types

--- storage/src/index/benches/insert.rs
+++ storage/src/index/benches/insert.rs
@@ -1,7 +1,7 @@
-use commonware_cryptography::{Hasher as _, Sha256};
+use commonware_cryptography::{Hasher, Sha256};
 use commonware_runtime::Metrics;
 use commonware_storage::{
-    index::{Index as _, Unordered as Index},
+    index::{Index, Ordered, Unordered},
     translator::TwoCap,
 };
 use criterion::{criterion_group, Criterion};
@@ -33,35 +33,46 @@ impl Metrics for DummyMetrics {
     fn register<N: Into<String>, H: Into<String>>(&self, _: N, _: H, _: impl Metric) {}
 }
 
-// TODO: Also benchmark OrderedIndex.
 fn bench_insert(c: &mut Criterion) {
-    for items in N_ITEMS {
-        let label = format!("{}/items={}", module_path!(), items,);
-        c.bench_function(&label, |b| {
-            b.iter_custom(move |iters| {
-                // Setup items
-                let mut rng = StdRng::seed_from_u64(0);
-                let mut kvs = Vec::with_capacity(items);
-                for i in 0..items {
-                    kvs.push((Sha256::hash(&i.to_be_bytes()), i as u64));
-                }
-
-                let mut total = Duration::ZERO;
-                for _ in 0..iters {
-                    // Shuffle items and setup Index
-                    kvs.shuffle(&mut rng);
-                    let mut index = Index::init(DummyMetrics, TwoCap);
-
-                    // Run benchmark
-                    let start = Instant::now();
-                    for (k, v) in &kvs {
-                        index.insert(k, *v);
+    for index in ["ordered", "unordered"] {
+        for items in N_ITEMS {
+            let label = format!("{}/items={},index={}", module_path!(), items, index);
+            c.bench_function(&label, |b| {
+                b.iter_custom(move |iters| {
+                    // Setup items
+                    let mut rng = StdRng::seed_from_u64(0);
+                    let mut kvs = Vec::with_capacity(items);
+                    for i in 0..items {
+                        kvs.push((Sha256::hash(&i.to_be_bytes()), i as u64));
                     }
-                    total += start.elapsed();
-                }
-                total
+
+                    let mut total = Duration::ZERO;
+                    for _ in 0..iters {
+                        // Shuffle items and setup Index
+                        kvs.shuffle(&mut rng);
+                        let start = Instant::now();
+                        if index == "ordered" {
+                            let mut index = Ordered::init(DummyMetrics, TwoCap);
+                            run_benchmark(&mut index, &kvs);
+                        } else {
+                            let mut index = Unordered::init(DummyMetrics, TwoCap);
+                            run_benchmark(&mut index, &kvs);
+                        };
+                        total += start.elapsed();
+                    }
+                    total
+                });
             });
-        });
+        }
+    }
+}
+
+fn run_benchmark<I: Index<Value = u64>>(
+    index: &mut I,
+    kvs: &Vec<(<Sha256 as Hasher>::Digest, u64)>,
+) {
+    for (k, v) in kvs {
+        index.insert(k, *v);
     }
 }
 
