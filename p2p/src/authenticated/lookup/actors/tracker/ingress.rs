3e34652347f731e8f64592669af2bdec62fd656a
André Silva <andrerfosilva@gmail.com>
2025-09-29 15:41:54 +0100

    [p2p] fix docs

--- p2p/src/authenticated/lookup/actors/tracker/ingress.rs
+++ p2p/src/authenticated/lookup/actors/tracker/ingress.rs
@@ -47,7 +47,7 @@ pub enum Message<C: PublicKey> {
 
     /// Request a reservation for a particular peer to dial.
     ///
-    /// The tracker will respond with an [Option<Reservation<C>>], which will be `None` if the
+    /// The tracker will respond with an [`Option<Reservation<C>>`], which will be `None` if the
     /// reservation cannot be granted (e.g., if the peer is already connected, blocked or already
     /// has an active reservation).
     Dial {
@@ -70,7 +70,7 @@ pub enum Message<C: PublicKey> {
 
     /// Request a reservation for a particular peer.
     ///
-    /// The tracker will respond with an [Option<Reservation<C>>], which will be `None` if  the
+    /// The tracker will respond with an [`Option<Reservation<C>>`], which will be `None` if  the
     /// reservation cannot be granted (e.g., if the peer is already connected, blocked or already
     /// has an active reservation).
     Listen {
