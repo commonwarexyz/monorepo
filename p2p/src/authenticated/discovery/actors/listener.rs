83adae76e2b08c91c5302917dca6adf0d79b0cfc
Patrick O'Grady <me@patrickogrady.xyz>
2025-09-30 14:03:31 -0700

    spike

--- p2p/src/authenticated/discovery/actors/listener.rs
+++ p2p/src/authenticated/discovery/actors/listener.rs
@@ -156,7 +156,7 @@ impl<E: Spawner + Clock + ReasonablyRealtime + Network + Rng + CryptoRng + Metri
             .expect("failed to bind listener");
 
         // Loop over incoming connections as fast as our rate limiter allows
-        let mut limited = 0;
+        let mut processed = 0;
         loop {
             // Accept a new connection
             let (address, sink, stream) = match listener.accept().await {
@@ -169,25 +169,33 @@ impl<E: Spawner + Clock + ReasonablyRealtime + Network + Rng + CryptoRng + Metri
             debug!(?address, "accepted incoming connection");
 
             // Cleanup the rate limiters periodically
-            if limited > CLEANUP_INTERVAL {
+            if processed > CLEANUP_INTERVAL {
                 self.ip_rate_limiter.shrink_to_fit();
                 self.subnet_rate_limiter.shrink_to_fit();
-                limited = 0;
+                processed = 0;
             }
+            processed += 1;
 
             // Drop the connection if the IP exceeds its rate limit
             let ip = address.ip();
-            if self.ip_rate_limiter.check_key(&ip).is_err() {
-                limited += 1;
+            let ip_permitted = if self.ip_rate_limiter.check_key(&ip).is_err() {
                 self.handshakes_ip_rate_limited.inc();
                 debug!(?address, "ip exceeded handshake rate limit");
-                continue;
-            }
+
+                false
+            } else {
+                true
+            };
             let subnet = ip.subnet();
-            if self.subnet_rate_limiter.check_key(&subnet).is_err() {
-                limited += 1;
+            let subnet_permitted = if self.subnet_rate_limiter.check_key(&subnet).is_err() {
                 self.handshakes_subnet_rate_limited.inc();
                 debug!(?address, "subnet exceeded handshake rate limit");
+
+                false
+            } else {
+                true
+            };
+            if ip_permitted && subnet_permitted {
                 continue;
             }
 
