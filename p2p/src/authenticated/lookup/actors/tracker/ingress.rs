01a73c779e2d5a67bed167fc35acec22fdb7a5e0
Patrick O'Grady <me@patrickogrady.xyz>
2025-10-02 10:33:26 -0700

    nit

--- p2p/src/authenticated/lookup/actors/tracker/ingress.rs
+++ p2p/src/authenticated/lookup/actors/tracker/ingress.rs
@@ -43,7 +43,7 @@ pub enum Message<C: PublicKey> {
 
     /// Request a reservation for a particular peer to dial.
     ///
-    /// The tracker will respond with an [Option<Reservation<C>>], which will be `None` if the
+    /// The tracker will respond with an [`Option<Reservation<C>>`], which will be `None` if the
     /// reservation cannot be granted (e.g., if the peer is already connected, blocked or already
     /// has an active reservation).
     Dial {
@@ -66,7 +66,7 @@ pub enum Message<C: PublicKey> {
 
     /// Request a reservation for a particular peer.
     ///
-    /// The tracker will respond with an [Option<Reservation<C>>], which will be `None` if  the
+    /// The tracker will respond with an [`Option<Reservation<C>>`], which will be `None` if  the
     /// reservation cannot be granted (e.g., if the peer is already connected, blocked or already
     /// has an active reservation).
     Listen {
