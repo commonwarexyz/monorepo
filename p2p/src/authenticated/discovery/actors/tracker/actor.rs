2e3165782aa0f12c69403f89f8f089edf44c59b7
Patrick O'Grady <me@patrickogrady.xyz>
2025-10-07 15:20:00 -0700

    basic impl

--- p2p/src/authenticated/discovery/actors/tracker/actor.rs
+++ p2p/src/authenticated/discovery/actors/tracker/actor.rs
@@ -11,7 +11,7 @@ use crate::authenticated::{
     mailbox::UnboundedMailbox,
 };
 use commonware_cryptography::Signer;
-use commonware_runtime::{Clock, Handle, Metrics as RuntimeMetrics, Spawner};
+use commonware_runtime::{Clock, ContextSlot, Handle, Metrics as RuntimeMetrics, Spawner};
 use commonware_utils::{union, SystemTimeExt};
 use futures::{channel::mpsc, StreamExt};
 use governor::clock::Clock as GClock;
@@ -23,7 +23,7 @@ const NAMESPACE_SUFFIX_IP: &[u8] = b"_IP";
 
 /// The tracker actor that manages peer discovery and connection reservations.
 pub struct Actor<E: Spawner + Rng + Clock + GClock + RuntimeMetrics, C: Signer> {
-    context: E,
+    context: ContextSlot<E>,
 
     // ---------- Configuration ----------
     /// For signing and verifying messages.
@@ -98,7 +98,7 @@ impl<E: Spawner + Rng + Clock + GClock + RuntimeMetrics, C: Signer> Actor<E, C> 
 
         (
             Self {
-                context,
+                context: ContextSlot::Present(context),
                 crypto: cfg.crypto,
                 max_peer_set_size: cfg.max_peer_set_size,
                 peer_gossip_max_count: cfg.peer_gossip_max_count,
@@ -112,26 +112,11 @@ impl<E: Spawner + Rng + Clock + GClock + RuntimeMetrics, C: Signer> Actor<E, C> 
     }
 
     /// Start the actor and run it in the background.
-    pub fn start(self) -> Handle<()> {
-        let Self {
-            context,
-            receiver,
-            crypto,
-            max_peer_set_size,
-            peer_gossip_max_count,
-            directory,
-        } = self;
+    pub fn start(mut self) -> Handle<()> {
+        let context = self.context.take();
         context.spawn(move |context| async move {
-            Self {
-                context,
-                receiver,
-                crypto,
-                max_peer_set_size,
-                peer_gossip_max_count,
-                directory,
-            }
-            .run()
-            .await;
+            self.context.return_context(context);
+            self.run().await;
         })
     }
 
