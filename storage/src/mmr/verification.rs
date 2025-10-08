3e14e905b3a008a6d4be081654cb59d12a911a34
Dan Laine <dan@commonware.xyz>
2025-10-08 16:31:19 -0400

    WIP prevent Location -> Position overflows

--- storage/src/mmr/verification.rs
+++ storage/src/mmr/verification.rs
@@ -96,9 +96,10 @@ pub async fn range_proof<D: Digest, S: Storage<D>>(
 ///
 /// # Errors
 ///
-/// - Returns [Error::LocationOverflow] if any location in `range` > [crate::mmr::MAX_LOCATION]
-/// - Returns [Error::ElementPruned] if some element needed to generate the proof has been pruned
-/// - Returns [Error::Empty] if the requested range is empty
+/// Returns [Error::LocationOverflow] if any location in `range` > [crate::mmr::MAX_LOCATION]
+/// Returns [Error::RangeOutOfBounds] if any location in `range` > `size`
+/// Returns [Error::ElementPruned] if some element needed to generate the proof has been pruned
+/// Returns [Error::Empty] if the requested range is empty
 pub async fn historical_range_proof<D: Digest, S: Storage<D>>(
     mmr: &S,
     size: Position,
