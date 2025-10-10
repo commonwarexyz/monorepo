869c3e9248b6eb909d10eb2472506f4440ee9f79
Roberto Bayardo <roberto@commonware.xyz>
2025-10-10 06:57:01 -0700

    remove todo

--- storage/fuzz/fuzz_targets/adb_operations.rs
+++ storage/fuzz/fuzz_targets/adb_operations.rs
@@ -5,7 +5,7 @@ use commonware_cryptography::Sha256;
 use commonware_runtime::{buffer::PoolRef, deterministic, Runner};
 use commonware_storage::{
     adb::{
-        any::fixed::{unordered::Any, Config}, // TODO: test Ordered as well
+        any::fixed::{unordered::Any, Config},
         verify_proof,
     },
     mmr::{Location, StandardHasher as Standard},
