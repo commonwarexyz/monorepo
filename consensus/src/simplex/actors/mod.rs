93c3a5dee65e852f5a10ca7c1f0d98bd2973b363
André Silva <andrerfosilva@gmail.com>
2025-10-11 19:33:04 +0100

    [consensus/simplex] good night sweet prince

--- consensus/src/simplex/actors/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod resolver;
-pub mod voter;
--- consensus/src/simplex/mocks/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Collection of mocks used to test `simplex`.
-
-pub mod application;
-pub mod conflicter;
-pub mod nuller;
-pub mod outdated;
-pub mod reconfigurer;
-pub mod relay;
-pub mod supervisor;
