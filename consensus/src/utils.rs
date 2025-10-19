5f9ae0823b4e0dc280f3b52708cd23904c489269
Patrick O'Grady <me@patrickogrady.xyz>
2025-10-19 14:29:07 -0700

    add comment on epoch organization

--- consensus/src/utils.rs
+++ consensus/src/utils.rs
@@ -4,6 +4,15 @@ use crate::types::Epoch;
 
 /// Returns the epoch the given height belongs to.
 ///
+/// Epochs are organized as follows:
+///
+/// ```txt
+/// 0: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+/// 1: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+/// 2: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+/// ...
+/// ```
+///
 /// Epoch length is defined in number of blocks. Panics if `epoch_length` is
 /// zero.
 #[inline]
