cee3d1147b97dec134908be63b536a1c4732e259
Patrick O'Grady <me@patrickogrady.xyz>
2025-10-28 18:45:56 -0700

    nits

--- consensus/fuzz/fuzz_targets/simplex_select_leader.rs
+++ consensus/fuzz/fuzz_targets/simplex_select_leader.rs
@@ -1,10 +1,14 @@
 #![no_main]
 use arbitrary::Arbitrary;
-use commonware_codec::Decode;
+use commonware_codec::DecodeExt;
 use commonware_consensus::{
     simplex::{
         select_leader,
-        signing_scheme::{bls12381_threshold::Seed, Scheme},
+        signing_scheme::{
+            bls12381_multisig,
+            bls12381_threshold::{self, Seed},
+            ed25519, Scheme,
+        },
     },
     types::Round,
 };
@@ -15,6 +19,12 @@ use commonware_cryptography::{
 };
 use libfuzzer_sys::fuzz_target;
 use rand::{rngs::StdRng, SeedableRng};
+
+type Ed25519Scheme = ed25519::Scheme;
+type Bls12381MultisigMinPk = bls12381_multisig::Scheme<PublicKey, MinPk>;
+type Bls12381MultisigMinSig = bls12381_multisig::Scheme<PublicKey, MinSig>;
+type Bls12381ThresholdMinPk = bls12381_threshold::Scheme<PublicKey, MinPk>;
+type Bls12381ThresholdMinSig = bls12381_threshold::Scheme<PublicKey, MinSig>;
 
 #[derive(Arbitrary, Debug)]
 enum FuzzScheme {
@@ -33,16 +43,6 @@ struct FuzzInput {
     scheme: FuzzScheme,
     encoded_seed: Vec<u8>,
 }
-
-type Ed25519Scheme = commonware_consensus::simplex::signing_scheme::ed25519::Scheme;
-type Bls12381MultisigMinPk =
-    commonware_consensus::simplex::signing_scheme::bls12381_multisig::Scheme<PublicKey, MinPk>;
-type Bls12381MultisigMinSig =
-    commonware_consensus::simplex::signing_scheme::bls12381_multisig::Scheme<PublicKey, MinSig>;
-type Bls12381ThresholdMinPk =
-    commonware_consensus::simplex::signing_scheme::bls12381_threshold::Scheme<PublicKey, MinPk>;
-type Bls12381ThresholdMinSig =
-    commonware_consensus::simplex::signing_scheme::bls12381_threshold::Scheme<PublicKey, MinSig>;
 
 fn fuzz<S: Scheme>(input: &FuzzInput, seed: Option<S::Seed>) {
     let participants: Vec<PublicKey> = (1..=input.participants_count)
@@ -66,11 +66,11 @@ fuzz_target!(|input: FuzzInput| {
             fuzz::<Ed25519Scheme>(&input, None);
         }
         FuzzScheme::Bls12381ThresholdMinPk => {
-            let seed = Seed::<MinPk>::decode_cfg(input.encoded_seed.as_slice(), &()).ok();
+            let seed = Seed::<MinPk>::decode(input.encoded_seed.as_slice()).ok();
             fuzz::<Bls12381ThresholdMinPk>(&input, seed);
         }
         FuzzScheme::Bls12381ThresholdMinSig => {
-            let seed = Seed::<MinSig>::decode_cfg(input.encoded_seed.as_slice(), &()).ok();
+            let seed = Seed::<MinSig>::decode(input.encoded_seed.as_slice()).ok();
             fuzz::<Bls12381ThresholdMinSig>(&input, seed);
         }
         FuzzScheme::Bls12381MultisigMinPk => {
