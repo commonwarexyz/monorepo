04870b267ec44abd933437bf2f5ff70cc6e57313
Dan Laine <dan@commonware.xyz>
2025-11-04 10:29:57 -0500

    remove authenticated journal from adb/mod.rs

--- storage/src/adb/mod.rs
+++ storage/src/adb/mod.rs
@@ -33,7 +33,6 @@ pub mod operation;
 pub mod store;
 pub mod sync;
 pub mod verify;
-pub use crate::journal::authenticated::Journal;
 pub use verify::{
     create_multi_proof, create_proof, create_proof_store, create_proof_store_from_digests,
     digests_required_for_proof, extract_pinned_nodes, verify_multi_proof, verify_proof,
