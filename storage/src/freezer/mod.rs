94e564e7a170053ac1ceac635ac0d411378c6b1f
FT <140458077+zeevick10@users.noreply.github.com>
2025-11-12 15:25:55 +0100

    Update mod.rs

--- storage/src/freezer/mod.rs
+++ storage/src/freezer/mod.rs
@@ -10,7 +10,7 @@
 //!
 //! # Format
 //!
-//! The [Freezer] uses a two-level architecture: an extendible hash table (written in a single [commonware_runtime::Blob])
+//! The [Freezer] uses a two-level architecture: an Extendable hash table (written in a single [commonware_runtime::Blob])
 //! that maps keys to locations and a [crate::journal::segmented::variable::Journal] that stores key-value data.
 //!
 //! ```text
