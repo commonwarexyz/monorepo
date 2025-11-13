1d5d2c350ec8273bb263d5a5e38fd8f5df063062
Patrick O'Grady <me@patrickogrady.xyz>
2025-11-12 18:56:54 -0800

    require no_std

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -20,7 +20,9 @@ use core::{
 
 pub mod sequence;
 pub use sequence::{Array, Span};
+#[cfg(feature = "std")]
 pub mod acknowledgement;
+#[cfg(feature = "std")]
 pub use acknowledgement::Acknowledgement;
 pub mod bitmap;
 #[cfg(feature = "std")]
