89207b6839e6824d83b560566ffef05ca54592b1
Roberto Bayardo <roberto@commonware.xyz>
2025-11-15 12:37:38 -0800

    fix lint

--- storage/src/adb/store/mod.rs
+++ storage/src/adb/store/mod.rs
@@ -29,7 +29,7 @@
 //!
 //! ```rust
 //! use commonware_storage::{
-//!     adb::store::{Config, Store},
+//!     adb::store::{Config, Store, Db as _},
 //!     translator::TwoCap,
 //! };
 //! use commonware_utils::{NZUsize, NZU64};
