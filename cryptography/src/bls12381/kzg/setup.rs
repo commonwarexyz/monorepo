09d8696037876c29f3670ccba0a337876e33962c
Patrick O'Grady <me@patrickogrady.xyz>
2025-11-22 12:14:11 -0800

    cleanup new

--- cryptography/src/bls12381/kzg/setup.rs
+++ cryptography/src/bls12381/kzg/setup.rs
@@ -41,33 +41,21 @@ impl Ethereum {
     ///
     /// Source: <https://github.com/ethereum/consensus-specs/blob/6070972f148bc3d9417e90418f97cb7f5a9a6417/presets/mainnet/trusted_setups/trusted_setup_4096.json>
     pub fn new() -> Self {
-        Self::from_bytes(&ETHEREUM_SETUP)
-    }
-
-    fn from_bytes(mut raw: &[u8]) -> Self {
-        // Read G1 count
-        let g1_count = u32::from_be_bytes(raw[0..4].try_into().unwrap()) as usize;
-        raw = &raw[4..];
+        let mut cursor = &ETHEREUM_SETUP[..];
 
         // Read G1 powers
+        let g1_count = u32::read(&mut cursor).unwrap() as usize;
         let mut g1_powers = Vec::with_capacity(g1_count);
         for _ in 0..g1_count {
-            let (mut bytes, rest) = raw.split_at(48);
-            raw = rest;
-            let g1 = G1::read(&mut bytes).expect("invalid g1 point");
+            let g1 = G1::read(&mut cursor).expect("invalid g1 point");
             g1_powers.push(g1);
         }
 
-        // Read G2 count
-        let g2_count = u32::from_be_bytes(raw[0..4].try_into().unwrap()) as usize;
-        raw = &raw[4..];
-
         // Read G2 powers
+        let g2_count = u32::read(&mut cursor).unwrap() as usize;
         let mut g2_powers = Vec::with_capacity(g2_count);
         for _ in 0..g2_count {
-            let (mut bytes, rest) = raw.split_at(96);
-            let g2 = G2::read(&mut bytes).expect("invalid g2 point");
-            raw = rest;
+            let g2 = G2::read(&mut cursor).expect("invalid g2 point");
             g2_powers.push(g2);
         }
 
@@ -101,7 +89,7 @@ mod tests {
     #[test]
     fn test_ethereum_kzg() {
         let setup = Ethereum::new();
-        assert_eq!(setup.g1_powers().len(), 4096, "Expected 4096 G1 powers");
-        assert_eq!(setup.g2_powers().len(), 65, "Expected 65 G2 powers");
+        assert_eq!(setup.g1_powers().len(), 4096, "unexpected G1 powers");
+        assert_eq!(setup.g2_powers().len(), 65, "unexpected G2 powers");
     }
 }
