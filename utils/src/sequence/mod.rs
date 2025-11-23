42a50e643d7c3a654fe68d4751f4ce002541880b
clabby <ben@clab.by>
2025-11-23 15:26:39 -0500

    [utils] Add `Unit` marker type

--- utils/src/sequence/mod.rs
+++ utils/src/sequence/mod.rs
@@ -15,6 +15,8 @@ pub use u64::U64;
 pub mod prefixed_u64;
 pub mod u32;
 pub use u32::U32;
+pub mod unit;
+pub use unit::Unit;
 
 /// Errors returned by the `Array` trait's functions.
 #[derive(Error, Debug, PartialEq)]
