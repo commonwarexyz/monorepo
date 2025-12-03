95e7f9fc6cfe0794b75f9ec4ae7cd4d97c16b95b
Lucas Meier <lucas@cronokirby.com>
2025-12-03 12:02:52 -0800

    [math] Add no_std support

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,4 +1,10 @@
 //! Mathematical utilities and operations.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+
 pub mod algebra;
 pub mod fields {
     pub mod goldilocks;
