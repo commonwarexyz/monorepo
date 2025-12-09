338a858e50ebcff12aedd7b1a5b1757bcfec0817
Patrick O'Grady <me@patrickogrady.xyz>
2025-12-08 17:25:31 -0800

    run fmt

--- p2p/src/authenticated/lookup/mod.rs
+++ p2p/src/authenticated/lookup/mod.rs
@@ -988,9 +988,10 @@ mod tests {
             );
 
             // Now register a peer set that DOES include self
-            let peer_set: Map<_, _> = [(self_pk.clone(), self_addr), (other_pk.clone(), other_addr)]
-                .try_into()
-                .unwrap();
+            let peer_set: Map<_, _> =
+                [(self_pk.clone(), self_addr), (other_pk.clone(), other_addr)]
+                    .try_into()
+                    .unwrap();
             oracle.update(2, peer_set.clone()).await;
 
             // Receive subscription notification
