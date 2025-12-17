86aee30fe52a5ea546d4edec86568971e7b41142
André Silva <andrerfosilva@gmail.com>
2025-12-17 15:43:12 +0000

    [consensus/simplex] fix mocks

--- consensus/src/simplex/mocks/equivocator.rs
+++ consensus/src/simplex/mocks/equivocator.rs
@@ -3,8 +3,8 @@
 use super::relay::Relay;
 use crate::{
     simplex::{
+        elector::Elector,
         scheme::Scheme,
-        select_leader,
         types::{Certificate, Notarize, Proposal, Vote},
     },
     types::{Epoch, Round, View},
@@ -16,17 +16,19 @@ use commonware_runtime::{spawn_cell, Clock, ContextCell, Handle, Spawner};
 use rand::{seq::IteratorRandom, Rng};
 use std::{collections::HashSet, sync::Arc};
 
-pub struct Config<S: certificate::Scheme, H: Hasher> {
+pub struct Config<S: certificate::Scheme, L: Elector<S>, H: Hasher> {
     pub scheme: S,
+    pub elector: L,
     pub namespace: Vec<u8>,
     pub epoch: Epoch,
     pub relay: Arc<Relay<H::Digest, S::PublicKey>>,
     pub hasher: H,
 }
 
-pub struct Equivocator<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> {
+pub struct Equivocator<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, L: Elector<S>, H: Hasher> {
     context: ContextCell<E>,
     scheme: S,
+    elector: L,
     namespace: Vec<u8>,
     epoch: Epoch,
     relay: Arc<Relay<H::Digest, S::PublicKey>>,
@@ -34,8 +36,10 @@ pub struct Equivocator<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher
     sent: HashSet<View>,
 }
 
-impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> Equivocator<E, S, H> {
-    pub fn new(context: E, cfg: Config<S, H>) -> Self {
+impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, L: Elector<S>, H: Hasher>
+    Equivocator<E, S, L, H>
+{
+    pub fn new(context: E, cfg: Config<S, L, H>) -> Self {
         Self {
             context: ContextCell::new(context),
             scheme: cfg.scheme,
@@ -43,6 +47,7 @@ impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> Equivocator<E, S
             epoch: cfg.epoch,
             relay: cfg.relay,
             hasher: cfg.hasher,
+            elector: cfg.elector,
             sent: HashSet::new(),
         }
     }
@@ -71,7 +76,7 @@ impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> Equivocator<E, S
             let (_, certificate) = certificate_receiver.recv().await.unwrap();
 
             // Parse certificate
-            let (view, parent, seed) = match Certificate::<S, H::Digest>::decode_cfg(
+            let (view, parent, certificate) = match Certificate::<S, H::Digest>::decode_cfg(
                 certificate,
                 &self.scheme.certificate_codec_config(),
             )
@@ -80,14 +85,12 @@ impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> Equivocator<E, S
                 Certificate::Notarization(notarization) => (
                     notarization.proposal.round.view(),
                     notarization.proposal.payload,
-                    self.scheme
-                        .seed(notarization.proposal.round, &notarization.certificate),
+                    notarization.certificate,
                 ),
                 Certificate::Finalization(finalization) => (
                     finalization.proposal.round.view(),
                     finalization.proposal.payload,
-                    self.scheme
-                        .seed(finalization.proposal.round, &finalization.certificate),
+                    finalization.certificate,
                 ),
                 _ => continue, // we don't build on nullifications to avoid tracking complexity
             };
@@ -102,8 +105,7 @@ impl<E: Clock + Rng + Spawner, S: Scheme<H::Digest>, H: Hasher> Equivocator<E, S
             let next_round = Round::new(self.epoch, next_view);
 
             // Check if we are the leader for the next view, otherwise move on
-            let (_, leader) =
-                select_leader::<S>(self.scheme.participants().as_ref(), next_round, seed);
+            let leader = self.elector.elect(next_round, Some(&certificate));
             if leader != self.scheme.me().unwrap() {
                 continue;
             }
