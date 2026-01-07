746ad81a68d007429f36eb69b4b07c0e2a3b6f1b
Patrick O'Grady <me@patrickogrady.xyz>
2026-01-06 16:13:18 -0800

    remove old benchmarks

--- storage/src/archive/benches/bench.rs
+++ storage/src/archive/benches/bench.rs
@@ -1,16 +1,8 @@
 use criterion::criterion_main;
 
 mod get;
-mod get_large;
-mod get_pressure;
 mod put;
 mod restart;
 mod utils;
 
-criterion_main!(
-    put::benches,
-    get::benches,
-    get_large::benches,
-    get_pressure::benches,
-    restart::benches
-);
+criterion_main!(put::benches, get::benches, restart::benches);
