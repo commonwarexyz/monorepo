3ada199f21b7253bee298817a70af4c7c1ecd754
Patrick O'Grady <me@patrickogrady.xyz>
2026-01-09 13:36:37 -0800

    add hash_to_curve benchmarks

--- cryptography/src/bls12381/benches/bench.rs
+++ cryptography/src/bls12381/benches/bench.rs
@@ -8,6 +8,7 @@ mod combine_public_keys;
 mod combine_signatures;
 mod dkg;
 mod evaluate_point;
+mod hash_to_curve;
 mod msm;
 mod pairing_verify;
 mod scheme_batch_verify_same_message;
@@ -23,6 +24,7 @@ mod tle_encrypt;
 criterion_main!(
     batch_to_affine::benches,
     dkg::benches,
+    hash_to_curve::benches,
     threshold_recover::benches,
     combine_public_keys::benches,
     combine_signatures::benches,
