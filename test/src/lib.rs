0a42464e38eafd1cdd08b75a9991b14b7b380a02
Lucas Meier <lucas@cronokirby.com>
2026-01-14 12:11:06 -0800

    Add new testing utility crate

---
+++ test/src/lib.rs
@@ -0,0 +1,6 @@
+//! Simplify testing and fuzzing of distributed systems.
+
+#![doc(
+    html_logo_url = "https://commonware.xyz/imgs/rustdoc_logo.svg",
+    html_favicon_url = "https://commonware.xyz/favicon.ico"
+)]
