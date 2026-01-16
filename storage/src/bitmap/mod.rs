0d89af1b95f7f3e5649764f33e0d5c955828e3e0
Patrick O'Grady <me@patrickogrady.xyz>
2026-01-15 19:56:33 -0800

    order

--- storage/src/bitmap/mod.rs
+++ storage/src/bitmap/mod.rs
@@ -1,3 +1,3 @@
 mod authenticated;
-pub use authenticated::{BitMap, CleanBitMap, DirtyBitMap};
 pub(crate) use authenticated::partial_chunk_root;
+pub use authenticated::{BitMap, CleanBitMap, DirtyBitMap};
