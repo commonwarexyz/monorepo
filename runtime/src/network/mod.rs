69f44664ce5702eb7fcbeabbc7409f00618eeb58
Patrick O'Grady <me@patrickogrady.xyz>
2026-01-19 23:35:47 -0800

    add 2 to metered

--- runtime/src/network/mod.rs
+++ runtime/src/network/mod.rs
@@ -2,7 +2,7 @@ use commonware_macros::ready_mod;
 
 ready_mod!(0, pub(crate) mod audited);
 ready_mod!(0, pub(crate) mod deterministic);
-ready_mod!(0, pub(crate) mod metered);
+ready_mod!(2, pub(crate) mod metered);
 
 #[cfg(all(not(target_arch = "wasm32"), not(feature = "iouring-network")))]
 ready_mod!(2, pub(crate) mod tokio);
