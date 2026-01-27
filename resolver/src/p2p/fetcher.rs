a23cd0853f9ec6bbe92fa5cf2193f462222272b7
Patrick O'Grady <me@patrickogrady.xyz>
2026-01-27 11:41:05 -0800

    nits

--- resolver/src/p2p/fetcher.rs
+++ resolver/src/p2p/fetcher.rs
@@ -8,7 +8,7 @@ use commonware_runtime::{
     },
     Clock, Metrics,
 };
-use commonware_utils::{PrioritySet, Span, SystemTimeExt};
+use commonware_utils::{PrioritySet, Span};
 use prometheus_client::{
     encoding::EncodeLabelSet,
     metrics::{family::Family, gauge::Gauge, histogram::Histogram},
@@ -349,7 +349,7 @@ where
             self.context.current() + self.retry_timeout
         } else {
             // No eligible peers - wait for external changes
-            self.context.current().saturating_add(Duration::MAX)
+            commonware_utils::SystemTimeExt::saturating_add(&self.context.current(), Duration::MAX)
         });
     }
 
