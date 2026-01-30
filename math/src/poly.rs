4b3d6bb824bb61bf0958aa375dc70c06c1824fc3
André Silva <123550+andresilva@users.noreply.github.com>
2026-01-30 22:33:45 +0000

    [math] fix no_std (#3007)

--- math/src/poly.rs
+++ math/src/poly.rs
@@ -603,7 +603,7 @@ mod test {
             prop_assume!(f.required().get() >= 2);
             prop_assume!(f.required().get() < F::MAX as u32);
             let n = f.required().get();
-            let points: Vec<_> = (1..n).map(|i| F::from(i as u8)).chain(std::iter::once(F::zero())).collect();
+            let points: Vec<_> = (1..n).map(|i| F::from(i as u8)).chain(core::iter::once(F::zero())).collect();
             let interpolator = Interpolator::new(points.iter().copied().enumerate());
             let evals = Map::from_iter_dedup(points.iter().map(|p| f.eval(p)).enumerate());
             let recovered = interpolator.interpolate(&evals, &Sequential);
