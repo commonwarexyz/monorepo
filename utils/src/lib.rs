a3c5031c1c3287a9b38a90a3898ba55b62fdc92a
clabby <ben@clab.by>
2026-01-31 16:36:02 -0500

    [workspace] Link `std` in `cfg(test)`

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -4,7 +4,7 @@
     html_logo_url = "https://commonware.xyz/imgs/rustdoc_logo.svg",
     html_favicon_url = "https://commonware.xyz/favicon.ico"
 )]
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(any(feature = "std", test)), no_std)]
 
 commonware_macros::stability_scope!(BETA {
     #[cfg(not(feature = "std"))]
