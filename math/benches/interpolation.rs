b61377ea46d7386f7836a47e3698ce3499d09863
Lucas Meier <lucas@cronokirby.com>
2026-02-13 16:39:20 -0800

    [math] Fix bench names

--- math/benches/interpolation.rs
+++ math/benches/interpolation.rs
@@ -4,30 +4,34 @@ use core::num::NonZeroU32;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_interpolator_creation(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("{}::interpolator_creation", module_path!()));
-
     for &n in &[4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096] {
         let t = N3f1::quorum(n);
         let total = NonZeroU32::new(n).unwrap();
 
         let points: Vec<(u32, u32)> = (0..t).map(|i| (i, i + 1)).collect();
 
-        group.bench_function(format!("method=naive n={n}"), |b| {
+        let naive_label = format!(
+            "{module}::interpolator_creation/type=naive n={n}",
+            module = module_path!()
+        );
+        c.bench_function(&naive_label, |b| {
             b.iter(|| {
                 let _: Interpolator<u32, F> =
                     Interpolator::roots_of_unity_naive(total, points.iter().copied());
             });
         });
 
-        group.bench_function(format!("method=fast n={n}"), |b| {
+        let fast_label = format!(
+            "{module}::interpolator_creation/type=fast n={n}",
+            module = module_path!()
+        );
+        c.bench_function(&fast_label, |b| {
             b.iter(|| {
                 let _: Interpolator<u32, F> =
                     Interpolator::roots_of_unity(total, points.iter().copied());
             });
         });
     }
-
-    group.finish();
 }
 
 criterion_group!(benches, bench_interpolator_creation,);
