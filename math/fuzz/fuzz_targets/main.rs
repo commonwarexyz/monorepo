8fbbe3a2641a27acff873123fe72e52f7f608758
Patrick O'Grady <me@patrickogrady.xyz>
2026-02-19 16:24:53 -0800

    nit

--- math/fuzz/fuzz_targets/main.rs
+++ math/fuzz/fuzz_targets/main.rs
@@ -1,7 +1,8 @@
 #![no_main]
 
+use arbitrary::{Arbitrary, Unstructured};
+
 libfuzzer_sys::fuzz_target!(|input: &[u8]| {
-    use arbitrary::{Arbitrary, Unstructured};
     let mut u = Unstructured::new(input);
     if let Ok(plan) = commonware_math::fuzz::Plan::arbitrary(&mut u) {
         let _ = plan.run(&mut u);
