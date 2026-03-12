d5b5f6d0cf4f4a1959600972a4f260c151b7a470
Lucas Meier <lucas@cronokirby.com>
2026-03-12 15:49:05 -0700

    WIP: Golden_dkg api

--- cryptography/src/bls12381/golden_dkg.rs
+++ cryptography/src/bls12381/golden_dkg.rs
@@ -1,2 +1,49 @@
 #[allow(dead_code)]
 mod evrf;
+
+use super::dkg::Info;
+use crate::bls12381::primitives::{group::Share, sharing::Sharing, variant::Variant};
+use commonware_utils::ordered::Map;
+pub use evrf::{PrivateKey, PublicKey};
+use rand_core::CryptoRngCore;
+use std::collections::BTreeMap;
+
+pub fn deal<V: Variant>(
+    _rng: &mut impl CryptoRngCore,
+    _info: &Info<V, PublicKey>,
+    _me: &PrivateKey,
+) -> SignedDealerLog {
+    todo!()
+}
+
+pub fn observe<V: Variant>(_logs: BTreeMap<PublicKey, DealerLog>) -> Sharing<V> {
+    todo!()
+}
+
+pub fn play<V: Variant>(
+    _logs: BTreeMap<PublicKey, DealerLog>,
+    _me: &PrivateKey,
+) -> (Sharing<V>, Share) {
+    todo!()
+}
+
+pub struct SignedDealerLog {}
+
+impl SignedDealerLog {
+    pub fn identify(self) -> Option<(PublicKey, DealerLog)> {
+        todo!()
+    }
+}
+
+pub struct DealerLog {}
+
+impl DealerLog {
+    #[allow(dead_code)]
+    fn batch_check(
+        _batch: impl IntoIterator<Item = (PublicKey, Self)>,
+    ) -> Map<PublicKey, EncryptedShares> {
+        todo!()
+    }
+}
+
+struct EncryptedShares {}
