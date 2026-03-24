545b69298c6ef952caa1047441b8c36c3db46b50
André Silva <andrerfosilva@gmail.com>
2026-03-24 18:19:48 +0000

    [runtime/tokio] log

--- runtime/src/tokio/runtime.rs
+++ runtime/src/tokio/runtime.rs
@@ -559,7 +559,7 @@ impl crate::Runner for Runner {
         // the timeout, log the slow shutdown and continue returning or resuming
         // the original panic.
         if !executor.tasks.wait_for_idle(self.cfg.shutdown_timeout) {
-            error!("timed out waiting for tracked tasks to finish during tokio runtime shutdown");
+            error!("timed out waiting for tasks to finish during tokio runtime shutdown");
         }
         result.unwrap_or_else(|payload| resume_unwind(payload))
     }
