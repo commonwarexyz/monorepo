1cb18db11ce060387a2874c67e020a9b9567ecb4
Patrick O'Grady <me@patrickogrady.xyz>
2026-03-24 22:47:50 -0400

    fmt

--- runtime/src/tokio/runtime.rs
+++ runtime/src/tokio/runtime.rs
@@ -740,7 +740,9 @@ impl Clock for Context {
     }
 
     fn sleep_until(&self, deadline: SystemTime) -> impl Future<Output = ()> + Send + 'static {
-        let duration_until_deadline = deadline.duration_since(SystemTime::now()).unwrap_or_default();
+        let duration_until_deadline = deadline
+            .duration_since(SystemTime::now())
+            .unwrap_or_default();
         tokio::time::sleep(duration_until_deadline)
     }
 }
