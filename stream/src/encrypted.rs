a200cc88c4e89c142efe521a4b8838bb8359208c
Patrick O'Grady <me@patrickogrady.xyz>
2026-04-16 15:31:32 -0700

    nits

--- stream/src/encrypted.rs
+++ stream/src/encrypted.rs
@@ -590,7 +590,7 @@ mod test {
             let dialer_config = transport_config(dialer_crypto.clone());
             let listener_config = transport_config(listener_crypto.clone());
 
-            let listener_handle = context.child("encrypted").spawn(move |context| async move {
+            let listener_handle = context.child("listener").spawn(move |context| async move {
                 listen(
                     context,
                     |_| async { true },
@@ -602,7 +602,7 @@ mod test {
             });
 
             let (mut dialer_sender, mut dialer_receiver) = dial(
-                context,
+                context.child("dialer"),
                 dialer_config,
                 listener_crypto.public_key(),
                 dialer_stream,
@@ -641,7 +641,7 @@ mod test {
             let dialer_config = transport_config(dialer_crypto.clone());
             let listener_config = transport_config(listener_crypto.clone());
 
-            let listener_handle = context.child("encrypted").spawn(move |context| async move {
+            let listener_handle = context.child("listener").spawn(move |context| async move {
                 listen(
                     context,
                     |_| async { true },
@@ -653,7 +653,7 @@ mod test {
             });
 
             let (mut dialer_sender, _dialer_receiver) = dial(
-                context,
+                context.child("dialer"),
                 dialer_config,
                 listener_crypto.public_key(),
                 dialer_stream,
@@ -716,7 +716,7 @@ mod test {
             let dialer_config = transport_config(dialer_crypto.clone());
             let listener_config = transport_config(listener_crypto.clone());
 
-            let listener_handle = context.child("encrypted").spawn(move |context| async move {
+            let listener_handle = context.child("listener").spawn(move |context| async move {
                 listen(
                     context,
                     |_| async { true },
@@ -728,7 +728,7 @@ mod test {
             });
 
             let (mut dialer_sender, _dialer_receiver) = dial(
-                context,
+                context.child("dialer"),
                 dialer_config,
                 listener_crypto.public_key(),
                 dialer_stream,
@@ -787,7 +787,7 @@ mod test {
             let dialer_config = transport_config(dialer_crypto.clone());
             let listener_config = transport_config(listener_crypto.clone());
 
-            let listener_handle = context.child("encrypted").spawn(move |context| async move {
+            let listener_handle = context.child("listener").spawn(move |context| async move {
                 listen(
                     context,
                     |_| async { true },
@@ -799,7 +799,7 @@ mod test {
             });
 
             let (mut dialer_sender, _dialer_receiver) = dial(
-                context,
+                context.child("dialer"),
                 dialer_config,
                 listener_crypto.public_key(),
                 dialer_stream,
@@ -846,7 +846,7 @@ mod test {
             let dialer_config = transport_config(dialer_crypto.clone());
             let listener_config = transport_config(listener_crypto.clone());
 
-            let listener_handle = context.child("encrypted").spawn(move |context| async move {
+            let listener_handle = context.child("listener").spawn(move |context| async move {
                 listen(
                     context,
                     |_| async { true },
@@ -858,7 +858,7 @@ mod test {
             });
 
             let (mut dialer_sender, _dialer_receiver) = dial(
-                context,
+                context.child("dialer"),
                 dialer_config,
                 listener_crypto.public_key(),
                 dialer_stream,
@@ -952,9 +952,9 @@ mod test {
             // Build a valid SynAck only to derive its true encoded size for the
             // oversized prefix we inject below.
             let (current_time, ok_timestamps) = dialer_config.time_information(&context);
-            let mut listener_rng = context.child("encrypted");
+            let mut listener_rng = context.child("listener");
             let (_, syn) = dial_start(
-                context.child("encrypted"),
+                context.child("dialer"),
                 Context::new(
                     &Transcript::new(&dialer_config.namespace),
                     current_time,
