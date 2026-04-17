d9ac849418784045d149bccf696a4a1acddeffc5
Patrick O'Grady <me@patrickogrady.xyz>
2026-04-16 22:40:11 -0700

    fmt

--- examples/reshare/src/orchestrator/mod.rs
+++ examples/reshare/src/orchestrator/mod.rs
@@ -4,6 +4,6 @@ mod actor;
 pub use actor::{Actor, Config};
 
 mod ingress;
-pub use ingress::{EpochTransition, Mailbox};
 #[cfg(test)]
 pub use ingress::Message;
+pub use ingress::{EpochTransition, Mailbox};
