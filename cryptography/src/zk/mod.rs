ed944b1bc12d63b20387b65d4397e3cb63b1b24a
Lucas Meier <lucas@cronokirby.com>
2026-04-20 17:20:25 -0700

    [zk] Cleanup around bulletproofs / circuit

--- cryptography/src/zk/mod.rs
+++ cryptography/src/zk/mod.rs
@@ -1,2 +1,25 @@
 #[cfg(feature = "std")]
 pub mod bulletproofs;
+
+#[cfg(all(feature = "std", feature = "fuzz"))]
+pub mod fuzz {
+    use arbitrary::{Arbitrary, Unstructured};
+
+    pub enum Plan {
+        Bulletproofs(crate::zk::bulletproofs::fuzz::Plan),
+    }
+
+    impl<'a> Arbitrary<'a> for Plan {
+        fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+            Ok(Self::Bulletproofs(u.arbitrary()?))
+        }
+    }
+
+    impl Plan {
+        pub fn run(self, u: &mut Unstructured<'_>) -> arbitrary::Result<()> {
+            match self {
+                Self::Bulletproofs(plan) => plan.run(u),
+            }
+        }
+    }
+}
