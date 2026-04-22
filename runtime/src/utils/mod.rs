1fb2e837c28a532b320faef3b90feeab77521f47
André Silva <andrerfosilva@gmail.com>
2026-04-22 23:42:18 +0100

    [runtime] fix wasm

--- runtime/src/utils/mod.rs
+++ runtime/src/utils/mod.rs
@@ -16,6 +16,7 @@ commonware_macros::stability_mod!(BETA, pub mod buffer);
 pub mod signal;
 #[cfg(not(target_arch = "wasm32"))]
 pub(crate) mod thread;
+#[cfg(not(target_arch = "wasm32"))]
 pub use thread::available_cpus;
 
 mod handle;
