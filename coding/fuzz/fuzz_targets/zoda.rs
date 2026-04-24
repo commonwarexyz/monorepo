1a7558fa7e6e72e7d4743e8ae5cac4655dedfbae
Denis Kolegov <denis.kolegov@asymmetric.re>
2026-04-24 11:31:01 +0200

    Fix slow crashes in zoda fuzzing (#3603)

--- coding/fuzz/fuzz_targets/zoda.rs
+++ coding/fuzz/fuzz_targets/zoda.rs
@@ -1,10 +1,10 @@
 #![no_main]
 
 use commonware_coding::Zoda;
-use commonware_coding_fuzz::{fuzz_phased, FuzzInput};
+use commonware_coding_fuzz::{fuzz_phased, PhasedFuzzInput};
 use commonware_cryptography::Sha256;
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: FuzzInput| {
+fuzz_target!(|input: PhasedFuzzInput| {
     fuzz_phased::<Zoda<Sha256>>(input);
 });
