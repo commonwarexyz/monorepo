e79da4bcc726ce3bb077fa4a3b1c895e437e65c3
Denis Kolegov <denis.kolegov@asymmetric.re>
2026-04-27 20:22:18 +0200

    Run Simplex invariant check only when the quorum is feasible (#3383)

--- coding/fuzz/fuzz_targets/codec_read.rs
+++ coding/fuzz/fuzz_targets/codec_read.rs
@@ -2,13 +2,12 @@
 
 use arbitrary::Arbitrary;
 use commonware_codec::Read;
-use commonware_coding::{NoCoding, ReedSolomon, Scheme, Zoda};
+use commonware_coding::{PhasedAsScheme, ReedSolomon, Scheme, Zoda};
 use commonware_cryptography::Sha256;
 use libfuzzer_sys::fuzz_target;
 
-type ZodaShard = <Zoda<Sha256> as Scheme>::Shard;
+type ZodaShard = <PhasedAsScheme<Zoda<Sha256>> as Scheme>::Shard;
 type ReedSolomonShard = <ReedSolomon<Sha256> as Scheme>::Shard;
-type NoCodingShard = <NoCoding<Sha256> as Scheme>::Shard;
 
 const MAX_DATA_BYTES: usize = 1 << 20;
 const MAX_INPUT_LEN: usize = 1 << 20;
@@ -17,7 +16,6 @@ const MAX_INPUT_LEN: usize = 1 << 20;
 enum SchemeSelector {
     Zoda,
     ReedSolomon,
-    NoCoding,
 }
 
 #[derive(Arbitrary, Debug)]
@@ -45,9 +43,6 @@ fn fuzz(input: FuzzInput) {
         }
         SchemeSelector::ReedSolomon => {
             let _ = ReedSolomonShard::read_cfg(&mut buf, &codec_config);
-        }
-        SchemeSelector::NoCoding => {
-            let _ = NoCodingShard::read_cfg(&mut buf, &codec_config);
         }
     }
 }
