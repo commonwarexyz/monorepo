9a05244154244a2322a6f39876af1aa7a9694ec1
Patrick O'Grady <me@patrickogrady.xyz>
2026-04-28 16:51:37 -0700

    nits

--- utils/src/channel/mod.rs
+++ utils/src/channel/mod.rs
@@ -1,9 +1,8 @@
 //! Utilities for working with channels.
 
 pub mod fallible;
-mod reservation;
+pub mod reservation;
 pub mod ring;
 pub mod tracked;
 
-pub use reservation::{Reservation, ReservationExt, Reserved};
 pub use tokio::sync::{mpsc, oneshot};
