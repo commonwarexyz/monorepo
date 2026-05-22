20258fb23fafde8033ab229dc0b505e6da478ba2
Denis Kolegov <denis.kolegov@asymmetric.re>
2026-05-22 22:24:28 +0200

    fix features

--- consensus/fuzz/fuzz_targets/marshal_coding.rs
+++ consensus/fuzz/fuzz_targets/marshal_coding.rs
@@ -1,9 +1,12 @@
 #![no_main]
 
-use commonware_consensus::marshal::mocks::harness::CodingHarness;
-use commonware_consensus_fuzz::marshal::{fuzz_marshal, MarshalFuzzInput};
-use libfuzzer_sys::fuzz_target;
+#[cfg(feature = "mocks")]
+mod fuzz {
+    use commonware_consensus::marshal::mocks::harness::CodingHarness;
+    use commonware_consensus_fuzz::marshal::{fuzz_marshal, MarshalFuzzInput};
+    use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: MarshalFuzzInput| {
-    fuzz_marshal::<CodingHarness>(input);
-});
+    fuzz_target!(|input: MarshalFuzzInput| {
+        fuzz_marshal::<CodingHarness>(input);
+    });
+}
--- consensus/fuzz/fuzz_targets/marshal_standard.rs
+++ consensus/fuzz/fuzz_targets/marshal_standard.rs
@@ -1,9 +1,12 @@
 #![no_main]
 
-use commonware_consensus::marshal::mocks::harness::StandardHarness;
-use commonware_consensus_fuzz::marshal::{fuzz_marshal, MarshalFuzzInput};
-use libfuzzer_sys::fuzz_target;
+#[cfg(feature = "mocks")]
+mod fuzz {
+    use commonware_consensus::marshal::mocks::harness::StandardHarness;
+    use commonware_consensus_fuzz::marshal::{fuzz_marshal, MarshalFuzzInput};
+    use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: MarshalFuzzInput| {
-    fuzz_marshal::<StandardHarness>(input);
-});
+    fuzz_target!(|input: MarshalFuzzInput| {
+        fuzz_marshal::<StandardHarness>(input);
+    });
+}
