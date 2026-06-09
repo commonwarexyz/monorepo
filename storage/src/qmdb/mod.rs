900638c9f64d7e7378822e17754270e9bd10c4ec
Roberto Bayardo <roberto@commonware.xyz>
2026-06-09 12:57:27 -0700

    address review

--- storage/src/qmdb/mod.rs
+++ storage/src/qmdb/mod.rs
@@ -31,8 +31,7 @@
 //!
 //! # Traits
 //!
-//! Keyed mutable variants ([any] and [current]) implement `any::traits::DbAny` and
-//! persistence methods exposed by each database type.
+//! Keyed mutable variants ([any] and [current]) implement `any::traits::DbAny`.
 //!
 //! # Acknowledgments
 //!
