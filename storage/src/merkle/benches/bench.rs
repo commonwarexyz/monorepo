f3b280d237f62f80bafc20703e462af3bb30cf3e
Roberto Bayardo <roberto@commonware.xyz>
2026-06-09 08:44:47 -0700

    position-to-location bench

--- storage/src/merkle/benches/bench.rs
+++ storage/src/merkle/benches/bench.rs
@@ -2,6 +2,7 @@ use criterion::criterion_main;
 
 mod append;
 mod append_additional;
+mod position_to_location;
 mod prove_many_elements;
 mod prove_single_element;
 mod update;
@@ -9,6 +10,7 @@ mod update;
 criterion_main!(
     append::benches,
     append_additional::benches,
+    position_to_location::benches,
     prove_many_elements::benches,
     prove_single_element::benches,
     update::benches,
