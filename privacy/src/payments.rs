364c6d67c916e3605bbf7e5fc598c4094ab1c513
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2026-06-18 15:38:21 -0700

    cleanup

--- privacy/src/payments.rs
+++ privacy/src/payments.rs
@@ -7,13 +7,16 @@
 
 use core::ops::{Add, Sub};
 
-use rand_core::RngCore;
+use rand_core::CryptoRngCore;
 
 /// A homomorphic commitment to a balance value.
 pub trait Commitment:
     Clone + Eq + for<'a> Add<&'a Self, Output = Self> + for<'a> Sub<&'a Self, Output = Self>
 {
     fn zero() -> Self;
+
+    // deterministically commit to a public value
+    fn commit_public(value: u64) -> Self;
 }
 
 /// The opening of a commitment
@@ -37,10 +40,13 @@ pub trait Backend: Sized {
     /// The commitment opening held by the account owner
     type Opening: Opening;
 
+    /// verified using amount and amount_commitment
     type FundProof;
 
+    /// verified using sender_commitment, amount_commitment
     type TransferProof;
 
+    /// verified using sender_commitment, amount
     type BurnProof;
 
     /// Deterministic source material for [`Self::setup`]: for example a seed for a
@@ -48,35 +54,65 @@ pub trait Backend: Sized {
     /// trusted setup.
     type SetupInput;
 
-    type SetupError: core::fmt::Debug;
+    type SetupError;
 
     /// Deterministically derive the public parameters from `input`.
     fn setup(input: &Self::SetupInput) -> Result<Self::Params, Self::SetupError>;
 
     /// mechanism to movefunds from a public account into a private account.
+    /// returns (amount_commitment, amount_opening, fund_proof)
+    /// to apply:
+    /// 1. verify(amount, amount_commitment, fund_proof) == 1
+    /// 2. sender_public_balance <- sender_public_balance - amount
+    /// 2. sender_commitment <- sender_commitment + amount_commitment
     fn fund(
         params: &Self::Params,
         value: u64,
-        rng: &mut impl RngCore,
+        rng: &mut impl CryptoRngCore,
     ) -> (Self::Commitment, Self::Opening, Self::FundProof);
 
     /// mechanism to move funds between private accounts.
     /// takes as input the sender's commitment, it's opening and the amount to transfer
     /// returns (amount_commitment, amount_opening, transfer_proof)
     /// note: can be computed before the receipient is even known
+    /// to apply: 
+    /// 1. verify(sender_commitment, amount_commitment, transfer_proof) == 1
+    /// 2. sender_commitment <- sender_commitment - amount_commitment
+    /// 3. recipient_commitment <- recipient_commitment + amount_commitment
     fn transfer(
         params: &Self::Params,
         input_commitment: &Self::Commitment,
         input_opening: &Self::Opening,
         amount: u64,
-        rng: &mut impl RngCore,
+        rng: &mut impl CryptoRngCore,
     ) -> (Self::Commitment, Self::Opening, Self::TransferProof);
 
-    /// mechanism to reveal the balance of a private account
+    /// mechanism to move funds from a private account into a public account
+    /// takes as input the sender's commitment, it's opening and the amount to burn
+    /// returns burn_proof
+    /// to apply:
+    /// 1. verify(commitment, amount, burn_proof) == 1
+    /// 2. sender_public_balance <- sender_public_balance + amount
+    /// 3. sender_commitment <- sender_commitment - commit_public(amount)
     fn burn(
         params: &Self::Params,
         commitment: &Self::Commitment,
         opening: &Self::Opening,
-        rng: &mut impl RngCore,
+        amount: u64,
+        rng: &mut impl CryptoRngCore,
     ) -> Self::BurnProof;
+
+    /// batch verify a set of funds, transfers, and burns,
+    /// return `true` iff all proofs are valid
+    fn batch_verify(
+        params: &Self::Params,
+        funds: &[(u64, Self::Commitment, Self::FundProof)],
+        transfers: &[(
+            Self::Commitment,
+            Self::Commitment,
+            Self::TransferProof,
+        )],
+        burns: &[(Self::Commitment, u64, Self::BurnProof)],
+        rng: &mut impl CryptoRngCore,
+    ) -> bool;
 }
