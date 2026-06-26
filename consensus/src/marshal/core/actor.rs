c40fb577c02ef16dff01d4c125d1ee34b9b656b5
Dan Laine <dan@commonware.xyz>
2026-06-25 17:42:01 -0700

    misc cleanup

--- consensus/src/marshal/core/actor.rs
+++ consensus/src/marshal/core/actor.rs
@@ -902,6 +902,7 @@ where
         join_all(
             produces
                 .into_iter()
+                .filter(|(_, response)| !response.is_closed()) // Ignore closed senders
                 .map(|(key, response)| self.handle_produce(key, response, buffer)),
         )
         .await;
