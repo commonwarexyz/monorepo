fb7c150b1cff3aecb8fcf1e6481d39ff3505bf98
Denis Kolegov <denis.kolegov@asymmetric.re>
2026-07-06 13:54:19 +0200

    Fix tests

--- consensus/fuzz/src/simplex.rs
+++ consensus/fuzz/src/simplex.rs
@@ -304,6 +304,8 @@ mod tests {
             degraded_network: false,
             strategy: StrategyChoice::AnyScope,
             messaging_faults: Vec::new(),
+            mailbox_size: crate::DEFAULT_MAILBOX_SIZE,
+            fetch_concurrent: crate::DEFAULT_FETCH_CONCURRENT,
             forwarding: ForwardingPolicy::Disabled,
             certify: CertifyChoice::Always,
             reporting: ReporterWiring::Solo,
