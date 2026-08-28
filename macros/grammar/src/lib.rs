7d62736f9b52a6b2e5c43b0820f4870d0f7cf382
André Silva <andrerfosilva@gmail.com>
2026-08-28 11:44:02 +0100

    [macros] Keep grammar available to stability checks

--- macros/grammar/src/lib.rs
+++ macros/grammar/src/lib.rs
@@ -9,17 +9,13 @@
     html_favicon_url = "https://commonware.xyz/favicon.ico"
 )]
 
-#[cfg(not(commonware_stability_RESERVED))]
 mod select;
-#[cfg(not(commonware_stability_RESERVED))]
 mod stability;
 
-#[cfg(not(commonware_stability_RESERVED))]
 pub use select::{
     SelectBranch, SelectInput, SelectLoopBranch, SelectLoopElse, SelectLoopInput,
     SelectLoopLifecycle,
 };
-#[cfg(not(commonware_stability_RESERVED))]
 pub use stability::{
     StabilityCfg, StabilityLevel, StabilityLevelSyntax, StabilityModInput, StabilityScopeInput,
 };
