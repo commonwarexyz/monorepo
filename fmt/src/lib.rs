bc8d638a0b1945bf6b6389de963984061f6989e2
André Silva <andrerfosilva@gmail.com>
2026-08-28 12:49:45 +0100

    [fmt] Classify formatter as internal tooling

--- fmt/src/lib.rs
+++ fmt/src/lib.rs
@@ -6,19 +6,12 @@
     html_favicon_url = "https://commonware.xyz/favicon.ico"
 )]
 
-#[cfg(not(commonware_stability_RESERVED))]
 pub mod file;
-#[cfg(not(commonware_stability_RESERVED))]
 pub mod macros;
-#[cfg(not(commonware_stability_RESERVED))]
 pub mod pretty;
-#[cfg(not(commonware_stability_RESERVED))]
 pub mod source;
 
-#[cfg(not(commonware_stability_RESERVED))]
 mod marker;
-#[cfg(not(commonware_stability_RESERVED))]
 mod skip;
-#[cfg(not(commonware_stability_RESERVED))]
 mod trivia;
 mod writer;
