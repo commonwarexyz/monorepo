c59672b586efa9f5b29108d0b77438e9a76864bc
Patrick O'Grady <me@patrickogrady.xyz>
2026-09-17 13:48:21 -0700

    [cryptography] Precompute BMT SHA-NI padding (#4804)

--- cryptography/src/sha256/benches/bench.rs
+++ cryptography/src/sha256/benches/bench.rs
@@ -2,5 +2,10 @@ use criterion::criterion_main;
 
 mod hash_many;
 mod hash_message;
+mod hash_pair;
 
-criterion_main!(hash_message::benches, hash_many::benches);
+criterion_main!(
+    hash_message::benches,
+    hash_many::benches,
+    hash_pair::benches
+);
