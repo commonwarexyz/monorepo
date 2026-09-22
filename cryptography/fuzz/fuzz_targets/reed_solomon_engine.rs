6e1ff67c1d431ba7a1a9ecd94d776e2ca8e9029a
Lucas Meier <lucas@cronokirby.com>
2026-09-22 16:59:54 -0700

    [cryptography] Share Reed-Solomon backend fuzz tests

--- cryptography/fuzz/fuzz_targets/reed_solomon_engine.rs
+++ cryptography/fuzz/fuzz_targets/reed_solomon_engine.rs
@@ -1,8 +1,12 @@
 #![no_main]
 
-use commonware_cryptography::reed_solomon::fuzz::differential_engine;
+use arbitrary::{Arbitrary, Unstructured};
+use commonware_cryptography::reed_solomon::fuzz::EnginePlan;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|input: &[u8]| {
-    differential_engine(input);
+    let mut u = Unstructured::new(input);
+    if let Ok(plan) = EnginePlan::arbitrary(&mut u) {
+        let _ = plan.run(&mut u);
+    }
 });
